275766502faeadeda59191973819344e5970bcef
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-11-07 11:50:33 -0500

    chore: change bundle + project name to mbta_app, add dev team to xcode project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mobile_app
+# mbta_app
 
 The MBTA Mobile App
 
--- android/app/src/main/kotlin/com/mbta/tid/mbta_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/mbta/tid/mbta_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile_app
+package com.mbta.tid.mbta_app
 
 import io.flutter.embedding.android.FlutterActivity
 
