62f9975bc6a77f1900cc883cbcf03c5e8187caf7
Emma Simon <esimon@mbta.com>
2023-11-28 11:34:09 -0500

    ci: Get build and deploys running on Xcode Cloud (#8)
    * ci: Add podfile for ios builds
    
    This is required for Xcode Cloud builds.
    
    * ci: Add Xcode Cloud post clone script
    
    * ci: Update configs to fix Xcode Cloud build
    
    * ci: Downgrade minimum iOS version back to 11.0
    
    This was a red herring, the real issue was the build configurations.
    
    * fix: Remove unused reference to nonexistent file

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Fail this script if any subcommand fails.
+set -e
+
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+
+# Install Flutter using git.
+git clone --quiet --branch $(awk '/flutter/ { print $2 }' .tool-versions) --depth 1 https://github.com/flutter/flutter.git $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter precache --ios
+
+# Install Flutter dependencies.
+flutter pub get
+
+# Generate code
+# TODO: Uncomment when we start needing to generate code
+# dart run build_runner build
+
+# Install CocoaPods using Homebrew.
+export HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates
+export HOMEBREW_NO_INSTALL_CLEANUP=1 # disable automatic cleanup
+brew install --quiet cocoapods
+
+# Install CocoaPods dependencies.
+cd ios && pod install && cd ..
+
+exit 0
