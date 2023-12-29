cfd7624d2087c3f84b0a63fb3d6ed1c4b1e5a37b
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-12-29 13:37:09 -0500

    tmp: Add test that takes 120s to run to check timeout

--- iosApp/iosAppUITests/IosAppUITests.swift
+++ iosApp/iosAppUITests/IosAppUITests.swift
@@ -16,6 +16,13 @@ final class IosAppUITests: XCTestCase {
         XCTAssertTrue(greeting.label.hasPrefix("Hello"), "When the app opens, greeting should be displayed")
     }
 
+    func testTimeoutConfiguration() throws {
+        let app = XCUIApplication()
+        app.launch()
+        let missingButton = app.buttons.firstMatch
+        XCTAssertTrue(missingButton.waitForExistence(timeout: 120), "When a timeout is > 60 seconds, the default max setting of 60 seconds is still enforced")
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
