0a7d2a89b05c1469f49061cd7e5c1246eb6f0585
Melody Horn <mhorn@mbta.com>
2024-01-11 11:12:16 -0700

    feat: add map
    attribution link does not work for mysterious reasons

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,58 @@
+import Mapbox
 import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = "Hello" // TODO: Add iOS i18n
-
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+        MapView().edgesIgnoringSafeArea(.all)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    let delegate = MapViewDelegate()
+
+    func makeUIView(context: Context) -> some UIView {
+        // Create the map view
+        let mapView = MGLMapView(frame: .zero)
+
+        mapView.logoView.isHidden = true
+
+        mapView.setCenter(
+            CLLocationCoordinate2D(
+                latitude: 42.355, longitude: -71.06555), animated: false)
+
+        mapView.setZoomLevel(13, animated: false)
+
+        mapView.delegate = delegate
+
+        return mapView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        //
+    }
+}
+
+class MapViewDelegate: NSObject, MGLMapViewDelegate {
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        // Set the tiles
+        let source = MGLRasterTileSource(
+            identifier: "dotcom",
+            tileURLTemplates: ["https://cdn.mbta.com/osm_tiles/{z}/{x}/{y}.png"],
+            options: [
+                .attributionInfos: [MGLAttributionInfo(
+                    title: .init(string: "© OpenStreetMap contributors"),
+                    url: .init(string: "http://osm.org/copyright"))],
+                .tileSize: 256])
+        let layer = MGLRasterStyleLayer(identifier: "dotcom", source: source)
+
+        style.addSource(source)
+        style.layers = [layer]
+    }
 }
