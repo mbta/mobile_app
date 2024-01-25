1bec59b271eb3923747abe9d1b3946e8aac90312
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-01-25 11:37:26 -0500

    conditional rendering, labels

--- iosApp/iosApp/HomeMap.swift
+++ iosApp/iosApp/HomeMap.swift
@@ -14,33 +14,74 @@ import Polyline
 
 struct HomeMapView: View {
     @ObservedObject var mapVM: HomeMapViewModel
+    @State private var viewport: Viewport = .camera(center: CLLocationCoordinate2D(latitude:42.355, longitude: -71.06555), zoom: 12, bearing: 0, pitch: 0)
+    var shouldShowStops = false
     
-   
+    
+    
     
     
     var body: some View {
+      
         
-        Map(initialViewport: .camera(center: CLLocationCoordinate2D(latitude:42.355, longitude: -71.06555), zoom: 12, bearing: 0, pitch: 0)) {
-           
-     
- 
+        Map(viewport: $viewport) {
             PolylineAnnotationGroup(mapVM.routes) { route in
                 PolylineAnnotation(id: route.id, lineCoordinates: route.coordinates).lineColor(StyleColor(route.color ?? .black)).lineWidth(3)
+                
+            }
+            
+            if (mapVM.zoom > 14) {
+      
+                PointAnnotationGroup(Array(stopsData.values)) { stop in
+                    PointAnnotation(coordinate:stop.coordinate).image(named: "t-logo")
                  
+                }
+                
+
             }
-            PointAnnotationGroup(Array(stopsData.values)) { stop in
-                PointAnnotation(coordinate:stop.coordinate).image(named: "t-logo")
+            
+            ForEvery(mapVM.vehicles) { vehicle in
+                MapViewAnnotation(layerId: "vehicles", featureId: vehicle.id) {
+                    Text("HELLO \(vehicle.id)!")
+                        .background(.white)
+                        .font(.callout)
+                        .offset(y:-25)
+                     
+                        
+                }.visible(mapVM.selectedVehicle == vehicle.id).selected(mapVM.selectedVehicle == vehicle.id)
+
             }
+            
+    
+            
             PointAnnotationGroup(mapVM.vehicles) { vehicle in
                 PointAnnotation(id: vehicle.id, coordinate: vehicle.coordinate).image(named: "bus-icon")
-            }
+                    .onTapGesture {
+                        print("TAPPED")
+                        mapVM.selectedVehicle = vehicle.id
+                        viewport = .camera(center: vehicle.coordinate, zoom: 15)
+                    
+                }
+            }.layerId("vehicles")
             
             
-  
-     
+            
+            
+            
+            
+            
+            
             
             
         }.mapStyle(.light)
+        // This is continuous - not like .onCameraChangeFinished. Debouncing recommended in docs
+        // https://docs.mapbox.com/ios/maps/api/11.1.0/documentation/mapboxmaps/swiftui-user-guide/#Using-Viewport-to-manage-camera
+            .onCameraChanged { cameraChanged in
+                mapVM.zoom = cameraChanged.cameraState.zoom
+                
+            }
+        
+        
     }
 }
 
@@ -48,9 +89,10 @@ final class HomeMapViewModel: NSObject, ObservableObject {
 
     @Published var vehicles: [VehicleAnnotation] = []
     @Published var routes: [CustomPolyline] = Array(routeFeatures.values)
+    @Published var zoom: CGFloat = 12
+    @Published var selectedVehicle: String? = nil
      var shouldShowStops = false
     var vehiclesTimer: Timer? = nil
-
 
 
     override init() {
