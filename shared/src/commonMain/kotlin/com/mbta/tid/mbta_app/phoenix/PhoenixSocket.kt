5defd7ce6e28bb2a6990211b77e144dbd6482c43
Melody Horn <mhorn@mbta.com>
2024-02-05 09:54:03 -0700

    feat: implement Phoenix channels in shared code

---
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/phoenix/PhoenixSocket.kt
@@ -0,0 +1,99 @@
+package com.mbta.tid.mbta_app.phoenix
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.parameter
+import io.ktor.http.appendPathSegments
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.serialization.json.JsonObject
+
+class PhoenixSocket(private var conn: WebSocketSession) {
+    private var nextJoin = 0
+    private var nextMessage = 0
+
+    private var channels = mutableMapOf<String, PhoenixChannel>()
+    private var messages = mutableMapOf<String, CompletableDeferred<RawPhoenixChannelMessage>>()
+
+    internal fun putChannel(channel: PhoenixChannel): String {
+        val joinReference = "$nextJoin"
+        nextJoin++
+        channels[joinReference] = channel
+        return joinReference
+    }
+
+    internal fun deleteChannel(joinReference: String) {
+        channels.remove(joinReference)
+    }
+
+    internal suspend fun sendAsync(
+        joinReference: String? = null,
+        topic: String,
+        event: String,
+        payload: JsonObject = JsonObject(emptyMap())
+    ): RawPhoenixChannelMessage {
+        val result = CompletableDeferred<RawPhoenixChannelMessage>()
+        val messageReference = "$nextMessage"
+        nextMessage++
+        messages[messageReference] = result
+        conn.sendSerialized(
+            RawPhoenixChannelMessage(joinReference, messageReference, topic, event, payload)
+        )
+        return result.await()
+    }
+
+    suspend fun disconnect() = conn.close()
+
+    suspend fun run(): Unit = coroutineScope {
+        val heartbeatJob = async {
+            while (isActive) {
+                delay(10.seconds)
+                sendAsync(topic = "phoenix", event = "heartbeat")
+            }
+        }
+        val receiveJob = async {
+            conn.consumeEachIncoming { message ->
+                if (message.ref != null) {
+                    messages.remove(message.ref)?.apply {
+                        complete(message)
+                        return@consumeEachIncoming
+                    }
+                }
+                if (message.joinRef != null) {
+                    channels[message.joinRef]?.apply {
+                        handle(message.event, message.payload)
+                        return@consumeEachIncoming
+                    }
+                }
+                println("Unhandled message")
+                println(message)
+            }
+        }
+        val onCancelJob = async {
+            conn.closeReason.await()
+            val ex = CancellationException("connection closed")
+            heartbeatJob.cancel(ex)
+            receiveJob.cancel(ex)
+        }
+        try {
+            awaitAll(heartbeatJob, receiveJob, onCancelJob)
+        } catch (_: CancellationException) {}
+    }
+}
+
+suspend fun HttpClient.phoenixSocket(request: HttpRequestBuilder.() -> Unit) =
+    PhoenixSocket(
+        this.webSocketSession {
+                request()
+                url { appendPathSegments("websocket") }
+                parameter("vsn", "2.0.0")
+            }
+            .wrap()
+    )
