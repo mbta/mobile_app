88db148eaa9cb4e89f3188f88d04291d7a89a92c
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-02-13 10:46:05 -0500

    perf(IosAppUITestsLaunchTests): don't run for every orientation + light/dark mode to prevent timeouts
    This test was taking 53 seconds on my machine because it really runs 8 tests. I don't think we need to assess launch time for each orientation

--- iosApp/iosAppUITests/IosAppUITestsLaunchTests.swift
+++ iosApp/iosAppUITests/IosAppUITestsLaunchTests.swift
@@ -13,10 +13,6 @@ final class IosAppUITestsLaunchTests: XCTestCase {
         executionTimeAllowance = 60
     }
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
