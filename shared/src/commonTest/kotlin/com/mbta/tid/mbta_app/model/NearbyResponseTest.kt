5a1c774eaa8435b419f1b15cc84a5b3422640f71
Melody Horn <mhorn@mbta.com>
2024-02-21 09:46:18 -0700

    test: add tests for predictions grouping

--- shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/model/NearbyResponseTest.kt
+++ shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/model/NearbyResponseTest.kt
@@ -1,6 +1,7 @@
 package com.mbta.tid.mbta_app.model
 
 import com.mbta.tid.mbta_app.TestData.pattern
+import com.mbta.tid.mbta_app.TestData.prediction
 import com.mbta.tid.mbta_app.TestData.route
 import com.mbta.tid.mbta_app.TestData.routePattern
 import com.mbta.tid.mbta_app.TestData.stop
@@ -8,6 +9,8 @@ import com.mbta.tid.mbta_app.TestData.trip
 import com.mbta.tid.mbta_app.model.response.StopAndRoutePatternResponse
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.datetime.Instant
 
 class NearbyResponseTest {
 
@@ -318,6 +321,109 @@ class NearbyResponseTest {
                 ),
             ),
             response.byRouteAndStop(predictions = null)
+        )
+    }
+
+    @Test
+    fun `byRouteAndStop includes predictions filtered to the correct stop and pattern`() {
+        val stop1 = stop()
+        val stop2 = stop()
+
+        val route1 = route()
+
+        val pattern1 =
+            route1.pattern(sortOrder = 1, representativeTrip = trip(headsign = "Harvard"))
+        val pattern2 =
+            route1.pattern(sortOrder = 2, representativeTrip = trip(headsign = "Harvard"))
+        val pattern3 = route1.pattern(sortOrder = 3, representativeTrip = trip(headsign = "Nubian"))
+
+        val response =
+            StopAndRoutePatternResponse(
+                stops = listOf(stop1, stop2),
+                routePatterns = listOf(pattern1, pattern2, pattern3).associateBy { it.id },
+                patternIdsByStop =
+                    mapOf(
+                        stop1.id to listOf(pattern1.id, pattern2.id),
+                        stop2.id to listOf(pattern1.id, pattern2.id, pattern3.id),
+                    ),
+                routes = mapOf(route1.id to route1)
+            )
+
+        val time = Instant.parse("2024-02-21T09:30:08-05:00")
+
+        // should be sorted before the pattern 1 prediction under Harvard
+        val stop1Pattern2Prediction =
+            prediction(
+                arrivalTime = time,
+                departureTime = time + 10.seconds,
+                stopId = stop1.id,
+                trip = pattern2.trip()
+            )
+
+        // should be sorted after the pattern 2 prediction under Harvard
+        val stop1Pattern1Prediction =
+            prediction(
+                arrivalTime = time + 5.seconds,
+                departureTime = time + 15.seconds,
+                stopId = stop1.id,
+                trip = pattern1.trip()
+            )
+
+        // should be ignored since pattern 1 shows at stop 1 instead
+        val stop2Pattern1Prediction =
+            prediction(
+                arrivalTime = time + 10.seconds,
+                departureTime = time + 20.seconds,
+                stopId = stop2.id,
+                trip = pattern1.trip()
+            )
+
+        // should be shown under Nubian
+        val stop2Pattern3Prediction =
+            prediction(
+                arrivalTime = time + 20.seconds,
+                departureTime = time + 30.seconds,
+                stopId = stop2.id,
+                trip = pattern3.trip()
+            )
+
+        assertEquals(
+            listOf(
+                StopAssociatedRoute(
+                    route1,
+                    listOf(
+                        PatternsByStop(
+                            stop1,
+                            listOf(
+                                PatternsByHeadsign(
+                                    "Harvard",
+                                    listOf(pattern1, pattern2),
+                                    listOf(stop1Pattern2Prediction, stop1Pattern1Prediction)
+                                )
+                            )
+                        ),
+                        PatternsByStop(
+                            stop2,
+                            listOf(
+                                PatternsByHeadsign(
+                                    "Nubian",
+                                    listOf(pattern3),
+                                    listOf(stop2Pattern3Prediction)
+                                )
+                            )
+                        )
+                    )
+                ),
+            ),
+            response.byRouteAndStop(
+                predictions =
+                    listOf(
+                        stop1Pattern1Prediction,
+                        stop1Pattern2Prediction,
+                        stop2Pattern1Prediction,
+                        stop2Pattern3Prediction
+                    )
+            )
         )
     }
 }
