1c393aa4827a498fe15c463ea979fa07d07f7b2b
Melody Horn <mhorn@mbta.com>
2024-03-01 15:42:17 -0700

    clarify purpose of BackendObject

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/BackendObject.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/BackendObject.kt
@@ -1,5 +1,10 @@
 package com.mbta.tid.mbta_app.model
 
+/**
+ * An object retrieved from the backend and originating in the V3 API.
+ *
+ * Useful in the [ObjectCollectionBuilder] for maintaining maps from IDs to objects.
+ */
 interface BackendObject {
     val id: String
 }
