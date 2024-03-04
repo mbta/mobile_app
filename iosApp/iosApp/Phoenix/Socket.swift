8edc9c2f05cdbe8349ed06753a3153838d2bfe91
Kayla Brady <31781298+KaylaBrady@users.noreply.github.com>
2024-03-04 15:57:00 -0500

    feat(ios): Decoder for swift phoenix messages (#62)

---
+++ iosApp/iosApp/Phoenix/Socket.swift
@@ -0,0 +1,92 @@
+//
+//  Socket.swift
+//  iosApp
+//
+//  Created by Brady, Kayla on 3/4/24.
+//  Copyright © 2024 MBTA. All rights reserved.
+//
+
+import Foundation
+import os
+import SwiftPhoenixClient
+
+/*
+ Return the decoded message in the expected [Any?] format:
+ [joinReference, messageReference, topic, eventName, eventPayload]
+ where eventPayload has type
+ ["jsonPayload": String, "payload": Any?] or
+ ["response": ["jsonPayload": String, "payload": Any?], "status": String]
+ "jsonPayload" is the raw payload string, and "payload" contains the decoded json payload object.
+ */
+func decodeWithRawMessage(data: Data) -> Any? {
+    let messageParts = decodeMessageParts(data: data)
+
+    guard let data = messageParts.rawEventString?.data(using: String.Encoding.utf8),
+          let eventPayload = Defaults.decode(data)
+    else {
+        Logger().warning("unable to parse raw payload \(messageParts.rawEventString ?? "")")
+        return nil
+    }
+
+    var event: [String: Any?] = [:]
+
+    if let eventPayloadMap = eventPayload as? [String: Any?], isResponseEvent(payload: eventPayloadMap) {
+        let existingResponse = eventPayloadMap["response"]!
+        event = eventPayloadMap
+        // Include the jsonPayload on the response
+        event["response"] = ["jsonPayload": String(data: Defaults.encode(existingResponse!),
+                                                   encoding: String.Encoding.utf8), "payload": existingResponse]
+    } else {
+        event = ["jsonPayload": messageParts.rawEventString, "payload": eventPayload]
+    }
+
+    return [messageParts.joinRef as Any, messageParts.messageRef as Any, messageParts.topic, messageParts.eventName, event]
+}
+
+struct DecodedMessageParts {
+    var joinRef: String?
+    var messageRef: String?
+    var topic: String
+    var eventName: String
+    var rawEventString: String?
+}
+
+private func decodeMessageParts(data: Data) -> DecodedMessageParts {
+    // Parsing based on
+    // https://github.com/dsrees/JavaPhoenixClient/blob/master/src/main/kotlin/org/phoenixframework/Defaults.kt#L68
+    var rawStringMessage = String(data: data, encoding: .utf8)!
+
+    rawStringMessage.removeFirst(1) // remove '['
+    let joinRef = String(rawStringMessage.prefix(while: { $0 != "," })) // take join ref, 'null' or '5'
+
+    rawStringMessage.removeFirst(joinRef.count) // remove join ref
+    rawStringMessage.removeFirst(1) // remove ','
+
+    let messageRef = String(rawStringMessage.prefix(while: { $0 != "," })) // take ref, 'null' or '5'
+    rawStringMessage.removeFirst(messageRef.count) // remove ref
+    rawStringMessage.removeFirst(2) // remove ',"'
+
+    let topic = String(rawStringMessage.prefix(while: { $0 != "\"" }))
+    rawStringMessage.removeFirst(topic.count)
+    rawStringMessage.removeFirst(3) // remove '","'
+
+    let eventName = String(rawStringMessage.prefix(while: { $0 != "\"" }))
+    rawStringMessage.removeFirst(eventName.count)
+    rawStringMessage.removeFirst(2) // remove '",'
+
+    let rawEventString = String(rawStringMessage.dropLast()) // remove ]
+
+    return .init(joinRef: parseValue(str: joinRef), messageRef: parseValue(str: messageRef), topic: topic, eventName: eventName, rawEventString: rawEventString)
+}
+
+private func parseValue(str: String) -> String? {
+    if str == "null" {
+        nil
+    } else {
+        str.replacingOccurrences(of: "\"", with: "")
+    }
+}
+
+private func isResponseEvent(payload: [String: Any?]) -> Bool {
+    payload["response"] != nil
+}
---
+++ iosApp/iosAppTests/Phoenix/MessageTests.swift
@@ -0,0 +1,30 @@
+//
+//  MessageTests.swift
+//  iosAppTests
+//
+//  Created by Brady, Kayla on 3/4/24.
+//  Copyright © 2024 MBTA. All rights reserved.
+//
+
+import Foundation
+@testable import iosApp
+@testable import SwiftPhoenixClient
+import XCTest
+
+final class MessageTest: XCTestCase {
+    override func setUp() {
+        executionTimeAllowance = 60
+    }
+
+    func testDecodesJsonPayload() {
+        let message = Message(ref: "ref", topic: "topic", event: "event", payload: ["jsonPayload": "{\"user_id\":\"abc123\"}", "payload": ["user_id": "abc123"]], joinRef: "join_ref")
+
+        XCTAssertEqual(message.jsonPayload(), "{\"user_id\":\"abc123\"}")
+    }
+
+    func testDecodesJsonPayloadNestedInResponse() {
+        let message = Message(ref: "ref", topic: "topic", event: "event", payload: ["response": ["jsonPayload": "{\"user_id\":\"abc123\"}", "payload": ["user_id": "abc123"]], "status": "ok"], joinRef: "join_ref")
+
+        XCTAssertEqual(message.jsonPayload(), "{\"user_id\":\"abc123\"}")
+    }
+}
