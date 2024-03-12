d967fb9114fba7d6a2d5de161aeb0a293e5a4a0a
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-03-12 15:49:17 -0400

    fix(BackendProtocol): remove runSocket

--- iosApp/iosApp/Fetchers/Backend.swift
+++ iosApp/iosApp/Fetchers/Backend.swift
@@ -10,7 +10,6 @@ import Foundation
 import shared
 
 protocol BackendProtocol {
-    func runSocket() async throws
     func getGlobalData() async throws -> StopAndRoutePatternResponse
     func getNearby(latitude: Double, longitude: Double) async throws -> StopAndRoutePatternResponse
     func getSearchResults(query: String) async throws -> SearchResponse
@@ -24,10 +23,6 @@ struct IdleBackend: BackendProtocol {
         while true {
             try? await Task.sleep(nanoseconds: .max)
         }
-    }
-
-    func runSocket() async throws {
-        await hang()
     }
 
     func getGlobalData() async throws -> StopAndRoutePatternResponse {
