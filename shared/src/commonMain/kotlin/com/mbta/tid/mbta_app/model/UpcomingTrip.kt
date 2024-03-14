890632e0fea8aaa4efeeeab4c314f692cca03471
Melody Horn <mhorn@mbta.com>
2024-03-14 09:46:02 -0600

    add documentation for UpcomingTrip

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/UpcomingTrip.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/UpcomingTrip.kt
@@ -5,10 +5,21 @@ import kotlin.math.roundToInt
 import kotlin.time.DurationUnit
 import kotlinx.datetime.Instant
 
+/**
+ * Conceptually, an [UpcomingTrip] represents a trip's future arrival and departure at a stop.
+ *
+ * The time may not be in the future, depending on when stale predictions are removed and how long
+ * ago the schedule was fetched. The trip may not arrive at a stop if that stop is the first and may
+ * not depart from a stop if that stop is the last.
+ *
+ * The trip might also neither arrive nor depart if the stop is skipped or the trip is dropped. For
+ * this reason, a prediction that exists but has null times should overwrite scheduled times.
+ */
 data class UpcomingTrip
 @DefaultArgumentInterop.Enabled
 constructor(val prediction: Prediction, val vehicle: Vehicle? = null) : Comparable<UpcomingTrip> {
     val time = prediction.predictionTime
+    /** The [Prediction.tripId] of the [prediction]. */
     val id = checkNotNull(prediction.tripId)
 
     override fun compareTo(other: UpcomingTrip) = nullsLast<Instant>().compare(time, other.time)
