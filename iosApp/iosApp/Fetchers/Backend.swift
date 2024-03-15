1ae2ea9eace7fc7ceeeca9ed91a12c80863320c1
Kayla Brady <31781298+KaylaBrady@users.noreply.github.com>
2024-03-15 16:02:42 -0400

    cleanup: remove unused phoenix channel / socket code (#66)
    * cleanup: remove unused phoenix channel / socket code
    
    * fix(BackendProtocol): remove runSocket

--- iosApp/iosApp/Fetchers/Backend.swift
+++ iosApp/iosApp/Fetchers/Backend.swift
@@ -10,13 +10,11 @@ import Foundation
 import shared
 
 protocol BackendProtocol {
-    func runSocket() async throws
     func getGlobalData() async throws -> StopAndRoutePatternResponse
     func getNearby(latitude: Double, longitude: Double) async throws -> StopAndRoutePatternResponse
     func getSchedule(stopIds: [String]) async throws -> ScheduleResponse
     func getSearchResults(query: String) async throws -> SearchResponse
     func getRailRouteShapes() async throws -> RouteResponse
-    func predictionsStopsChannel(stopIds: [String]) async throws -> PredictionsStopsChannel
 }
 
 extension Backend: BackendProtocol {}
@@ -26,10 +24,6 @@ struct IdleBackend: BackendProtocol {
         while true {
             try? await Task.sleep(nanoseconds: .max)
         }
-    }
-
-    func runSocket() async throws {
-        await hang()
     }
 
     func getGlobalData() async throws -> StopAndRoutePatternResponse {
@@ -49,10 +43,6 @@ struct IdleBackend: BackendProtocol {
     }
 
     func getSearchResults(query _: String) async throws -> SearchResponse {
-        await hang()
-    }
-
-    func predictionsStopsChannel(stopIds _: [String]) async throws -> PredictionsStopsChannel {
         await hang()
     }
 }
--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/Backend.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/Backend.kt
@@ -5,13 +5,9 @@ import com.mbta.tid.mbta_app.model.response.RouteResponse
 import com.mbta.tid.mbta_app.model.response.ScheduleResponse
 import com.mbta.tid.mbta_app.model.response.SearchResponse
 import com.mbta.tid.mbta_app.model.response.StopAndRoutePatternResponse
-import com.mbta.tid.mbta_app.phoenix.PhoenixSocket
-import com.mbta.tid.mbta_app.phoenix.phoenixSocket
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.network.sockets.ConnectTimeoutException
-import io.ktor.client.network.sockets.SocketTimeoutException
 import io.ktor.client.plugins.HttpRequestTimeoutException
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.ResponseException
@@ -22,15 +18,12 @@ import io.ktor.client.plugins.expectSuccess
 import io.ktor.client.plugins.timeout
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.request.get
-import io.ktor.client.request.url
 import io.ktor.http.path
 import io.ktor.serialization.JsonConvertException
 import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.utils.io.errors.IOException
 import kotlin.coroutines.cancellation.CancellationException
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
@@ -49,33 +42,6 @@ class Backend(engine: HttpClientEngine) {
             install(HttpTimeout) { requestTimeoutMillis = 5000 }
             defaultRequest { url("https://$mobileBackendHost") }
         }
-
-    // mutex to avoid creating a second socket while the first one is still connecting
-    private var socketMutex = Mutex()
-    private var _socket: PhoenixSocket? = null
-
-    private suspend fun socket(): PhoenixSocket =
-        socketMutex.withLock {
-            when (val socket = _socket) {
-                null -> {
-                    val newSocket =
-                        httpClient.phoenixSocket { url("wss://$mobileBackendHost/socket") }
-                    _socket = newSocket
-                    newSocket
-                }
-                else -> socket
-            }
-        }
-
-    @Throws(
-        IOException::class,
-        CancellationException::class,
-        JsonConvertException::class,
-        ResponseException::class,
-        SocketTimeoutException::class,
-        ConnectTimeoutException::class
-    )
-    suspend fun runSocket() = socket().run()
 
     @Throws(
         IOException::class,
@@ -165,15 +131,4 @@ class Backend(engine: HttpClientEngine) {
                 }
             }
             .body()
-
-    @Throws(
-        IOException::class,
-        CancellationException::class,
-        JsonConvertException::class,
-        ResponseException::class,
-        SocketTimeoutException::class,
-        ConnectTimeoutException::class
-    )
-    suspend fun predictionsStopsChannel(stopIds: List<String>) =
-        PredictionsStopsChannel(socket(), stopIds)
 }
