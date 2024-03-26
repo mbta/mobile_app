a44cd55325414416dd257ceb3ba152c2b75e9648
Melody Horn <mhorn@mbta.com>
2024-03-26 09:20:12 -0600

    refactor: move headsign predictions selection to Kotlin (#102)

--- iosApp/iosApp/Pages/NearbyTransit/NearbyStopRoutePatternView.swift
+++ iosApp/iosApp/Pages/NearbyTransit/NearbyStopRoutePatternView.swift
@@ -11,57 +11,19 @@ import SwiftUI
 
 struct NearbyStopRoutePatternView: View {
     let headsign: String
-    let predictions: PredictionState
-
-    struct TripWithFormat: Identifiable {
-        let trip: UpcomingTrip
-        let format: UpcomingTrip.Format
-
-        var id: String { trip.id }
-
-        init(_ trip: UpcomingTrip, now: Instant) {
-            self.trip = trip
-            format = trip.format(now: now)
-        }
-
-        func isHidden() -> Bool {
-            format is UpcomingTrip.FormatHidden
-        }
-    }
-
-    enum PredictionState {
-        case loading
-        case none
-        case some([TripWithFormat])
-        case noService(shared.Alert)
-
-        static func from(upcomingTrips: [UpcomingTrip]?, alertsHere: [shared.Alert]?, now: Instant) -> Self {
-            guard let upcomingTrips else { return .loading }
-            let tripsToShow = upcomingTrips
-                .map { TripWithFormat($0, now: now) }
-                .filter { !$0.isHidden() }
-                .prefix(2)
-            if tripsToShow.isEmpty {
-                if let alert = alertsHere?.first {
-                    return .noService(alert)
-                }
-                return .none
-            }
-            return .some(Array(tripsToShow))
-        }
-    }
+    let predictions: PatternsByHeadsign.Format
 
     var body: some View {
         HStack {
             Text(headsign)
             Spacer()
-            switch predictions {
-            case let .some(predictions):
-                ForEach(predictions) { prediction in
+            switch onEnum(of: predictions) {
+            case let .some(trips):
+                ForEach(trips.trips, id: \.id) { prediction in
                     UpcomingTripView(prediction: .some(prediction.format))
                 }
             case let .noService(alert):
-                UpcomingTripView(prediction: .noService(alert.effect))
+                UpcomingTripView(prediction: .noService(alert.alert.effect))
             case .none:
                 UpcomingTripView(prediction: .none)
             case .loading:
@@ -75,14 +37,14 @@ struct NearbyStopRoutePatternView_Previews: PreviewProvider {
     static var previews: some View {
         VStack(alignment: .trailing) {
             let now = Date.now
-            NearbyStopRoutePatternView(headsign: "Some", predictions: .some([
-                .init(.init(prediction: ObjectCollectionBuilder.Single.shared.prediction { prediction in
+            NearbyStopRoutePatternView(headsign: "Some", predictions: PatternsByHeadsign.FormatSome(trips: [
+                .init(trip: .init(prediction: ObjectCollectionBuilder.Single.shared.prediction { prediction in
                     prediction.departureTime = now.addingTimeInterval(5 * 60).toKotlinInstant()
                 }), now: now.toKotlinInstant()),
             ]))
-            NearbyStopRoutePatternView(headsign: "None", predictions: .none)
-            NearbyStopRoutePatternView(headsign: "Loading", predictions: .loading)
-            NearbyStopRoutePatternView(headsign: "No Service", predictions: .noService(
+            NearbyStopRoutePatternView(headsign: "None", predictions: PatternsByHeadsign.FormatNone.shared)
+            NearbyStopRoutePatternView(headsign: "Loading", predictions: PatternsByHeadsign.FormatLoading.shared)
+            NearbyStopRoutePatternView(headsign: "No Service", predictions: PatternsByHeadsign.FormatNoService(alert:
                 ObjectCollectionBuilder.Single.shared.alert { alert in
                     alert.effect = .suspension
                 }
