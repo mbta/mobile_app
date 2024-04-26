b4bf9908772d865c0771ad8ee1846a0a4986f469
Melody Horn <mhorn@mbta.com>
2024-04-26 10:06:41 -0600

    feat(Android): show routes, stops, alerts on map (#146)
    * feat(Android): show routes, stops, alerts on map
    
    * fix resolveParent call from Swift

--- iosApp/iosApp/Utils/StopExtension.swift
+++ iosApp/iosApp/Utils/StopExtension.swift
@@ -13,11 +13,4 @@ extension Stop {
     var coordinate: CLLocationCoordinate2D {
         .init(latitude: latitude, longitude: longitude)
     }
-
-    // TODO: This should be moved into a stop caching layer once it exists
-    func resolveParent(_ stopMap: [String: Stop]) -> Stop? {
-        guard let parentStationId else { return self }
-        guard let parent = stopMap[parentStationId] else { return nil }
-        return parent.resolveParent(stopMap)
-    }
 }
