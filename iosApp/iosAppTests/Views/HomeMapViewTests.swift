0749f3bc89b09a5d1cfd790c8b3eaa0f97639bb8
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-04-29 15:24:10 -0400

    test(HomeMapView): Add test for selected stop changing

--- iosApp/iosAppTests/Views/HomeMapViewTests.swift
+++ iosApp/iosAppTests/Views/HomeMapViewTests.swift
@@ -10,6 +10,7 @@
 import ViewInspector
 @_spi(Experimental) import MapboxMaps
 import shared
+import SwiftUI
 import XCTest
 
 final class HomeMapViewTests: XCTestCase {
@@ -140,6 +141,38 @@ final class HomeMapViewTests: XCTestCase {
         let hasAppeared = sut.on(\.didAppear) { _ in }
         ViewHosting.host(view: sut)
         wait(for: [hasAppeared], timeout: 1)
+        XCTAssertEqual(stop.coordinate, sut.viewportProvider.viewport.camera!.center)
+    }
+
+    func testCentersOnStopWhenNewSelectedStop() throws {
+        let objectCollection = ObjectCollectionBuilder()
+        let stop = objectCollection.stop { stop in
+            stop.id = "1"
+            stop.latitude = 1
+            stop.longitude = 1
+        }
+        let alertsFetcher: AlertsFetcher = .init(socket: MockSocket())
+        let globalFetcher: GlobalFetcher = .init(backend: IdleBackend(), stops: [stop.id: stop], routes: [:])
+        let nearbyFetcher: NearbyFetcher = .init(backend: IdleBackend())
+        let railRouteShapeFetcher: RailRouteShapeFetcher = .init(backend: IdleBackend())
+        let locationDataManager: LocationDataManager = .init(locationFetcher: MockLocationFetcher())
+        var sut = HomeMapView(
+            alertsFetcher: alertsFetcher,
+            globalFetcher: globalFetcher,
+            nearbyFetcher: nearbyFetcher,
+            railRouteShapeFetcher: railRouteShapeFetcher,
+            viewportProvider: ViewportProvider(),
+            locationDataManager: locationDataManager,
+            navigationStack: .constant([]),
+            sheetHeight: .constant(0)
+        )
+
+        let hasAppeared = sut.on(\.didAppear) { _ in }
+        ViewHosting.host(view: sut)
+        wait(for: [hasAppeared], timeout: 1)
+        XCTAssertEqual(ViewportProvider.defaultCenter, sut.viewportProvider.viewport.camera!.center)
+
+        try sut.inspect().find(Map.self).callOnChange(newValue: stop)
         XCTAssertEqual(stop.coordinate, sut.viewportProvider.viewport.camera!.center)
     }
 }
