3cb8edc34019f2c32abf4277861511c3e596df3a
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-04-29 09:22:23 -0400

    doc(IOSApp): doc dummy app condition

--- iosApp/iosApp/IOSApp.swift
+++ iosApp/iosApp/IOSApp.swift
@@ -5,10 +5,13 @@ import SwiftUI
 
 @main
 struct IOSApp: App {
+    // When running unit tests or previews, don't mount the entire app which makes real API requests.
+    private let isTestOrPreview = ProcessInfo.processInfo.arguments.contains("--dummy-test-app")
+        || ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil
+
     var body: some Scene {
         WindowGroup {
-            if ProcessInfo.processInfo.arguments.contains("--dummy-test-app")
-                || ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil {
+            if isTestOrPreview {
                 DummyTestAppView()
             } else {
                 ProductionAppView()
