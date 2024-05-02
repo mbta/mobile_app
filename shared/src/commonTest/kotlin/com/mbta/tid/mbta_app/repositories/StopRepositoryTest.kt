e67aad1bc4ef674d5a80b0a9c07f216986c07ebe
Emma Simon <esimon@mbta.com>
2024-05-02 12:59:44 -0400

    test: Add test for StopRepository

---
+++ shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/repositories/StopRepositoryTest.kt
@@ -0,0 +1,141 @@
+package com.mbta.tid.mbta_app
+
+import com.mbta.tid.mbta_app.model.LocationType
+import com.mbta.tid.mbta_app.model.RouteSegment
+import com.mbta.tid.mbta_app.model.SegmentedRouteShape
+import com.mbta.tid.mbta_app.model.Shape
+import com.mbta.tid.mbta_app.model.Stop
+import com.mbta.tid.mbta_app.model.response.MapFriendlyRouteResponse
+import com.mbta.tid.mbta_app.model.response.StopMapResponse
+import com.mbta.tid.mbta_app.network.MobileBackendClient
+import com.mbta.tid.mbta_app.repositories.StopRepository
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.utils.io.ByteReadChannel
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+
+class StopRepositoryTest : KoinTest {
+    @Test
+    fun testGetStopData() {
+        val mockEngine = MockEngine { _ ->
+            respond(
+                content =
+                    ByteReadChannel(
+                        """
+                    {
+                      "map_friendly_route_shapes": [
+                        {
+                          "route_id": "Blue",
+                          "route_shapes": [
+                            {
+                              "source_route_pattern_id": "Blue-6-0",
+                              "source_route_id": "Blue",
+                              "direction_id": 0,
+                              "route_segments": [
+                                {
+                                  "id": "place-wondl-place-bomnl",
+                                  "source_route_id": "Blue",
+                                  "stop_ids": [
+                                    "place-wondl",
+                                    "place-bomnl"
+                                  ],
+                                  "other_patterns_by_stop_id": {},
+                                  "source_route_pattern_id": "Blue-6-0"
+                                }
+                              ],
+                              "shape": {
+                                "id": "canonical-946_0013",
+                                "polyline": "s|zaG~phpLpBwO"
+                              }
+                            }
+                          ]
+                        }
+                      ],
+                      "child_stops": {
+                        "70060": {
+                          "id": "70060",
+                          "name": "Wonderland",
+                          "description": "Wonderland - Blue Line - Exit Only",
+                          "location_type": "stop",
+                          "latitude": 42.413361,
+                          "longitude": -70.991685,
+                          "vehicle_type": "heavy_rail",
+                          "platform_name": "Exit Only",
+                          "parent_station_id": "place-wondl"
+                        }
+                      }
+                    }
+                    """
+                            .trimIndent()
+                    ),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+
+        startKoin {
+            modules(module { single { MobileBackendClient(mockEngine, AppVariant.Staging) } })
+        }
+        runBlocking {
+            val response = StopRepository().getStopMapData(stopId = "place-wondl")
+            assertEquals(
+                StopMapResponse(
+                    routeShapes =
+                        listOf(
+                            MapFriendlyRouteResponse.RouteWithSegmentedShapes(
+                                routeId = "Blue",
+                                segmentedShapes =
+                                    listOf(
+                                        SegmentedRouteShape(
+                                            sourceRoutePatternId = "Blue-6-0",
+                                            sourceRouteId = "Blue",
+                                            routeSegments =
+                                                listOf(
+                                                    RouteSegment(
+                                                        id = "place-wondl-place-bomnl",
+                                                        sourceRouteId = "Blue",
+                                                        stopIds =
+                                                            listOf("place-wondl", "place-bomnl"),
+                                                        otherPatternsByStopId = mapOf(),
+                                                        sourceRoutePatternId = "Blue-6-0"
+                                                    )
+                                                ),
+                                            shape =
+                                                Shape(
+                                                    id = "canonical-946_0013",
+                                                    polyline = "s|zaG~phpLpBwO"
+                                                )
+                                        )
+                                    )
+                            )
+                        ),
+                    childStops =
+                        mapOf(
+                            Pair(
+                                "70060",
+                                Stop(
+                                    id = "70060",
+                                    name = "Wonderland",
+                                    description = "Wonderland - Blue Line - Exit Only",
+                                    locationType = LocationType.STOP,
+                                    latitude = 42.413361,
+                                    longitude = -70.991685,
+                                    platformName = "Exit Only",
+                                    parentStationId = "place-wondl"
+                                )
+                            )
+                        )
+                ),
+                response
+            )
+        }
+    }
+}
