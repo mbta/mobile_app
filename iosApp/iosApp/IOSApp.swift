e5a1e3d05959fa0c604932011aa125ad6afa1722
Emma Simon <esimon@mbta.com>
2024-05-06 12:17:31 -0400

    feat: Introduce Google Analytics (#165)
    * feat: Add Google Firebase for analytics collection
    
    * ci: Handle loading the Firebase API key in CD
    
    * fix: Decrement Package.resolved version
    
    This is annoying, we should figure out how to stop cocoapods doing this.
    
    * fix: PR feedback
    
    * fix: Actually remove originHash

--- iosApp/iosApp/IOSApp.swift
+++ iosApp/iosApp/IOSApp.swift
@@ -1,10 +1,24 @@
+import FirebaseCore
 import os
 import shared
 import SwiftPhoenixClient
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct IOSApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     // When running unit tests or previews, don't mount the entire app which makes real API requests.
     private let isTestOrPreview = ProcessInfo.processInfo.arguments.contains("--dummy-test-app")
         || ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != nil
