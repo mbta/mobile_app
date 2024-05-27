85199e49b2aadcb3a9afe41b0cf31c2197e56ddd
Emma Simon <esimon@mbta.com>
2024-05-27 17:40:14 -0400

    feat: Resize stop icons by mode when zooming
    Bus stops shrink to remain legible, commuter rail stops increase in size
    at very wide zoom levels, and subway/SL increase their size mid-wide.

--- iosApp/iosApp/Pages/Map/StopLayerGenerator.swift
+++ iosApp/iosApp/Pages/Map/StopLayerGenerator.swift
@@ -13,21 +13,33 @@ import SwiftUI
 class StopLayerGenerator {
     let stopLayers: [SymbolLayer] = createStopLayers()
 
-    static let stopZoomThreshold: Double = 13.0
-    static let closeZoomThreshold: Double = 16.0
+    static let stopZoomThreshold = 8.0
+    static let midZoomThreshold = 11.0
+    static let closeZoomThreshold = 16.0
 
     static let stopLayerId = "stop-layer"
     static func getTransferLayerId(_ index: Int) -> String {
         "\(stopLayerId)-transfer-\(index.description)"
     }
 
-    static let selectedSizeExpression: Expression = Exp(.switchCase) {
-        Exp(.eq) {
-            Exp(.get) { StopSourceGenerator.propIsSelectedKey }
-            true
+    static let routesExp = Exp(.get) { StopSourceGenerator.propMapRoutesKey }
+
+    static let selectedExp = Exp(.boolean) { Exp(.get) { StopSourceGenerator.propIsSelectedKey } }
+    static let selectedSizeExp: Expression =
+        Exp(.interpolate) {
+            Exp(.exponential) { 2 }
+            Exp(.zoom)
+            midZoomThreshold; withMultipliers(0.2, modeResize: [0.5, 3, 2])
+            12; withMultipliers(0.5, modeResize: [0.75, 2, 2])
+            14; withMultipliers(1)
         }
-        1.25
-        1
+
+    static let topRouteExp = Exp(.string) {
+        Exp(.switchCase) {
+            Exp(.eq) { Exp(.length) { routesExp }; 0 }
+            ""
+            Exp(.at) { 0; routesExp }
+        }
     }
 
     static func createStopLayers() -> [SymbolLayer] {
@@ -39,7 +51,7 @@ class StopLayerGenerator {
         let transferLayers = (0 ..< 3).map { index in
             var transferLayer = SymbolLayer(id: Self.getTransferLayerId(index), source: sourceId)
             transferLayer.iconImage = StopIcons.getTransferLayerIcon(index)
-            transferLayer.iconOffset = getTransferOffsetValue(index: index)
+            transferLayer.iconOffset = transferOffsetValue(index: index)
             includeSharedProps(on: &transferLayer)
 
             return transferLayer
@@ -48,7 +60,28 @@ class StopLayerGenerator {
         return [stopLayer] + transferLayers
     }
 
-    static func getTransferOffsetExpression(closeZoom: Bool, _ index: Int) -> Expression {
+    static func includeSharedProps(on layer: inout SymbolLayer) {
+        layer.iconSize = .expression(selectedSizeExp)
+
+        layer.iconAllowOverlap = .constant(true)
+        layer.iconOpacity = .constant(0)
+        layer.iconOpacityTransition = StyleTransition(duration: 1, delay: 0)
+        layer.minZoom = stopZoomThreshold - 1
+        layer.symbolSortKey = .expression(Exp(.get) { StopSourceGenerator.propSortOrderKey })
+        layer.textAllowOverlap = .constant(true)
+    }
+
+    static func modeSizeMultiplierExp(resizeWith: [Double]) -> Expression {
+        Exp(.switchCase) {
+            Exp(.eq) { topRouteExp; MapStopRoute.bus.name }
+            resizeWith[0]
+            Exp(.eq) { topRouteExp; MapStopRoute.commuter.name }
+            resizeWith[1]
+            resizeWith[2]
+        }
+    }
+
+    static func transferOffsetExp(closeZoom: Bool, _ index: Int) -> Expression {
         let doubleRouteOffset: Double = closeZoom ? 13 : 8
         let tripleRouteOffset: Double = closeZoom ? 26 : 16
         return Exp(.step) {
@@ -61,27 +94,25 @@ class StopLayerGenerator {
         }
     }
 
-    static func getTransferOffsetValue(index: Int) -> Value<[Double]> {
+    static func transferOffsetValue(index: Int) -> Value<[Double]> {
         .expression(Exp(.step) {
             Exp(.zoom)
-            getTransferOffsetExpression(closeZoom: false, index)
+            transferOffsetExp(closeZoom: false, index)
             closeZoomThreshold
-            getTransferOffsetExpression(closeZoom: true, index)
+            transferOffsetExp(closeZoom: true, index)
         })
     }
 
-    static func includeSharedProps(on layer: inout SymbolLayer) {
-        // TODO: We actually want to give the icon a halo, but that is only supported for SDFs,
-        // which can only be one color.
-        // Alternates of stop icon SVGs with halo applied?
-        layer.iconSize = .expression(selectedSizeExpression)
-
-        layer.iconAllowOverlap = .constant(true)
-        layer.iconOpacity = .constant(0)
-        layer.iconOpacityTransition = StyleTransition(duration: 1, delay: 0)
-        layer.minZoom = stopZoomThreshold - 1
-        layer.symbolSortKey = .expression(Exp(.get) { StopSourceGenerator.propSortOrderKey })
-        layer.textAllowOverlap = .constant(true)
+    // The modeResize array must contain 3 entries for [BUS, COMMUTER, fallback]
+    static func withMultipliers(_ base: Double, modeResize: [Double] = [1, 1, 1]) -> Expression {
+        Exp(.product) {
+            base
+            modeSizeMultiplierExp(resizeWith: modeResize)
+            // TODO: We actually want to give the icon a halo rather than resize,
+            // but that is only supported for SDFs, which can only be one color.
+            // Alternates of stop icon SVGs with halo applied?
+            Exp(.switchCase) { selectedExp; 1.25; 1 }
+        }
     }
 
     static func xyExp(_ pair: [Double]) -> Expression {
