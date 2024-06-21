09936085f7c243c8288d65781c3531579b5e5d38
Melody Horn <mhorn@mbta.com>
2024-06-21 10:12:58 -0600

    feat: use prod backend for prod app variant (#242)

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/AppVariant.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/AppVariant.kt
@@ -2,8 +2,7 @@ package com.mbta.tid.mbta_app
 
 enum class AppVariant(val backendHost: String) {
     Staging("mobile-app-backend-staging.mbtace.com"),
-    // TODO use prod backend once created
-    Prod("mobile-app-backend-staging.mbtace.com");
+    Prod("mobile-app-backend.mbtace.com");
 
     val backendRoot = "https://$backendHost"
     val socketUrl = "wss://$backendHost/socket"
