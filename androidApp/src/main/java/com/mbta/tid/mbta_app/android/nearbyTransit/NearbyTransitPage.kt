861d7de0ea110e27e7297f1435286e239b976683
Melody Horn <mhorn@mbta.com>
2024-06-28 14:06:43 -0600

    refactor: use clean architecture on Android

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/nearbyTransit/NearbyTransitPage.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/nearbyTransit/NearbyTransitPage.kt
@@ -4,54 +4,59 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.mbta.tid.mbta_app.Backend
 import com.mbta.tid.mbta_app.android.fetcher.GlobalData
-import com.mbta.tid.mbta_app.android.fetcher.getSchedules
-import com.mbta.tid.mbta_app.android.fetcher.subscribeToPredictions
 import com.mbta.tid.mbta_app.android.util.timer
+import com.mbta.tid.mbta_app.model.Coordinate
 import com.mbta.tid.mbta_app.model.NearbyStaticData
 import com.mbta.tid.mbta_app.model.StopsAssociated
 import com.mbta.tid.mbta_app.model.response.AlertsStreamDataResponse
-import com.mbta.tid.mbta_app.model.response.NearbyResponse
+import com.mbta.tid.mbta_app.model.response.PredictionsStreamDataResponse
+import com.mbta.tid.mbta_app.model.response.ScheduleResponse
 import com.mbta.tid.mbta_app.model.withRealtimeInfo
+import com.mbta.tid.mbta_app.repositories.INearbyRepository
+import com.mbta.tid.mbta_app.repositories.IPredictionsRepository
+import com.mbta.tid.mbta_app.repositories.ISchedulesRepository
 import io.github.dellisd.spatialk.geojson.Position
 import kotlin.time.Duration.Companion.seconds
-import org.phoenixframework.Socket
+import org.koin.compose.koinInject
 
 @Composable
 fun NearbyTransitPage(
     modifier: Modifier = Modifier,
-    backend: Backend,
-    socket: Socket,
     alertData: AlertsStreamDataResponse?,
     globalData: GlobalData,
     targetLocation: Position,
     setLastLocation: (Position) -> Unit,
+    nearbyRepository: INearbyRepository = koinInject(),
+    predictionsRepository: IPredictionsRepository = koinInject(),
+    schedulesRepository: ISchedulesRepository = koinInject()
 ) {
-    var nearby by remember { mutableStateOf<NearbyResponse?>(null) }
-    val nearbyByRouteAndStop =
-        remember(globalData, nearby) {
-            val globalResponse = globalData.response
-            val nearbyData = nearby
-            if (globalResponse != null && nearbyData != null) {
-                NearbyStaticData(globalResponse, nearbyData)
-            } else {
-                null
-            }
-        }
-    val stopIds = remember(nearby) { nearby?.stopIds }
+    var nearby: NearbyStaticData? by remember { mutableStateOf(null) }
+    val stopIds = remember(nearby) { nearby?.stopIds()?.toList() }
     val now = timer(updateInterval = 5.seconds)
-    val schedules = getSchedules(backend, stopIds, now)
-    val predictions = subscribeToPredictions(socket = socket, stopIds = stopIds)
+    var schedules: ScheduleResponse? by remember { mutableStateOf(null) }
+    LaunchedEffect(stopIds, now) {
+        if (stopIds != null) {
+            schedules = schedulesRepository.getSchedule(stopIds, now)
+        }
+    }
+    var predictions: PredictionsStreamDataResponse? by remember { mutableStateOf(null) }
+    DisposableEffect(stopIds) {
+        if (stopIds != null) {
+            predictionsRepository.connect(stopIds) { predictions = it.data }
+        }
+        onDispose { predictionsRepository.disconnect() }
+    }
     val nearbyWithRealtimeInfo =
-        remember(nearbyByRouteAndStop, targetLocation, schedules, predictions, alertData, now) {
-            nearbyByRouteAndStop?.withRealtimeInfo(
+        remember(nearby, targetLocation, schedules, predictions, alertData, now) {
+            nearby?.withRealtimeInfo(
                 sortByDistanceFrom = targetLocation,
                 schedules,
                 predictions,
@@ -62,12 +67,18 @@ fun NearbyTransitPage(
         }
 
     LaunchedEffect(targetLocation) {
-        nearby =
-            backend.getNearby(
-                latitude = targetLocation.latitude,
-                longitude = targetLocation.longitude
-            )
-        setLastLocation(targetLocation)
+        if (globalData.response != null) {
+            nearby =
+                nearbyRepository.getNearby(
+                    global = globalData.response,
+                    location =
+                        Coordinate(
+                            latitude = targetLocation.latitude,
+                            longitude = targetLocation.longitude
+                        )
+                )
+            setLastLocation(targetLocation)
+        }
     }
 
     if (nearbyWithRealtimeInfo != null) {
---
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/phoenix/PhoenixPushWrapper.kt
@@ -0,0 +1,16 @@
+package com.mbta.tid.mbta_app.android.phoenix
+
+import com.mbta.tid.mbta_app.network.PhoenixMessage
+import com.mbta.tid.mbta_app.network.PhoenixPush
+import com.mbta.tid.mbta_app.network.PhoenixPushStatus
+import org.phoenixframework.Push
+
+@JvmInline
+value class PhoenixPushWrapper(private val push: Push) : PhoenixPush {
+    override fun receive(
+        status: PhoenixPushStatus,
+        callback: (PhoenixMessage) -> Unit
+    ): PhoenixPush = push.receive(status.value, callback.unwrapped()).wrapped()
+}
+
+fun Push.wrapped() = PhoenixPushWrapper(this)
