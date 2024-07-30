5e4e06855e9c74480d226320f7709931457e39cb
jackcurtis <jcurtis3@mbta.com>
2024-07-30 10:36:29 -0400

    remove unused appContext

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/MainApplication.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.mbta.tid.mbta_app.android
 
 import android.app.Application
-import android.content.Context
 import com.mbta.tid.mbta_app.AppVariant
 import com.mbta.tid.mbta_app.android.phoenix.wrapped
 import com.mbta.tid.mbta_app.android.repositories.AppCheckRepository
@@ -19,9 +18,5 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         initKoin(appVariant, makeNativeModule(appCheck, socket.wrapped()))
-    }
-
-    companion object {
-        lateinit var appContext: Context
     }
 }
