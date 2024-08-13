26ce609e28a14c161d34d544fe90d24797c253d5
Melody Horn <mhorn@mbta.com>
2024-08-13 14:48:42 -0600

    refactor(android): lay ground work for stop details (#334)
    * refactor(android): lay ground work for stop details
    
    * add test for StopDeparturesSummaryList
    
    * fix formatting
    
    i wish very much that format-on-save actually behaved correctly in Android Studio
    
    * add tests for repository subscriber utils
    
    * move repository subscribers to separate files
    
    * fix imports in split files

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/nearbyTransit/NearbyStopView.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/nearbyTransit/NearbyStopView.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,11 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.mbta.tid.mbta_app.android.R
-import com.mbta.tid.mbta_app.android.component.DirectionRowView
-import com.mbta.tid.mbta_app.android.component.HeadsignRowView
-import com.mbta.tid.mbta_app.android.component.PillDecoration
+import com.mbta.tid.mbta_app.android.component.StopDeparturesSummaryList
 import com.mbta.tid.mbta_app.model.PatternsByStop
-import com.mbta.tid.mbta_app.model.RealtimePatterns
 import com.mbta.tid.mbta_app.model.TripInstantDisplay
 import kotlinx.datetime.Instant
 
@@ -34,31 +30,10 @@ fun NearbyStopView(
         )
     }
 
-    for (patterns in patternsAtStop.patterns) {
-        when (patterns) {
-            is RealtimePatterns.ByHeadsign -> {
-                HeadsignRowView(
-                    patterns.headsign,
-                    patterns.format(
-                        now,
-                        if (condenseHeadsignPredictions) 1 else 2,
-                        TripInstantDisplay.Context.NearbyTransit
-                    ),
-                    pillDecoration =
-                        if (patternsAtStop.line != null) PillDecoration.OnRow(patterns.route)
-                        else null
-                )
-            }
-            is RealtimePatterns.ByDirection -> {
-                DirectionRowView(
-                    patterns.direction,
-                    patterns.format(now, TripInstantDisplay.Context.NearbyTransit),
-                    pillDecoration = PillDecoration.OnPrediction(patterns.routesByTrip)
-                )
-            }
-        }
-        if (patterns != patternsAtStop.patterns.last()) {
-            HorizontalDivider(color = MaterialTheme.colorScheme.surface)
-        }
-    }
+    StopDeparturesSummaryList(
+        patternsAtStop,
+        condenseHeadsignPredictions,
+        now,
+        TripInstantDisplay.Context.NearbyTransit
+    )
 }
