52d5090954c393891eefae5c9bac0d1461b21988
jackcurtis <jcurtis3@mbta.com>
2024-08-23 08:34:46 -0400

    revert to nested routes and hide nav bar via callback

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/StopDetailsPage.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/StopDetailsPage.kt
@@ -1,18 +1,11 @@
 package com.mbta.tid.mbta_app.android.pages
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.mapbox.maps.MapboxExperimental
-import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
-import com.mbta.tid.mbta_app.android.component.DragHandle
-import com.mbta.tid.mbta_app.android.map.HomeMapView
 import com.mbta.tid.mbta_app.android.stopDetails.StopDetailsView
 import com.mbta.tid.mbta_app.android.util.StopDetailsFilter
 import com.mbta.tid.mbta_app.android.util.getGlobalData
@@ -23,7 +16,6 @@ import com.mbta.tid.mbta_app.android.util.timer
 import com.mbta.tid.mbta_app.model.Stop
 import com.mbta.tid.mbta_app.model.StopDetailsDepartures
 import com.mbta.tid.mbta_app.model.response.AlertsStreamDataResponse
-import io.github.dellisd.spatialk.geojson.Position
 import kotlin.time.Duration.Companion.seconds
 
 @Composable
@@ -34,9 +26,6 @@ fun StopDetailsPage(
     stop: Stop,
     filterState: MutableState<StopDetailsFilter?>,
     alertData: AlertsStreamDataResponse?,
-    scaffoldState: BottomSheetScaffoldState,
-    mapViewportState: MapViewportState,
-    lastNearbyTransitLocation: Position?,
     onClose: () -> Unit
 ) {
     val globalResponse = getGlobalData()
@@ -71,28 +60,14 @@ fun StopDetailsPage(
                 )
             } else null
         }
-    BottomSheetScaffold(
-        sheetDragHandle = { DragHandle() },
-        sheetContent = {
-            StopDetailsView(
-                modifier,
-                stop,
-                filterState,
-                departures,
-                pinnedRoutes.orEmpty(),
-                togglePinnedRoute,
-                onClose
-            )
-        },
-        scaffoldState = scaffoldState,
-        sheetPeekHeight = 422.dp,
-    ) { sheetPadding ->
-        HomeMapView(
-            Modifier.padding(sheetPadding),
-            mapViewportState,
-            globalResponse = globalResponse,
-            alertsData = alertData,
-            lastNearbyTransitLocation = lastNearbyTransitLocation
-        )
-    }
+
+    StopDetailsView(
+        modifier,
+        stop,
+        filterState,
+        departures,
+        pinnedRoutes.orEmpty(),
+        togglePinnedRoute,
+        onClose
+    )
 }
