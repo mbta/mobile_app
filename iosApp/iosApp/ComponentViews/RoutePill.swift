04f3c3d4c3dc86327d5400ba8c394d2b4bb02589
Melody Horn <mhorn@mbta.com>
2024-08-30 14:41:26 -0600

    give rectangle pills rounded corners

--- iosApp/iosApp/ComponentViews/RoutePill.swift
+++ iosApp/iosApp/ComponentViews/RoutePill.swift
@@ -58,7 +58,7 @@ struct RoutePill: View {
                     .background(pill.routeColor)
             } else if pill.spec.shape == .rectangle {
                 content.overlay(
-                    Rectangle().stroke(pill.routeColor, lineWidth: 1).padding(1)
+                    RoundedRectangle(cornerRadius: 4).stroke(pill.routeColor, lineWidth: 1).padding(1)
                 )
             } else {
                 content.overlay(
@@ -73,7 +73,7 @@ struct RoutePill: View {
 
         func body(content: Content) -> some View {
             switch spec.shape {
-            case .rectangle: content.clipShape(Rectangle())
+            case .rectangle: content.clipShape(RoundedRectangle(cornerRadius: 4))
             case .capsule: content.clipShape(Capsule())
             }
         }
