3a662ae28701cf0041b3989cea0a5e1a5e72729b
Melody Horn <mhorn@mbta.com>
2024-10-16 13:31:51 -0600

    fix HomeMapViewUITests not starting permissions request

--- iosApp/iosAppUITests/HomeMapViewUITests.swift
+++ iosApp/iosAppUITests/HomeMapViewUITests.swift
@@ -32,6 +32,8 @@ final class HomeMapViewUITests: XCTestCase {
         app.activate()
         app.launch()
 
+        app.buttons["Allow Location"].tap()
+
         acceptLocationPermissionAlert(timeout: 10)
 
         let map = app.otherElements.matching(identifier: "transitMap").element
@@ -50,6 +52,8 @@ final class HomeMapViewUITests: XCTestCase {
     func testNoRecenterWithNoLocation() throws {
         app.activate()
         app.launch()
+
+        app.buttons["Allow Location"].tap()
 
         denyLocationPermissionAlert(timeout: 10)
 
