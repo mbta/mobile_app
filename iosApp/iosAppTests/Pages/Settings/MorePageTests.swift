bb69955046e89f5834b3d8012f3e20a327770cc5
Emma Simon <esimon@mbta.com>
2024-10-28 17:26:44 -0400

    test: Get existing tests passing

--- iosApp/iosAppTests/Pages/Settings/MorePageTests.swift
+++ iosApp/iosAppTests/Pages/Settings/MorePageTests.swift
@@ -1,5 +1,5 @@
 //
-//  SettingsPageTests.swift
+//  MorePageTests.swift
 //  iosAppTests
 //
 //  Created by Horn, Melody on 2024-05-03.
@@ -12,7 +12,7 @@ import shared
 import ViewInspector
 import XCTest
 
-final class SettingsPageTests: XCTestCase {
+final class MorePageTests: XCTestCase {
     class FakeSettingsRepository: ISettingsRepository {
         var settings: Set<Setting>
         let onGet: (() -> Void)?
@@ -43,54 +43,51 @@ final class SettingsPageTests: XCTestCase {
         }
     }
 
-    @MainActor func testLoadsState() throws {
+    @MainActor func testLoadsState() async throws {
         let loadedPublisher = PassthroughSubject<Void, Never>()
 
         let settingsRepository = FakeSettingsRepository(
             mapDebug: true,
             searchRouteResults: false,
-            onGet: {
-                loadedPublisher.send(())
-            }
+            onGet: { loadedPublisher.send(()) }
         )
-        let sut = MorePage(
-            viewModel: SettingsViewModel(settingsRepository: settingsRepository)
-        )
+        let viewModel = SettingsViewModel(settingsRepository: settingsRepository)
+
+        let sut = MorePage(viewModel: viewModel)
         let exp = sut.inspection.inspect(onReceive: loadedPublisher, after: 1) { view in
-            XCTAssertTrue(try view.find(ViewType.Toggle.self).isOn())
+            XCTAssertTrue(try view.find(text: "Map Debug").parent().parent().find(ViewType.Toggle.self).isOn())
         }
 
         ViewHosting.host(view: sut)
+        await viewModel.getSections()
 
-        wait(for: [exp], timeout: 2)
+        await fulfillment(of: [exp], timeout: 2)
     }
 
-    @MainActor func testSavesState() throws {
+    @MainActor func testSavesState() async throws {
         let loadedPublisher = PassthroughSubject<Void, Never>()
         let savedExp = expectation(description: "saved state")
 
         let settingsRepository = FakeSettingsRepository(
             mapDebug: false,
             searchRouteResults: false,
-            onGet: {
-                loadedPublisher.send(())
-            },
+            onGet: { loadedPublisher.send(()) },
             onSet: {
                 let mapSetting = $0.first(where: { $0.key == .map })
                 XCTAssertTrue(mapSetting?.isOn == true)
                 savedExp.fulfill()
             }
         )
-        let sut = MorePage(
-            viewModel: SettingsViewModel(settingsRepository: settingsRepository)
-        )
+        let viewModel = SettingsViewModel(settingsRepository: settingsRepository)
 
-        ViewHosting.host(view: sut)
-
+        let sut = MorePage(viewModel: viewModel)
         let tapExp = sut.inspection.inspect(onReceive: loadedPublisher, after: 1) { view in
-            try view.find(ViewType.Toggle.self).tap()
+            try view.find(text: "Map Debug").parent().parent().find(ViewType.Toggle.self).tap()
         }
 
-        wait(for: [tapExp, savedExp], timeout: 2)
+        ViewHosting.host(view: sut)
+        await viewModel.getSections()
+
+        await fulfillment(of: [tapExp, savedExp], timeout: 5)
     }
 }
