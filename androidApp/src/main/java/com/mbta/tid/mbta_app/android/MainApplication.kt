ddcc533b4af3ad61e517971bf246b575cabd5329
Brandon Rodriguez <brodriguez2@CTD27901BRODRIG.local>
2024-11-15 13:17:25 -0600

    fix: cleanup + android removal

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/MainApplication.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/MainApplication.kt
@@ -3,7 +3,6 @@ package com.mbta.tid.mbta_app.android
 import android.app.Application
 import com.mbta.tid.mbta_app.AppVariant
 import com.mbta.tid.mbta_app.android.phoenix.wrapped
-import com.mbta.tid.mbta_app.android.repositories.AppCheckRepository
 import com.mbta.tid.mbta_app.android.util.decodeMessage
 import com.mbta.tid.mbta_app.dependencyInjection.makeNativeModule
 import com.mbta.tid.mbta_app.initKoin
@@ -14,17 +13,12 @@ val appVariant = AppVariant.Prod
 
 class MainApplication : Application() {
     private val socket = Socket(appVariant.socketUrl, decode = ::decodeMessage)
-    private val appCheck = AppCheckRepository()
 
     override fun onCreate() {
         super.onCreate()
         initKoin(
             appVariant,
-            makeNativeModule(
-                AccessibilityStatusRepository(applicationContext),
-                appCheck,
-                socket.wrapped()
-            ),
+            makeNativeModule(AccessibilityStatusRepository(applicationContext), socket.wrapped()),
             this
         )
     }
