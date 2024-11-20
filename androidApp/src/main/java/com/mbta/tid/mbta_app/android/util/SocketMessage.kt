6cfa59bf49ca2ba22c27f97cd540ff4101f15a9c
jackcurtis <jcurtis3@mbta.com>
2024-11-20 09:26:12 -0500

    fix tests

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/util/SocketMessage.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/util/SocketMessage.kt
@@ -1,25 +1,26 @@
 package com.mbta.tid.mbta_app.android.util
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 import org.phoenixframework.Message
 
 fun decodeMessage(rawMessage: String): Message {
     val message = Json.parseToJsonElement(rawMessage).jsonArray
-    val joinRef = message[0].jsonPrimitive.content
+    val joinRef = message[0].jsonPrimitive.contentOrNull
 
-    val ref = message[1].jsonPrimitive.content
-    val topic = message[2].jsonPrimitive.content
-    val event = message[3].jsonPrimitive.content
+    val ref = message[1].jsonPrimitive.contentOrNull
+    val topic = message[2].jsonPrimitive.contentOrNull
+    val event = message[3].jsonPrimitive.contentOrNull
 
     val payloadJson = message[4].toString()
 
     return Message(
         joinRef = joinRef,
-        ref = ref,
-        topic = topic,
-        event = event,
+        ref = ref ?: "",
+        topic = topic ?: "",
+        event = event ?: "",
         rawPayload = emptyMap(),
         payloadJson = payloadJson
     )
