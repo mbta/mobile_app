788c7fa6ed94a6aeb3e94ae55c4d343b4533cb76
Melody Horn <mhorn@mbta.com>
2024-12-02 10:21:47 -0700

    fix(Android): only render map-related iOS icons as PNGs (#553)
    * fix(Android): only render map-related iOS icons as PNGs
    
    * remove new unnecessarily rendered assets

---
+++ androidApp/src/test/java/com/mbta/tid/mbta_app/android/map/DrawableByNameTest.kt
@@ -0,0 +1,38 @@
+package com.mbta.tid.mbta_app.android.map
+
+import com.mbta.tid.mbta_app.android.generated.drawableByName
+import com.mbta.tid.mbta_app.map.AlertIcons
+import com.mbta.tid.mbta_app.map.StopIcons
+import com.mbta.tid.mbta_app.model.Alert
+import com.mbta.tid.mbta_app.model.MapStopRoute
+import com.mbta.tid.mbta_app.model.ObjectCollectionBuilder.Single.alert
+import com.mbta.tid.mbta_app.model.RealtimePatterns
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class DrawableByNameTest {
+    @Test
+    fun `all stop icons are defined`() {
+        for (icon in StopIcons.all) {
+            assertNotNull(drawableByName(icon))
+        }
+    }
+
+    @Test
+    fun `all alert icons are defined`() {
+        for (icon in AlertIcons.all) {
+            assertNotNull(drawableByName(icon))
+        }
+    }
+
+    @Test
+    fun `all secondary alert icon names are defined`() {
+        for (alertEffect in Alert.Effect.entries) {
+            val alert = alert { effect = alertEffect }
+            for (route in MapStopRoute.entries + null) {
+                val icon = RealtimePatterns.Format.SecondaryAlert(alert, route)
+                assertNotNull(drawableByName(icon.iconName))
+            }
+        }
+    }
+}
