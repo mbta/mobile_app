8bcab055221c18cff7000f424839800eff6d5bd4
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-12-03 16:59:32 -0500

    feat(MorePage): contents + styling

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -21,9 +21,17 @@ android {
         versionName = (findProperty("android.injected.version.name") ?: "0.1.0") as String
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    buildFeatures { compose = true }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-    buildTypes { getByName("release") { isMinifyEnabled = false } }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
     flavorDimensions += "environment"
     productFlavors {
         create("staging") {
--- androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/pages/MorePageTests.kt
+++ androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/pages/MorePageTests.kt
@@ -1,9 +1,11 @@
 package com.mbta.tid.mbta_app.android.pages
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import com.mbta.tid.mbta_app.repositories.ISettingsRepository
 import com.mbta.tid.mbta_app.repositories.MockSettingsRepository
 import com.mbta.tid.mbta_app.repositories.Settings
@@ -54,5 +56,30 @@ class MorePageTests : KoinTest {
         composeTestRule.onNodeWithText("Hide Maps").performClick()
 
         assertTrue { hideMapToggleCalled }
+    }
+
+    @Test
+    fun testLinksExist() {
+        val koinApplication = koinApplication {
+            modules(
+                module {
+                    single<ISettingsRepository> { MockSettingsRepository(onSaveSettings = {}) }
+                }
+            )
+        }
+        composeTestRule.setContent {
+            KoinContext(koinApplication.koin) { MorePage(bottomBar = {}) }
+        }
+
+        composeTestRule.onNodeWithText("Send app feedback").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Trip Planner").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Fare Information").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Commuter Rail and Ferry tickets").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Terms of Use").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Privacy Policy").assertIsDisplayed()
+        composeTestRule.onNodeWithText("View source on GitHub").assertIsDisplayed()
+        composeTestRule.onNodeWithText("View source on GitHub")
+        composeTestRule.onNode(hasText("617-222-3200")).performScrollTo()
+        composeTestRule.onNodeWithText("617-222-3200").assertIsDisplayed()
     }
 }
