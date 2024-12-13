b5740cd774c5bdb714c00d64338e1685215c1584
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-12-13 16:53:52 -0500

    feat(android): leave / rejoin predictions & alerts after backgrounding

--- androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/state/SubscribeToPredictionsTest.kt
+++ androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/state/SubscribeToPredictionsTest.kt
@@ -1,133 +1,108 @@
 package com.mbta.tid.mbta_app.android.state
 
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStore
-import com.mbta.tid.mbta_app.android.util.TimerViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.testing.TestLifecycleOwner
 import com.mbta.tid.mbta_app.model.ObjectCollectionBuilder
-import com.mbta.tid.mbta_app.model.response.ApiResult
 import com.mbta.tid.mbta_app.model.response.PredictionsByStopJoinResponse
-import com.mbta.tid.mbta_app.model.response.PredictionsByStopMessageResponse
 import com.mbta.tid.mbta_app.model.response.PredictionsStreamDataResponse
-import com.mbta.tid.mbta_app.repositories.IPredictionsRepository
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.launch
+import com.mbta.tid.mbta_app.repositories.MockPredictionsRepository
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.Instant
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
 import org.junit.Rule
 import org.junit.Test
-
-class MockPredictionsRepository(private val scope: CoroutineScope) : IPredictionsRepository {
-    val stopIdsChannel = Channel<List<String>>()
-    lateinit var onJoin: (ApiResult<PredictionsByStopJoinResponse>) -> Unit
-    lateinit var onMessage: (ApiResult<PredictionsByStopMessageResponse>) -> Unit
-    var disconnectHook: () -> Unit = { println("original disconnect hook called") }
-
-    override fun connect(
-        stopIds: List<String>,
-        onReceive: (ApiResult<PredictionsStreamDataResponse>) -> Unit
-    ) {
-        /* null-op */
-    }
-
-    override fun connectV2(
-        stopIds: List<String>,
-        onJoin: (ApiResult<PredictionsByStopJoinResponse>) -> Unit,
-        onMessage: (ApiResult<PredictionsByStopMessageResponse>) -> Unit
-    ) {
-
-        this.onJoin = onJoin
-        scope.launch { stopIdsChannel.send(stopIds) }
-    }
-
-    override var lastUpdated: Instant? = null
-
-    override fun shouldForgetPredictions(predictionCount: Int) = false
-
-    override fun disconnect() {
-        disconnectHook()
-    }
-}
 
 class SubscribeToPredictionsTest {
     @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
     fun testPredictions() = runTest {
-        fun buildSomePredictions(): PredictionsByStopJoinResponse {
-            val objects = ObjectCollectionBuilder()
-            objects.prediction()
-            objects.prediction()
-            return PredictionsByStopJoinResponse(objects)
-        }
-        val predictionsRepo = MockPredictionsRepository(this)
+        val objects = ObjectCollectionBuilder()
+        objects.prediction()
+        objects.prediction()
+        val predictionsOnJoin = PredictionsByStopJoinResponse(objects)
 
-        var stopIds by mutableStateOf(listOf("place-a"))
-        var unmounted by mutableStateOf(false)
+        var connectProps: List<String>? = null
+        var disconnectCount = 0
+
+        val predictionsRepo =
+            MockPredictionsRepository(
+                {},
+                { stops -> connectProps = stops },
+                { disconnectCount += 1 },
+                null,
+                predictionsOnJoin
+            )
+
+        var stopIds = mutableStateOf(listOf("place-a"))
         var predictions: PredictionsStreamDataResponse? =
             PredictionsStreamDataResponse(ObjectCollectionBuilder())
+
         composeTestRule.setContent {
-            if (!unmounted) predictions = subscribeToPredictions(stopIds, predictionsRepo)
+            var stopIds by remember { stopIds }
+            predictions = subscribeToPredictions(stopIds, predictionsRepo)
         }
 
-        composeTestRule.awaitIdle()
-        assertEquals(listOf("place-a"), predictionsRepo.stopIdsChannel.receive())
-        assertNull(predictions)
+        composeTestRule.waitUntil { connectProps == listOf("place-a") }
 
-        val expectedPredictions1 = buildSomePredictions()
-        predictionsRepo.onJoin(ApiResult.Ok(expectedPredictions1))
-        composeTestRule.awaitIdle()
-        assertEquals(expectedPredictions1.toPredictionsStreamDataResponse(), predictions)
+        composeTestRule.waitUntil {
+            predictions != null &&
+                predictions == predictionsOnJoin?.toPredictionsStreamDataResponse()
+        }
 
-        stopIds = listOf("place-b")
-        composeTestRule.awaitIdle()
-        assertEquals(listOf("place-b"), predictionsRepo.stopIdsChannel.receive())
-        predictionsRepo.onJoin(ApiResult.Ok(expectedPredictions1))
-        composeTestRule.awaitIdle()
-        assertEquals(expectedPredictions1.toPredictionsStreamDataResponse(), predictions)
+        assertEquals(0, disconnectCount)
 
-        val expectedPredictions2 = buildSomePredictions()
-        predictionsRepo.onJoin(ApiResult.Ok(expectedPredictions2))
-        composeTestRule.awaitIdle()
-        assertEquals(expectedPredictions2.toPredictionsStreamDataResponse(), predictions)
+        stopIds.value = listOf("place-b")
+        composeTestRule.waitUntil { disconnectCount == 1 }
 
-        unmounted = true
-        composeTestRule.awaitIdle()
+        composeTestRule.waitUntil { connectProps == listOf("place-b") }
     }
 
     @Test
-    fun testPredictionsOnClear() = runTest {
-        var disconnectCalled = false
-        val stopIds by mutableStateOf(listOf("place-a"))
-        val mockPredictionsRepository = MockPredictionsRepository(this.backgroundScope)
-        mockPredictionsRepository.disconnectHook = { disconnectCalled = true }
+    fun testDisconnectsOnPause() = runTest {
+        val lifecycleOwner = TestLifecycleOwner(Lifecycle.State.RESUMED)
 
-        val viewModelStore = ViewModelStore()
-        val viewModelProvider =
-            ViewModelProvider(
-                viewModelStore,
-                object : ViewModelProvider.Factory {
-                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                        return PredictionsViewModel(
-                            stopIds,
-                            mockPredictionsRepository,
-                            TimerViewModel(1.seconds)
-                        )
-                            as T
-                    }
-                }
+        var connectCount = 0
+        var disconnectCount = 0
+
+        val predictionsRepo =
+            MockPredictionsRepository(
+                {},
+                { stopIds -> connectCount += 1 },
+                { disconnectCount += 1 },
+                null,
+                null
             )
-        viewModelProvider.get(PredictionsViewModel::class)
-        viewModelStore.clear()
-        assertEquals(true, disconnectCalled)
+
+        var stopIds = mutableStateOf(listOf("place-a"))
+        var predictions: PredictionsStreamDataResponse? =
+            PredictionsStreamDataResponse(ObjectCollectionBuilder())
+
+        composeTestRule.setContent {
+            CompositionLocalProvider(LocalLifecycleOwner provides lifecycleOwner) {
+                var stopIds by remember { stopIds }
+                predictions = subscribeToPredictions(stopIds, predictionsRepo)
+            }
+        }
+
+        composeTestRule.waitUntil { connectCount == 1 }
+        assertEquals(0, disconnectCount)
+
+        composeTestRule.runOnIdle { lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE) }
+
+        composeTestRule.waitUntil { disconnectCount == 1 }
+        assertEquals(1, connectCount)
+
+        composeTestRule.runOnIdle { lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_RESUME) }
+
+        composeTestRule.waitUntil { connectCount == 2 }
+        assertEquals(1, disconnectCount)
     }
 }
--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/state/subscribeToPredictions.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/state/subscribeToPredictions.kt
@@ -1,87 +1,89 @@
 package com.mbta.tid.mbta_app.android.state
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asFlow
+import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.mbta.tid.mbta_app.android.util.TimerViewModel
 import com.mbta.tid.mbta_app.model.response.ApiResult
 import com.mbta.tid.mbta_app.model.response.PredictionsByStopJoinResponse
+import com.mbta.tid.mbta_app.model.response.PredictionsByStopMessageResponse
 import com.mbta.tid.mbta_app.model.response.PredictionsStreamDataResponse
 import com.mbta.tid.mbta_app.repositories.IPredictionsRepository
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
-import okhttp3.internal.notifyAll
 import org.koin.compose.koinInject
 
 class PredictionsViewModel(
-    private val stopIds: List<String>,
     private val predictionsRepository: IPredictionsRepository,
-    private val timerViewModel: TimerViewModel
 ) : ViewModel() {
     private val _predictions: MutableLiveData<PredictionsByStopJoinResponse> = MutableLiveData()
     val predictions: LiveData<PredictionsByStopJoinResponse> = _predictions
     val predictionsFlow = predictions.asFlow().map { it.toPredictionsStreamDataResponse() }
-
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            if (stopIds.size > 0) {
-                connectToPredictions()
-            }
-            timerViewModel.timerFlow.collect {
-                synchronized(predictions) { predictions.notifyAll() }
-            }
-        }
-    }
 
     override fun onCleared() {
         super.onCleared()
         predictionsRepository.disconnect()
     }
 
-    private fun connectToPredictions() {
-        predictionsRepository.connectV2(
-            stopIds,
-            {
-                when (it) {
-                    is ApiResult.Ok -> {
-                        _predictions.postValue(it.data)
-                        synchronized(predictions) { predictions.notifyAll() }
-                    }
-                    is ApiResult.Error -> {
-                        /* TODO("handle errors") */
-                    }
-                }
-            },
-            {
-                when (it) {
-                    is ApiResult.Ok -> {
-                        _predictions.postValue(
-                            (_predictions.value
-                                    ?: PredictionsByStopJoinResponse(
-                                        mapOf(it.data.stopId to it.data.predictions),
-                                        it.data.trips,
-                                        it.data.vehicles
-                                    ))
-                                .mergePredictions(it.data)
-                        )
-                    }
-                    is ApiResult.Error -> {
-                        /* TODO("handle errors") */
-                    }
-                }
+    fun connect(stopIds: List<String>?) {
+
+        if (!stopIds.isNullOrEmpty()) {
+            predictionsRepository.connectV2(stopIds, ::handleJoinMessage, ::handlePushMessage)
+        }
+    }
+
+    private fun handleJoinMessage(message: ApiResult<PredictionsByStopJoinResponse>) {
+        when (message) {
+            is ApiResult.Ok -> {
+                _predictions.postValue(message.data)
             }
-        )
+            is ApiResult.Error -> {
+                Log.e(
+                    "PredictionsViewModel",
+                    "Predictions stream failed to join: ${message.message}"
+                )
+            }
+        }
+    }
+
+    private fun handlePushMessage(message: ApiResult<PredictionsByStopMessageResponse>) {
+        when (message) {
+            is ApiResult.Ok -> {
+                _predictions.postValue(
+                    (_predictions.value
+                            ?: PredictionsByStopJoinResponse(
+                                mapOf(message.data.stopId to message.data.predictions),
+                                message.data.trips,
+                                message.data.vehicles
+                            ))
+                        .mergePredictions(message.data)
+                )
+            }
+            is ApiResult.Error -> {
+                Log.e(
+                    "PredictionsViewModel",
+                    "Predictions stream failed on message: ${message.message}"
+                )
+            }
+        }
+    }
+
+    fun disconnect() {
+        predictionsRepository.disconnect()
+    }
+
+    class Factory(private val predictionsRepository: IPredictionsRepository) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return PredictionsViewModel(predictionsRepository) as T
+        }
     }
 }
 
@@ -90,10 +92,13 @@ fun subscribeToPredictions(
     stopIds: List<String>?,
     predictionsRepository: IPredictionsRepository = koinInject()
 ): PredictionsStreamDataResponse? {
-    val timerViewModel = remember { TimerViewModel(1.seconds) }
     val viewModel: PredictionsViewModel =
-        remember(stopIds) {
-            PredictionsViewModel(stopIds ?: emptyList(), predictionsRepository, timerViewModel)
-        }
+        viewModel(factory = PredictionsViewModel.Factory(predictionsRepository))
+
+    LifecycleResumeEffect(key1 = stopIds) {
+        viewModel.connect(stopIds)
+
+        onPauseOrDispose { viewModel.disconnect() }
+    }
     return viewModel.predictionsFlow.collectAsState(initial = null).value
 }
