10011ba1efa0f2c506fb99367215d39f91071537
Melody Horn <mhorn@mbta.com>
2024-12-17 11:04:12 -0700

    remember sharingLocation as saveable

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/onboarding/OnboardingScreenView.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/onboarding/OnboardingScreenView.kt
@@ -7,8 +7,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
@@ -33,7 +33,7 @@ fun OnboardingScreenView(
     settingsRepository: ISettingsRepository = koinInject()
 ) {
     val coroutineScope = rememberCoroutineScope()
-    var sharingLocation by remember { mutableStateOf(false) }
+    var sharingLocation by rememberSaveable { mutableStateOf(false) }
     val permissions =
         locationDataManager.rememberPermissions(
             onPermissionsResult = {
