a6e040bf37194e5d9ec020cdd5719fd5d1a29fdc
Melody Horn <mhorn@mbta.com>
2024-12-20 13:06:08 -0700

    feat(Android): allow small sheet size

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/NearbyTransitPage.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/NearbyTransitPage.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -34,6 +32,8 @@ import androidx.navigation.toRoute
 import com.mapbox.maps.MapboxExperimental
 import com.mbta.tid.mbta_app.android.SheetRoutes
 import com.mbta.tid.mbta_app.android.component.DragHandle
+import com.mbta.tid.mbta_app.android.component.sheet.BottomSheetScaffold
+import com.mbta.tid.mbta_app.android.component.sheet.BottomSheetScaffoldState
 import com.mbta.tid.mbta_app.android.location.LocationDataManager
 import com.mbta.tid.mbta_app.android.location.ViewportProvider
 import com.mbta.tid.mbta_app.android.map.HomeMapView
@@ -267,7 +267,6 @@ fun NearbyTransitPage(
                 },
                 sheetContainerColor = MaterialTheme.colorScheme.surface,
                 scaffoldState = nearbyTransit.scaffoldState,
-                sheetPeekHeight = 422.dp,
             ) { sheetPadding ->
                 HomeMapView(
                     Modifier.padding(sheetPadding),
