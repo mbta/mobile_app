e021aa74d732d99b6834c28601678bc943ff7894
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-01-10 10:43:07 -0500

    perf(getStopMapData): use Dispatchers.IO to fetch stop map data

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/state/getStopMapData.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/state/getStopMapData.kt
@@ -1,19 +1,19 @@
 package com.mbta.tid.mbta_app.android.state
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mbta.tid.mbta_app.model.response.ApiResult
 import com.mbta.tid.mbta_app.model.response.StopMapResponse
 import com.mbta.tid.mbta_app.repositories.IStopRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 
 class StopMapViewModel(private val stopRepository: IStopRepository) : ViewModel() {
@@ -21,10 +21,11 @@ class StopMapViewModel(private val stopRepository: IStopRepository) : ViewModel(
     val stopMapResponse: StateFlow<StopMapResponse?> = _stopMapResponse
 
     suspend fun getStopMapData(stopId: String) {
-        Log.i("KB", "get stop map data ${stopId}")
-        when (val data = stopRepository.getStopMapData(stopId)) {
-            is ApiResult.Ok -> _stopMapResponse.emit(data.data)
-            is ApiResult.Error -> TODO("handle errors")
+        CoroutineScope(Dispatchers.IO).launch {
+            when (val data = stopRepository.getStopMapData(stopId)) {
+                is ApiResult.Ok -> _stopMapResponse.emit(data.data)
+                is ApiResult.Error -> TODO("handle errors")
+            }
         }
     }
 
