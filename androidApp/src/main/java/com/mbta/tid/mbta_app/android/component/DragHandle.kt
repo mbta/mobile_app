254b4ed6a69c7e37655e3061b5b3d8d056a0ca3f
Brandon Rodriguez <brodriguez2@CTD27901BRODRIG.local>
2025-01-13 13:53:47 -0600

    fix(Android): sheet drag handle padding

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/DragHandle.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/DragHandle.kt
@@ -1,23 +1,33 @@
 package com.mbta.tid.mbta_app.android.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.mbta.tid.mbta_app.android.R
 
 @Composable
 @ExperimentalMaterial3Api
 fun DragHandle() {
     Column(
-        modifier = Modifier.fillMaxWidth().background(colorResource(id = R.color.fill1)),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier =
+            Modifier.height(36.dp).fillMaxWidth().background(colorResource(id = R.color.fill1)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        BottomSheetDefaults.DragHandle()
+        Surface(color = colorResource(R.color.neutral75), shape = MaterialTheme.shapes.extraLarge) {
+            Box(Modifier.size(width = 32.dp, height = 4.dp))
+        }
     }
 }
