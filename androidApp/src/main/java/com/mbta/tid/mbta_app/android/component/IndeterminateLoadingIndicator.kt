3c34bba3ee8221d14762f76c67195ad7c0afae87
Brandon Rodriguez <BrandonTR@users.noreply.github.com>
2025-01-13 09:29:24 -0600

    fix(Android): Error banner UI polish (#642)
    * fix(Android): Error banner UI polish
    
    * remove unused string
    
    * size modifier
    
    Co-authored-by: Melody Horn <mhorn@mbta.com>
    
    * move plural back to strings.xml
    
    * fix import
    
    ---------
    
    Co-authored-by: Brandon Rodriguez <brodriguez2@CTD27901BRODRIG.local>
    Co-authored-by: Melody Horn <mhorn@mbta.com>

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/IndeterminateLoadingIndicator.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/IndeterminateLoadingIndicator.kt
@@ -2,16 +2,17 @@ package com.mbta.tid.mbta_app.android.component
 
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.mbta.tid.mbta_app.android.R
 
 @Composable
 fun IndeterminateLoadingIndicator(modifier: Modifier = Modifier.width(20.dp)) {
     CircularProgressIndicator(
         modifier = modifier,
-        color = MaterialTheme.colorScheme.secondary,
-        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        color = colorResource(R.color.contrast),
+        trackColor = colorResource(R.color.contrast).copy(alpha = 0.25f),
     )
 }
