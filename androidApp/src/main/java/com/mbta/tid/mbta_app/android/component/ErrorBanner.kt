d28b6351396b7f3682c485dcba8dbebde8ea8332
Brandon Rodriguez <brodriguez2@CTD27901BRODRIG.local>
2025-01-13 09:00:33 -0600

    fix import

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/ErrorBanner.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/ErrorBanner.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
