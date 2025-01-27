6459b7486e0f6cda9bd3c60b88073f48e8aedc63
Melody Horn <mhorn@mbta.com>
2025-01-27 14:23:31 -0700

    add background for legibility

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/TripStops.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/TripStops.kt
@@ -1,5 +1,6 @@
 package com.mbta.tid.mbta_app.android.stopDetails
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -63,7 +65,7 @@ fun TripStops(
 
     val lastStopSequence = stops.stops.lastOrNull()?.stopSequence
 
-    Column {
+    Column(Modifier.background(colorResource(R.color.fill2))) {
         if (splitStops != null && target != null) {
             if (showFirstStopSeparately) {
                 val firstStop = splitStops.firstStop
