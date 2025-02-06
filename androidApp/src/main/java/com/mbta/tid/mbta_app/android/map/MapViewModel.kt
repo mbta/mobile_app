dd1b1268ceab55b29c70e2ec2b2c2ead8ecddbcc
Melody Horn <mhorn@mbta.com>
2025-02-06 13:30:38 -0700

    move feature builders to background thread

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/map/MapViewModel.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/map/MapViewModel.kt
@@ -126,26 +126,30 @@ open class MapViewModel(
     }
 
     override suspend fun refreshRouteLineData(now: Instant) {
-        val globalResponse = globalResponse.first() ?: return
-        val railRouteShapes = railRouteShapes.first() ?: return
-        _railRouteLineData.value =
-            RouteFeaturesBuilder.generateRouteLines(
-                railRouteShapes.routesWithSegmentedShapes,
-                globalResponse.routes,
-                globalResponse.stops,
-                globalMapData(now)?.alertsByStop
-            )
+        withContext(Dispatchers.Default) {
+            val globalResponse = globalResponse.first() ?: return@withContext
+            val railRouteShapes = railRouteShapes.first() ?: return@withContext
+            _railRouteLineData.value =
+                RouteFeaturesBuilder.generateRouteLines(
+                    railRouteShapes.routesWithSegmentedShapes,
+                    globalResponse.routes,
+                    globalResponse.stops,
+                    globalMapData(now)?.alertsByStop
+                )
+        }
     }
 
     override suspend fun refreshStopFeatures(now: Instant, selectedStop: Stop?) {
-        val routeLineData = railRouteLineData.first() ?: return
-        _stopSourceData.value =
-            StopFeaturesBuilder.buildCollection(
-                    StopSourceData(selectedStopId = selectedStop?.id),
-                    globalMapData(now)?.mapStops.orEmpty(),
-                    routeLineData
-                )
-                .toMapbox()
+        withContext(Dispatchers.Default) {
+            val routeLineData = railRouteLineData.first() ?: return@withContext
+            _stopSourceData.value =
+                StopFeaturesBuilder.buildCollection(
+                        StopSourceData(selectedStopId = selectedStop?.id),
+                        globalMapData(now)?.mapStops.orEmpty(),
+                        routeLineData
+                    )
+                    .toMapbox()
+        }
     }
 
     override suspend fun setAlertsData(alertsData: AlertsStreamDataResponse?) {
