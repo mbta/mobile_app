bdf906afa06cbe66facca769a0cbe92ee19c67fa
Melody Horn <mhorn@mbta.com>
2025-02-11 16:36:01 -0700

    fix: consistently use sentence case for downstream alert header

--- iosApp/iosApp/Utils/AlertExtension.swift
+++ iosApp/iosApp/Utils/AlertExtension.swift
@@ -11,15 +11,20 @@ import shared
 import SwiftUI
 
 extension shared.Alert {
-    func effectDescription() -> Text {
-        switch effect {
-        case .detour: Text("Detour", comment: "Possible alert effect")
-        case .shuttle: Text("Shuttle buses", comment: "Possible alert effect")
-        case .snowRoute: Text("Snow route", comment: "Possible alert effect")
-        case .stopClosure: Text("Stop closed", comment: "Possible alert effect")
-        case .suspension: Text("Service suspended", comment: "Possible alert effect")
-        case .serviceChange: Text("Service change", comment: "Possible alert effect")
-        default: Text("Alert", comment: "Possible alert effect")
+    func downstreamEffectDescription() -> String {
+        let effect = switch self.effect {
+        case .detour: NSLocalizedString("Detour", comment: "Possible alert effect")
+        case .shuttle: NSLocalizedString("Shuttle buses", comment: "Possible alert effect")
+        case .snowRoute: NSLocalizedString("Snow route", comment: "Possible alert effect")
+        case .stopClosure: NSLocalizedString("Stop closed", comment: "Possible alert effect")
+        case .suspension: NSLocalizedString("Service suspended", comment: "Possible alert effect")
+        case .serviceChange: NSLocalizedString("Service change", comment: "Possible alert effect")
+        default: NSLocalizedString("Alert", comment: "Possible alert effect")
         }
+        return String(format: NSLocalizedString("%@ ahead", comment: """
+        Label for an alert that exists on a future stop along the selected route,
+        the interpolated value can be any alert effect,
+        ex. "[Detour] ahead", "[Shuttle buses] ahead"
+        """), effect)
     }
 }
