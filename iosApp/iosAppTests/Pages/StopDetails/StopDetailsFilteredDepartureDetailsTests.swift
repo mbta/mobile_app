1eec6835f84eb1afd24e66d133def308ac7cfbab
Melody Horn <mhorn@mbta.com>
2025-02-12 10:11:41 -0700

    fix text in test

--- iosApp/iosAppTests/Pages/StopDetails/StopDetailsFilteredDepartureDetailsTests.swift
+++ iosApp/iosAppTests/Pages/StopDetails/StopDetailsFilteredDepartureDetailsTests.swift
@@ -462,9 +462,9 @@ final class StopDetailsFilteredDepartureDetailsTests: XCTestCase {
 
         XCTAssertNotNil(try sut.inspect().find(DepartureTile.self))
         XCTAssertNotNil(try sut.inspect().find(AlertCard.self))
-        XCTAssertNotNil(try sut.inspect().find(text: "Suspension ahead"))
+        XCTAssertNotNil(try sut.inspect().find(text: "Service suspended ahead"))
         XCTAssertThrowsError(try sut.inspect().find(text: alert.header!))
-        try sut.inspect().find(AlertCard.self).button().tap()
+        try sut.inspect().find(AlertCard.self).implicitAnyView().button().tap()
         XCTAssertEqual(
             nearbyVM.navigationStack.last,
             .alertDetails(alertId: alert.id, line: nil, routes: [route], stop: stop)
