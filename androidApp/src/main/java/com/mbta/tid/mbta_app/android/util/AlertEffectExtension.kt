e3a974929c47ea7659567eed1656b262992abd01
Melody Horn <mhorn@mbta.com>
2025-02-12 10:43:59 -0700

    fix: consistently use sentence case for downstream alert header (#737)
    * fix: consistently use sentence case for downstream alert header
    
    * fix text in test

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/util/AlertEffectExtension.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/util/AlertEffectExtension.kt
@@ -6,16 +6,17 @@ import com.mbta.tid.mbta_app.android.R
 import com.mbta.tid.mbta_app.model.Alert
 
 @Composable
-fun Alert.effectDescription(): String {
+fun Alert.downstreamEffectDescription(): String {
     val effectAhead = R.string.effect_ahead
     return when (effect) {
         Alert.Effect.Detour -> stringResource(effectAhead, stringResource(R.string.detour))
         Alert.Effect.ServiceChange ->
-            stringResource(effectAhead, stringResource(R.string.service_change))
+            stringResource(effectAhead, stringResource(R.string.service_change_sentence_case))
         Alert.Effect.Shuttle -> stringResource(effectAhead, stringResource(R.string.shuttle_buses))
-        Alert.Effect.SnowRoute -> stringResource(effectAhead, stringResource(R.string.snow_route))
+        Alert.Effect.SnowRoute ->
+            stringResource(effectAhead, stringResource(R.string.snow_route_sentence_case))
         Alert.Effect.StopClosure ->
-            stringResource(effectAhead, stringResource(R.string.stop_closure))
+            stringResource(effectAhead, stringResource(R.string.stop_closed_sentence_case))
         Alert.Effect.Suspension ->
             stringResource(effectAhead, stringResource(R.string.service_suspended))
         else -> stringResource(effectAhead, stringResource(R.string.alert))
