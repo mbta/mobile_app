79fd68502df9efb58556862d73b55bf524b75d6f
Emma Simon <emmamaysimon@gmail.com>
2025-02-14 16:53:45 -0500

    fix(SearchBarOverlay): halo border attached to search bar

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/search/SearchBarOverlay.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/search/SearchBarOverlay.kt
@@ -2,15 +2,10 @@ package com.mbta.tid.mbta_app.android.search
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -36,13 +31,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.mbta.tid.mbta_app.android.R
@@ -72,8 +64,6 @@ fun SearchBarOverlay(
     val searchResultsVm: SearchResultsViewModel = koinViewModel()
     val searchResults = searchResultsVm.searchResults.collectAsState(initial = null).value
 
-    var searchBarHeight by rememberSaveable { mutableStateOf(0) }
-
     val buttonColors =
         ButtonColors(
             containerColor = colorResource(R.color.fill3),
@@ -94,160 +84,104 @@ fun SearchBarOverlay(
     }
 
     Box(contentAlignment = Alignment.TopCenter) {
-        val baseModifier =
-            if (expanded) {
-                Modifier
-            } else {
-                Modifier.absoluteOffset { IntOffset(0, 12.dp.roundToPx()) }.statusBarsPadding()
-            }
         Box(
-            modifier = baseModifier.zIndex(1f),
+            modifier = Modifier.zIndex(1f),
             contentAlignment = Alignment.Center,
         ) {
             if (visible) {
-                Box(
-                    modifier =
-                        if (expanded) {
-                            Modifier
-                        } else {
-                            Modifier.padding(horizontal = 14.dp)
-                        }
-                ) {
-                    if (!expanded) {
-                        // Workaround for adding border
-                        Box(
-                            modifier =
-                                Modifier.offset(y = 6.dp)
-                                    .haloContainer(
-                                        2.dp,
-                                        borderRadius = 10.dp,
-                                        backgroundColor = Color.Transparent
-                                    )
-                                    .fillMaxWidth()
-                                    .height(
-                                        with(LocalDensity.current) {
-                                            androidx.compose.ui.unit.min(
-                                                (searchBarHeight).toDp() - 8.dp,
-                                                // Prevent giant box during animation when
-                                                // returning from results
-                                                64.dp
-                                            )
-                                        }
-                                    )
-                        ) {}
-                    }
-
-                    Column(
-                        modifier =
-                            Modifier.padding(
-                                    horizontal =
-                                        if (expanded) {
-                                            0.dp
-                                        } else {
-                                            2.dp
-                                        }
-                                )
-                                .onSizeChanged {
-                                    if (!expanded) {
-                                        searchBarHeight = it.height
-                                    }
-                                }
-                    ) {
-                        SearchBar(
-                            shape = RoundedCornerShape(10.dp),
+                SearchBar(
+                    shape = RoundedCornerShape(10.dp),
+                    colors =
+                        SearchBarDefaults.colors(
+                            containerColor =
+                                if (expanded) colorResource(R.color.fill1) else Color.Transparent
+                        ),
+                    inputField = {
+                        SearchBarDefaults.InputField(
                             colors =
-                                SearchBarDefaults.colors(
-                                    containerColor = colorResource(R.color.fill3)
+                                SearchBarDefaults.inputFieldColors(
+                                    focusedTextColor = colorResource(R.color.text),
+                                    unfocusedTextColor = colorResource(R.color.text),
+                                    focusedPlaceholderColor = colorResource(R.color.deemphasized),
+                                    unfocusedPlaceholderColor = colorResource(R.color.deemphasized),
                                 ),
-                            inputField = {
-                                SearchBarDefaults.InputField(
-                                    colors =
-                                        SearchBarDefaults.inputFieldColors(
-                                            focusedTextColor = colorResource(R.color.text),
-                                            unfocusedTextColor = colorResource(R.color.text),
-                                            focusedPlaceholderColor =
-                                                colorResource(R.color.deemphasized),
-                                            unfocusedPlaceholderColor =
-                                                colorResource(R.color.deemphasized),
-                                        ),
-                                    query = searchInputState,
-                                    placeholder = {
-                                        Text(
-                                            stringResource(R.string.stops),
-                                            // This will be drawn in bodyLarge if we don't
-                                            // re-override it
-                                            // here
-                                            style = MaterialTheme.typography.bodySmall
-                                        )
-                                    },
-                                    expanded = expanded,
-                                    onQueryChange = { searchInputState = it },
-                                    onExpandedChange = onExpandedChange,
-                                    modifier =
-                                        Modifier.focusRequester(inputFieldFocusRequester)
-                                            .fillMaxWidth(),
-                                    onSearch = {},
-                                    leadingIcon = {
-                                        Icon(
-                                            painterResource(R.drawable.magnifying_glass),
-                                            null,
-                                            tint = colorResource(R.color.deemphasized)
-                                        )
-                                    },
-                                    trailingIcon = {
-                                        if (expanded) {
-                                            Button(
-                                                colors = buttonColors,
-                                                onClick = { onExpandedChange(false) },
-                                            ) {
-                                                Icon(
-                                                    painterResource(R.drawable.fa_xmark),
-                                                    stringResource(R.string.close_button_label),
-                                                    tint = colorResource(R.color.deemphasized)
-                                                )
-                                            }
-                                        }
-                                    }
+                            query = searchInputState,
+                            placeholder = {
+                                Text(
+                                    stringResource(R.string.stops),
+                                    // This will be drawn in bodyLarge if we don't
+                                    // re-override it here
+                                    style = MaterialTheme.typography.bodySmall
                                 )
                             },
                             expanded = expanded,
-                            onExpandedChange = {},
-                        ) {
-                            LazyColumn(
-                                modifier =
-                                    Modifier.fillMaxSize().background(colorResource(R.color.fill1)),
-                                contentPadding = PaddingValues(16.dp)
-                            ) {
-                                if (searchInputState.isEmpty()) {
-                                    item {
-                                        Text(
-                                            modifier = Modifier.padding(bottom = 10.dp),
-                                            text = stringResource(R.string.recently_viewed),
-                                            style = MaterialTheme.typography.headlineSmall,
-                                            fontWeight = FontWeight.Bold
+                            onQueryChange = { searchInputState = it },
+                            onExpandedChange = onExpandedChange,
+                            modifier =
+                                Modifier.padding(horizontal = 14.dp)
+                                    .haloContainer(
+                                        2.dp,
+                                        borderRadius = 8.dp,
+                                        backgroundColor = colorResource(R.color.fill3)
+                                    )
+                                    .fillMaxWidth()
+                                    .focusRequester(inputFieldFocusRequester),
+                            onSearch = {},
+                            leadingIcon = {
+                                Icon(
+                                    painterResource(R.drawable.magnifying_glass),
+                                    null,
+                                    tint = colorResource(R.color.deemphasized)
+                                )
+                            },
+                            trailingIcon = {
+                                if (expanded) {
+                                    Button(
+                                        colors = buttonColors,
+                                        onClick = { onExpandedChange(false) },
+                                    ) {
+                                        Icon(
+                                            painterResource(R.drawable.fa_xmark),
+                                            stringResource(R.string.close_button_label),
+                                            tint = colorResource(R.color.deemphasized)
                                         )
                                     }
                                 }
-                                itemsIndexed(searchResults?.stops ?: emptyList()) { index, stop ->
-                                    val shape =
-                                        if (searchResults?.stops?.size == 1) {
-                                            RoundedCornerShape(10.dp)
-                                        } else if (searchResults?.stops?.first() == stop) {
-                                            RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
-                                        } else if (searchResults?.stops?.last() == stop) {
-                                            RoundedCornerShape(
-                                                bottomStart = 10.dp,
-                                                bottomEnd = 10.dp
-                                            )
-                                        } else {
-                                            RoundedCornerShape(0.dp)
-                                        }
-                                    if (index != 0) {
-                                        HorizontalDivider(color = colorResource(R.color.fill1))
-                                    }
-                                    StopResultsView(shape, stop, globalResponse, onStopNavigation)
-                                }
                             }
+                        )
+                    },
+                    expanded = expanded,
+                    onExpandedChange = {},
+                ) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize().background(colorResource(R.color.fill1)),
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
+                        if (searchInputState.isEmpty()) {
+                            item {
+                                Text(
+                                    modifier = Modifier.padding(bottom = 10.dp),
+                                    text = stringResource(R.string.recently_viewed),
+                                    style = MaterialTheme.typography.headlineSmall,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        }
+                        itemsIndexed(searchResults?.stops ?: emptyList()) { index, stop ->
+                            val shape =
+                                if (searchResults?.stops?.size == 1) {
+                                    RoundedCornerShape(10.dp)
+                                } else if (searchResults?.stops?.first() == stop) {
+                                    RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
+                                } else if (searchResults?.stops?.last() == stop) {
+                                    RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp)
+                                } else {
+                                    RoundedCornerShape(0.dp)
+                                }
+                            if (index != 0) {
+                                HorizontalDivider(color = colorResource(R.color.fill1))
+                            }
+                            StopResultsView(shape, stop, globalResponse, onStopNavigation)
                         }
                     }
                 }
