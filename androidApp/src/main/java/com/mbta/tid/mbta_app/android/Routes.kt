3c0580f7ddf4b82c0457465760bb49f823e26056
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-02-21 13:37:27 -0500

    feat(android.BottomNavbar): Use native tab bar for accessibility

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/Routes.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/Routes.kt
@@ -1,9 +1,23 @@
 package com.mbta.tid.mbta_app.android
 
+import androidx.navigation.NavBackStackEntry
 import kotlinx.serialization.Serializable
 
 sealed class Routes {
     @Serializable object NearbyTransit : Routes()
 
     @Serializable object More : Routes()
+
+    companion object {
+        fun fromNavBackStackEntry(backStackEntry: NavBackStackEntry?): Routes {
+
+            return if (
+                backStackEntry != null && backStackEntry.destination.route?.contains("More") == true
+            ) {
+                Routes.More
+            } else {
+                Routes.NearbyTransit
+            }
+        }
+    }
 }
