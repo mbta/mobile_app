d4ba77e304ba37467f9a2b39099558b8aae11493
jackcurtis <jcurtis3@mbta.com>
2025-02-24 11:21:50 -0500

    recovered from bad git state software dependency list

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/more/MoreLink.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/more/MoreLink.kt
@@ -16,21 +16,21 @@ import androidx.compose.ui.unit.dp
 import com.mbta.tid.mbta_app.android.R
 
 @Composable
+fun LinkIcon(isKey: Boolean) {
+    Icon(
+        painterResource(R.drawable.arrow_up_right),
+        contentDescription = stringResource(id = R.string.more_link_external),
+        modifier = Modifier.size(12.dp),
+        tint =
+            if (isKey) {
+                colorResource(R.color.fill3)
+            } else colorResource(R.color.deemphasized),
+    )
+}
+
+@Composable
 fun MoreLink(label: String, url: String, note: String? = null, isKey: Boolean = false) {
     val context = LocalContext.current
-
-    @Composable
-    fun LinkIcon() {
-        Icon(
-            painterResource(R.drawable.arrow_up_right),
-            contentDescription = stringResource(id = R.string.more_link_external),
-            modifier = Modifier.size(12.dp),
-            tint =
-                if (isKey) {
-                    colorResource(R.color.fill3)
-                } else colorResource(R.color.deemphasized),
-        )
-    }
 
     MoreButton(
         label,
@@ -44,7 +44,18 @@ fun MoreLink(label: String, url: String, note: String? = null, isKey: Boolean = 
             }
         },
         note,
-        { LinkIcon() },
+        { LinkIcon(isKey) },
+        isKey,
+    )
+}
+
+@Composable
+fun MoreLink(label: String, callback: () -> Unit, note: String? = null, isKey: Boolean = false) {
+    MoreButton(
+        label,
+        callback,
+        note,
+        { LinkIcon(isKey) },
         isKey,
     )
 }
--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/MorePage.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/pages/MorePage.kt
@@ -1,14 +1,20 @@
 package com.mbta.tid.mbta_app.android.pages
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -18,6 +24,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -26,62 +33,194 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mbta.tid.mbta_app.android.BuildConfig
 import com.mbta.tid.mbta_app.android.R
+import com.mbta.tid.mbta_app.android.more.MoreButton
 import com.mbta.tid.mbta_app.android.more.MoreSectionView
 import com.mbta.tid.mbta_app.android.more.MoreViewModel
 import com.mbta.tid.mbta_app.android.util.Typography
+import com.mbta.tid.mbta_app.model.Dependency
+import com.mbta.tid.mbta_app.model.getAllDependencies
 import org.koin.compose.koinInject
 
 @Composable
 fun MorePage(
     bottomBar: @Composable () -> Unit,
-    viewModel: MoreViewModel = MoreViewModel(LocalContext.current, koinInject())
 ) {
 
+    val navController = rememberNavController()
+    val viewModel =
+        MoreViewModel(LocalContext.current, { navController.navigate("licenses") }, koinInject())
+
     val sections by viewModel.sections.collectAsState()
+    val dependencies = Dependency.getAllDependencies()
+
     Scaffold(bottomBar = bottomBar) { outerSheetPadding ->
         Column(Modifier.padding(outerSheetPadding).background(colorResource(R.color.fill3))) {
-            Row(
-                modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-                verticalAlignment = Alignment.Bottom
-            ) {
-                Text(
-                    text = stringResource(R.string.more_title),
-                    style = Typography.title1Bold,
-                    modifier = Modifier.semantics { heading() }
-                )
-                Text(
-                    stringResource(R.string.app_version_number, BuildConfig.VERSION_NAME),
-                    style = Typography.footnote
-                )
-            }
-            HorizontalDivider()
+            NavHost(navController, startDestination = "more") {
+                composable("more") {
+                    Row(
+                        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Absolute.SpaceBetween,
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        Text(
+                            text = stringResource(R.string.more_title),
+                            style = Typography.title1Bold,
+                            modifier = Modifier.semantics { heading() }
+                        )
+                        Text(
+                            stringResource(R.string.app_version_number, BuildConfig.VERSION_NAME),
+                            style = Typography.footnote
+                        )
+                    }
+                    HorizontalDivider()
+                    Column(
+                        Modifier.verticalScroll(rememberScrollState())
+                            .background(colorResource(R.color.fill1))
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        sections.map { section ->
+                            MoreSectionView(section = section) { setting ->
+                                viewModel.toggleSetting(setting)
+                            }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painterResource(R.drawable.mbta_logo),
+                                contentDescription = null,
+                                modifier = Modifier.size(64.dp),
+                                tint = Color.Unspecified
+                            )
 
-            Column(
-                Modifier.verticalScroll(rememberScrollState())
-                    .background(colorResource(R.color.fill1))
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                sections.map { section ->
-                    MoreSectionView(section = section) { setting ->
-                        viewModel.toggleSetting(setting)
+                            Text(
+                                stringResource(R.string.more_page_footer),
+                                style = Typography.callout
+                            )
+                        }
                     }
                 }
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painterResource(R.drawable.mbta_logo),
-                        contentDescription = null,
-                        modifier = Modifier.size(64.dp),
-                        tint = Color.Unspecified
-                    )
-
-                    Text(stringResource(R.string.more_page_footer), style = Typography.callout)
+                composable("licenses") {
+                    Column(Modifier.background(colorResource(R.color.fill1))) {
+                        Row(
+                            modifier =
+                                Modifier.background(colorResource(R.color.fill3))
+                                    .padding(vertical = 8.dp, horizontal = 10.dp)
+                                    .fillMaxWidth()
+                                    .height(44.dp),
+                            horizontalArrangement = Arrangement.Absolute.SpaceAround,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Button(
+                                colors =
+                                    ButtonDefaults.buttonColors(
+                                        contentColor = colorResource(R.color.key),
+                                        containerColor = colorResource(R.color.fill3),
+                                    ),
+                                onClick = { navController.popBackStack() }
+                            ) {
+                                Icon(
+                                    painterResource(R.drawable.fa_chevron_left),
+                                    modifier = Modifier.size(14.dp),
+                                    contentDescription = null
+                                )
+                                Text(
+                                    stringResource(R.string.back_button_label),
+                                    modifier = Modifier.padding(start = 4.dp)
+                                )
+                            }
+                            Text(
+                                text = stringResource(R.string.software_licenses),
+                                style = Typography.headlineSemibold,
+                                modifier = Modifier.semantics { heading() }
+                            )
+                            Spacer(Modifier.weight(1f))
+                        }
+                        HorizontalDivider(Modifier.padding(bottom = 24.dp))
+                        Column(Modifier.padding(16.dp).verticalScroll(rememberScrollState())) {
+                            Column(
+                                modifier =
+                                    Modifier.border(
+                                            1.dp,
+                                            colorResource(R.color.halo),
+                                            RoundedCornerShape(8.dp)
+                                        )
+                                        .clip(RoundedCornerShape(8.dp))
+                            ) {
+                                dependencies.mapIndexed { index, it ->
+                                    MoreButton(
+                                        label = it.name,
+                                        action = { navController.navigate("dependency/${index}") },
+                                        icon = {
+                                            Icon(
+                                                painterResource(R.drawable.fa_chevron_right),
+                                                modifier = Modifier.size(14.dp),
+                                                contentDescription = null
+                                            )
+                                        }
+                                    )
+                                    if (index < dependencies.size - 1) {
+                                        HorizontalDivider()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                composable("dependency/{index}") { backStackEntry ->
+                    val id = backStackEntry.arguments?.getString("index") ?: ""
+                    val dependency =
+                        Dependency.getAllDependencies().getOrNull(id.toInt()) ?: return@composable
+                    Column(
+                        Modifier.background(colorResource(R.color.fill1)),
+                    ) {
+                        Row(
+                            modifier =
+                                Modifier.background(colorResource(R.color.fill3))
+                                    .padding(vertical = 8.dp, horizontal = 10.dp)
+                                    .fillMaxWidth()
+                                    .height(44.dp),
+                            horizontalArrangement = Arrangement.Absolute.Left,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Button(
+                                colors =
+                                    ButtonDefaults.buttonColors(
+                                        contentColor = colorResource(R.color.key),
+                                        containerColor = colorResource(R.color.fill3),
+                                    ),
+                                onClick = { navController.popBackStack() }
+                            ) {
+                                Icon(
+                                    painterResource(R.drawable.fa_chevron_left),
+                                    modifier = Modifier.size(14.dp),
+                                    contentDescription = null
+                                )
+                                Text(
+                                    stringResource(R.string.software_licenses),
+                                    modifier = Modifier.padding(start = 4.dp)
+                                )
+                            }
+                        }
+                        HorizontalDivider(Modifier.padding(bottom = 24.dp))
+                        Column(
+                            modifier = Modifier.padding(16.dp).verticalScroll(rememberScrollState())
+                        ) {
+                            Text(
+                                text = dependency.name,
+                                style = Typography.title2Bold,
+                                modifier = Modifier.padding(bottom = 24.dp).semantics { heading() }
+                            )
+                            Text(text = dependency.licenseText, style = Typography.body)
+                        }
+                    }
                 }
             }
         }
