4a2f77c03de8345a385929d9a478de1b663ab1ad
Jack Curtis <jack.v.curtis@gmail.com>
2025-02-27 10:15:28 -0500

    Software dependency list (#785)

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/morePage/MoreItem.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/morePage/MoreItem.kt
@@ -8,6 +8,9 @@ sealed class MoreItem {
 
     data class Link(val label: String, val url: String, val note: String? = null) : MoreItem()
 
+    data class NavLink(val label: String, val callback: () -> Unit, val note: String? = null) :
+        MoreItem()
+
     data class Phone(val label: String, val phoneNumber: String) : MoreItem()
 
     data class Toggle(val label: String, val settings: Settings, val value: Boolean) : MoreItem()
@@ -16,6 +19,7 @@ sealed class MoreItem {
         when (this) {
             is Action -> label
             is Link -> url
+            is NavLink -> label
             is Phone -> phoneNumber
             is Toggle -> settings.name
         }
