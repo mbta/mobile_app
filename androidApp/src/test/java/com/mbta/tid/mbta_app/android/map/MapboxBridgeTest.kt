de1a8262e694c9c0e384e12cefff285b035b6b67
Melody Horn <mhorn@mbta.com>
2025-03-17 16:12:42 -0600

    fix more tests

--- androidApp/src/test/java/com/mbta/tid/mbta_app/android/map/MapboxBridgeTest.kt
+++ androidApp/src/test/java/com/mbta/tid/mbta_app/android/map/MapboxBridgeTest.kt
@@ -3,12 +3,13 @@ package com.mbta.tid.mbta_app.android.map
 import com.mapbox.maps.extension.style.layers.generated.SymbolLayer as MapboxSymbolLayer
 import com.mbta.tid.mbta_app.map.style.Exp
 import com.mbta.tid.mbta_app.map.style.SymbolLayer
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MapboxBridgeTest {
     @Test
-    fun `symbol layer gets translated properly`() {
+    fun `symbol layer gets translated properly`() = runBlocking {
         val symbolLayer = SymbolLayer(id = "symbol-layer", source = "stop-source")
         symbolLayer.textField = Exp("this is text")
 
