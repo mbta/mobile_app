d95a22d5ec05bc26076f8ff7f868012eeca99e44
Emma Simon <esimon@mbta.com>
2025-03-19 13:07:43 -0400

    test: Add tests for caching updates

--- shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/cache/ResponseCacheTest.kt
+++ shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/cache/ResponseCacheTest.kt
@@ -29,7 +29,7 @@ import kotlin.time.Duration.Companion.seconds
 import kotlin.time.TimeSource
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.encodeToString
+import kotlinx.serialization.Serializable
 import okio.FileSystem
 import okio.fakefilesystem.FakeFileSystem
 import org.koin.core.context.startKoin
@@ -493,5 +493,158 @@ class ResponseCacheTest {
                 }
             )
         )
+    }
+
+    @Test
+    fun `writes full json even when some of it isn't parsed`() = runBlocking {
+        @Serializable data class PartialObject(val a: String,  val b: Int)
+        @Serializable data class CompleteObject(val a: String, val b: Int, val c: Double)
+        @Serializable data class FutureObject(val a: String, val b: Int, val c: Double, val d: String)
+
+        val partial = PartialObject("a", 1)
+        val complete = CompleteObject("a", 1, 2.0)
+        val future = FutureObject("a", 1, 2.0, "d")
+
+        val responseEtag = "etag"
+        val mockEngine = MockEngine { _ ->
+            respond(
+                content = ByteReadChannel(json.encodeToString(future)),
+                status = HttpStatusCode.OK,
+                headers =
+                    headersOf(
+                        Pair(HttpHeaders.ContentType, listOf("application/json")),
+                        Pair(HttpHeaders.ETag, listOf(responseEtag))
+                    )
+            )
+        }
+
+        val client = MobileBackendClient(mockEngine, AppVariant.Staging)
+
+        val partialCache = ResponseCache.create<PartialObject>(cacheKey = "test")
+
+        val mockPaths = MockSystemPaths(data = "data", cache = "cache")
+        val fileSystem = FakeFileSystem()
+
+        startKoin {
+            modules(
+                module {
+                    single<SystemPaths> { mockPaths }
+                    single<FileSystem> { fileSystem }
+                }
+            )
+        }
+
+        var didFetch = false
+
+        assertEquals(
+            ApiResult.Ok(partial),
+            partialCache.getOrFetch {
+                didFetch = true
+                client.get { url { path("api/global") } }
+            }
+        )
+        assertTrue(didFetch)
+
+        assertEquals(
+            future,
+            json.decodeFromString(
+                fileSystem.read(mockPaths.cache / ResponseCache.CACHE_SUBDIRECTORY / "test.json") {
+                    readUtf8()
+                }
+            )
+        )
+
+        val completeCache = ResponseCache.create<CompleteObject>(cacheKey = "test")
+        assertEquals(
+            ApiResult.Ok(complete),
+            completeCache.getOrFetch { fail("Should not fetch again") }
+        )
+    }
+
+    @Test
+    fun `cache is invalidated by changing the invalidation key`() = runBlocking {
+        val objects = ObjectCollectionBuilder()
+        objects.stop()
+        objects.stop()
+        objects.route()
+        val globalData = GlobalResponse(objects, emptyMap())
+
+        val responseEtag = "etag"
+        val mockEngine = MockEngine { _ ->
+            respond(
+                content = ByteReadChannel(json.encodeToString(globalData)),
+                status = HttpStatusCode.OK,
+                headers =
+                    headersOf(
+                        Pair(HttpHeaders.ContentType, listOf("application/json")),
+                        Pair(HttpHeaders.ETag, listOf(responseEtag))
+                    )
+            )
+        }
+
+        val client = MobileBackendClient(mockEngine, AppVariant.Staging)
+
+        val cache = ResponseCache.create<GlobalResponse>(cacheKey = "test")
+
+        val mockPaths = MockSystemPaths(data = "data", cache = "cache")
+        val fileSystem = FakeFileSystem()
+        val directory = mockPaths.cache / ResponseCache.CACHE_SUBDIRECTORY
+        fileSystem.createDirectories(directory)
+
+        startKoin {
+            modules(
+                module {
+                    single<SystemPaths> { mockPaths }
+                    single<FileSystem> { fileSystem }
+                }
+            )
+        }
+
+        var fetchCount = 0
+        suspend fun fetch(): HttpResponse {
+            fetchCount++
+            return client.get { url { path("api/global") } }
+        }
+
+        assertEquals(
+            ApiResult.Ok(globalData),
+            cache.getOrFetch { fetch() }
+        )
+        assertEquals(1, fetchCount)
+        assertEquals(ApiResult.Ok(globalData), cache.getOrFetch { fail() })
+
+        fun metadataFromDisk() = json
+            .decodeFromString<ResponseMetadata>(
+                fileSystem.read(
+                    mockPaths.cache / ResponseCache.CACHE_SUBDIRECTORY / "test-meta.json"
+                ) {
+                    readUtf8()
+                }
+            )
+
+        assertEquals(responseEtag, metadataFromDisk().etag)
+        assertNull(cache.data!!.metadata.invalidationKey)
+
+        val firstInvalidationKey = "key1"
+        val invalidatedCache = ResponseCache.create<GlobalResponse>(cacheKey = "test", invalidationKey = firstInvalidationKey)
+        assertEquals(
+            ApiResult.Ok(globalData),
+            invalidatedCache.getOrFetch { fetch() }
+        )
+        assertEquals(2, fetchCount)
+        assertEquals(responseEtag, metadataFromDisk().etag)
+        assertEquals(firstInvalidationKey, metadataFromDisk().invalidationKey)
+        assertEquals(ApiResult.Ok(globalData), cache.getOrFetch { fail() })
+
+        val secondInvalidationKey = "key2"
+        val otherInvalidatedCache = ResponseCache.create<GlobalResponse>(cacheKey = "test", invalidationKey = secondInvalidationKey)
+        assertEquals(
+            ApiResult.Ok(globalData),
+            otherInvalidatedCache.getOrFetch { fetch() }
+        )
+        assertEquals(3, fetchCount)
+        assertEquals(responseEtag, metadataFromDisk().etag)
+        assertEquals(secondInvalidationKey, metadataFromDisk().invalidationKey)
+        assertEquals(ApiResult.Ok(globalData), cache.getOrFetch { fail() })
     }
 }
