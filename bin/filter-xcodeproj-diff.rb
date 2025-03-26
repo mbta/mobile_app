016ae030864a1e098793aa72e6bb81f5b9d01124
Melody Horn <mhorn@mbta.com>
2025-03-26 12:53:11 -0600

    use Rubocop for formatting and linting

--- bin/filter-xcodeproj-diff.rb
+++ bin/filter-xcodeproj-diff.rb
@@ -1,48 +1,82 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'yaml'
 
 module DiffCrawler
   refine Hash do
-    def crawl!(path = "")
-      if self.keys.size == 2 and self.keys.count {|el| el.include?("diff-xcodeproj.sh")} == 1
-        dirty_key = self.keys.find {|el| not el.include?("diff-xcodeproj.sh")}
-        gen_key = self.keys.find {|el| el.include?("diff-xcodeproj.sh")}
-        self["dirty"] = self.delete(dirty_key)
-        self["generated"] = self.delete(gen_key)
-        if path.end_with?("lastKnownFileType") and ["text", "text.xml", "file"].include?(self["dirty"]) and self["generated"] == nil
-          self.replace({})
-        end
-        if path == ".objectVersion"
-          self.replace({})
-        end
-        if path == ".rootObject.mainGroup.children.Pods.name" and self["dirty"] == nil and self["generated"] == "Pods"
-          self.replace({})
-        end
+    def crawl!(path = '')
+      if leaf?
+        replace_leaf_keys!
+        ignore_leaf_noise!(path)
       else
-        for k in self.keys
-          if self[k].is_a? Hash
-            self[k].crawl!(path + "." + k)
-            if self[k].empty?
-              self.delete(k)
-            end
-          end
-        end
-        if (self["explicitFileType"]&.fetch("dirty") || :nil_and_shouldnt_count) == self["lastKnownFileType"]&.fetch("generated") and self["explicitFileType"]["generated"] == nil and self["lastKnownFileType"]["dirty"] == nil
-          self.delete("explicitFileType")
-          self.delete("lastKnownFileType")
-        end
+        crawl_recursive!(path)
+        ignore_file_type_becoming_explicit!(path)
       end
+    end
+
+    def leaf?
+      (keys.size == 2) && (keys.count { |el| el.include?('diff-xcodeproj.sh') } == 1)
+    end
+
+    def replace_leaf_keys!
+      dirty_key = keys.find { |el| !el.include?('diff-xcodeproj.sh') }
+      gen_key = keys.find { |el| el.include?('diff-xcodeproj.sh') }
+      self['dirty'] = delete(dirty_key)
+      self['generated'] = delete(gen_key)
+    end
+
+    def ignore_leaf_noise!(path)
+      ignore_added_file_type!(path)
+      ignore_object_version!(path)
+      ignore_pods_name!(path)
+    end
+
+    def ignore_added_file_type!(path)
+      boring_file_types = ['text', 'text.xml', 'file']
+      if path.end_with?('lastKnownFileType') && boring_file_types.include?(self['dirty']) && self['generated'].nil?
+        replace({})
+      end
+    end
+
+    def ignore_object_version!(path)
+      return unless path == '.objectVersion'
+
+      replace({})
+    end
+
+    def ignore_pods_name!(path)
+      if (path == '.rootObject.mainGroup.children.Pods.name') && self['dirty'].nil? && (self['generated'] == 'Pods')
+        replace({})
+      end
+    end
+
+    def crawl_recursive!(path)
+      keys.each do |k|
+        next unless self[k].is_a? Hash
+
+        self[k].crawl!("#{path}.#{k}")
+        delete(k) if self[k].empty?
+      end
+    end
+
+    def ignore_file_type_becoming_explicit!(_path)
+      explicit = self['explicitFileType'] || return
+      last_known = self['lastKnownFileType'] || return
+      unless explicit['dirty'] == last_known['generated'] && explicit['generated'].nil? && last_known['dirty'].nil?
+        return
+      end
+
+      delete('explicitFileType')
+      delete('lastKnownFileType')
     end
   end
 end
 
 using DiffCrawler
 
-data = YAML.safe_load_file(ARGV[0] || fail("usage: filter-xcodeproj-diff.rb path/to/diff.yml")) || {}
+data = YAML.safe_load_file(ARGV[0] || raise('usage: filter-xcodeproj-diff.rb path/to/diff.yml')) || {}
 
 data.crawl!
 
-unless data.empty?
-  puts(YAML.dump(data).sub("---\n", ""))
-end
+puts(YAML.dump(data).sub("---\n", '')) unless data.empty?
