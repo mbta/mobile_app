2d05c0761688508c3c6983a8672d9f34d1ba7f77
Melody Horn <mhorn@mbta.com>
2025-03-27 15:25:29 -0600

    build: use XcodeGen to control Xcode project (#846)
    * build: use XcodeGen to control Xcode project
    
    * fix the obvious issue in github actions
    
    * i hate curl sometimes
    
    * correctly specify path to binary
    
    * use pre-commit to check for Xcode project drift
    
    * filter out the easiest noise to catch in the diff
    
    * use Rubocop for formatting and linting
    
    * set up Ruby for pre-commit for Rubocop

--- bin/generate-xcodeproj.sh
+++ bin/generate-xcodeproj.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 set -e
 
-# Installs the CocoaPods dependencies and updates the Gradle cache to recognize that the
-# dependencies are installed.
+# Generates iosApp.xcodeproj and iosApp.xcworkspace based on project.yml.
 
 cd "$(dirname "$0")"/..
 ls gradlew > /dev/null 2>/dev/null || (echo "Failed to move to repo root" && exit 1)
@@ -10,4 +9,3 @@ pushd iosApp
 xcodegen generate
 bundle exec pod install
 popd
-./gradlew :shared:podInstallSyntheticIos
