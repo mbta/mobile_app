a6f50ce87655e4a372f9e49f6ce87bc820e16ff9
Melody Horn <mhorn@mbta.com>
2025-04-28 14:59:47 -0600

    document the SettingsCache API

--- iosApp/iosApp/Utils/SettingsCache.swift
+++ iosApp/iosApp/Utils/SettingsCache.swift
@@ -9,6 +9,7 @@
 import Shared
 import SwiftUI
 
+/// Stores the state of the `Settings` so that they can be read instantly from anywhere once they have been loaded.
 class SettingsCache: ObservableObject {
     var settingsRepo: ISettingsRepository
     @Published var cache: [Settings: Bool]?
@@ -18,14 +19,19 @@ class SettingsCache: ObservableObject {
         self.cache = cache
     }
 
+    /// Retrieves the state of a `setting` from the cache.
+    ///
+    /// Unlike the Android version, does not transparently load in the background.
     func get(_ setting: Settings) -> Bool {
         cache?[setting] ?? false
     }
 
+    /// Loads the cache from the settings repository. Should be called by `SettingsCacheProvider` in the full app.
     func load() async throws {
         cache = try await settingsRepo.getSettings().mapValues { $0.boolValue }
     }
 
+    /// Edits the value of a single `setting` in both the cache and the settings repository.
     func set(_ setting: Settings, _ value: Bool) {
         let newSettings = [setting: value]
         cache = newSettings.merging(cache ?? [:]) { newValue, _ in newValue }
@@ -35,7 +41,7 @@ class SettingsCache: ObservableObject {
     }
 }
 
-/// Struggling to inject environment values because your unit tests are a complete disaster of pointer manipulation?
+/// Struggling to inject environment values because your unit tests are a house of cards of pointer manipulation?
 /// Simply put them in the default fallback! Nothing could possibly go wrong!
 ///
 /// Always call `reset` if you called `set` or `setRepo`. Use `defer`, probably.
@@ -60,6 +66,7 @@ extension EnvironmentValues {
     @Entry var settingsCache: SettingsCache = DefaultSettings.cache
 }
 
+/// Provides the `EnvironmentValues.settingsCache` to the `content` and also loads the cache data.
 struct SettingsCacheProvider<Content: View>: View {
     let content: () -> Content
     @State var cache = SettingsCache()
@@ -71,6 +78,9 @@ struct SettingsCacheProvider<Content: View>: View {
     }
 }
 
+/// Retrieves the value of an individual setting from a `SettingsCache` retrieved from the environment.
+///
+/// May or may not actually update the component when the settings cache finishes loading, but in practice seems to.
 @propertyWrapper struct GetSetting: DynamicProperty {
     let setting: Settings
     @Environment(\.settingsCache) var settingsCache: SettingsCache
