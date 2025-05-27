1249c54e371ea6c5db3ae8c3f7981b02a16ccf07
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-05-27 15:47:11 -0400

    fix(iOS.ContentView): Don't stop recording sheet height at 50%

--- iosApp/iosApp/Utils/PresentationDetentExtension.swift
+++ iosApp/iosApp/Utils/PresentationDetentExtension.swift
@@ -10,8 +10,10 @@ import SwiftUI
 import UIKit
 
 extension PresentationDetent {
+    static let MEDIUM_DETENT_FRACTION = 0.6
+
     static let small = Self.height(150)
-    static let medium = Self.fraction(0.6)
+    static let medium = Self.fraction(MEDIUM_DETENT_FRACTION)
     static let almostFull = Self.custom(AlmostFull.self)
 }
 
