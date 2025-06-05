5b6e491b3a60aa332cbd56a96a51d7ac39588f9c
Melody Horn <mhorn@mbta.com>
2025-06-05 13:31:23 -0600

    fix(Android): add border on recenter button

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/map/RecenterButton.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/map/RecenterButton.kt
@@ -1,35 +1,55 @@
 package com.mbta.tid.mbta_app.android.map
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mbta.tid.mbta_app.android.R
 
 @Composable
-fun RecenterButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
-    IconButton(
-        onClick = onClick,
-        modifier.semantics(mergeDescendants = true) {},
-        colors =
-            IconButtonDefaults.iconButtonColors(
-                containerColor = colorResource(R.color.fill3),
-                contentColor = colorResource(R.color.key),
-            ),
+fun RecenterButton(painter: Painter, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Box(
+        modifier =
+            modifier
+                .semantics(mergeDescendants = true) {}
+                .clickable(onClick = onClick, role = Role.Button)
+                .size(44.dp)
+                .background(colorResource(R.color.halo), CircleShape),
+        contentAlignment = Alignment.Center,
     ) {
-        Icon(Icons.Default.LocationOn, stringResource(R.string.recenter))
+        Box(
+            Modifier.size(40.dp)
+                .background(color = colorResource(R.color.fill3), shape = CircleShape),
+            contentAlignment = Alignment.Center,
+        ) {
+            Icon(painter, stringResource(R.string.recenter), tint = colorResource(R.color.key))
+        }
     }
+}
+
+@Composable
+fun RecenterButton(imageVector: ImageVector, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    RecenterButton(rememberVectorPainter(imageVector), modifier, onClick)
 }
 
 @Preview
 @Composable
 fun RecenterButtonPreview() {
-    RecenterButton(onClick = {})
+    RecenterButton(Icons.Default.LocationOn, onClick = {})
 }
