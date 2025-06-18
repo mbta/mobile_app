5cf5648231aae5ac3dfbb73b5af93b8ab64a276e
Emma Simon <esimon@mbta.com>
2025-06-18 12:14:25 -0400

    refactor(iOS): Consolidate trip VoiceOver labels

--- iosApp/iosApp/ComponentViews/UpcomingTripView.swift
+++ iosApp/iosApp/ComponentViews/UpcomingTripView.swift
@@ -25,8 +25,6 @@ struct UpcomingTripView: View {
     var isOnly: Bool = true
     var maxTextAlpha: Double = 1.0
 
-    let accessibilityFormatters = UpcomingTripAccessibilityFormatters()
-
     private static let subjectSpacing: CGFloat = 4
     @ScaledMetric private var iconSize: CGFloat = 16
 
@@ -44,143 +42,105 @@ struct UpcomingTripView: View {
     }
 
     @ViewBuilder
-    var predictionView: some View {
-        switch prediction {
-        case let .some(prediction):
-            switch onEnum(of: prediction) {
-            case let .overridden(overridden):
-                Text(overridden.textWithLocale()).realtime(hideIndicator: hideRealtimeIndicators)
-                    .opacity(min(1.0, maxTextAlpha))
-            case .hidden, .skipped:
-                // should have been filtered out already
-                Text(verbatim: "")
-            case .now:
-                Text("Now", comment: "Label for a trip that's arriving right now")
-                    .font(Typography.headlineBold)
-                    .realtime(hideIndicator: hideRealtimeIndicators)
-                    .opacity(min(1.0, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters
-                        .arrivingFirst(vehicleText: routeType?.typeText(isOnly: isOnly) ?? "")
-                        : accessibilityFormatters.arrivingOther())
-            case .boarding:
-                Text("BRD", comment: "Shorthand for boarding")
-                    .font(Typography.headlineBold)
-                    .realtime(hideIndicator: hideRealtimeIndicators)
-                    .opacity(min(1.0, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters
-                        .boardingFirst(vehicleText: routeType?.typeText(isOnly: isOnly) ?? "")
-                        : accessibilityFormatters.boardingOther())
-            case .arriving:
-                Text("ARR", comment: "Shorthand for arriving")
-                    .font(Typography.headlineBold)
-                    .realtime(hideIndicator: hideRealtimeIndicators)
-                    .opacity(min(1.0, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters
-                        .arrivingFirst(vehicleText: routeType?.typeText(isOnly: isOnly) ?? "")
-                        : accessibilityFormatters.arrivingOther())
-            case .approaching:
-                PredictionText(minutes: 1)
-                    .realtime(hideIndicator: hideRealtimeIndicators)
-                    .opacity(min(1.0, maxTextAlpha))
-            case let .time(format):
+    func tripInstantView(_ prediction: TripInstantDisplay) -> some View {
+        let label = prediction.accessibilityLabel(
+            isFirst: isFirst,
+            vehicleType: routeType?.typeText(isOnly: isOnly) ?? ""
+        )
+        switch onEnum(of: prediction) {
+        case let .overridden(overridden):
+            Text(overridden.textWithLocale()).realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+        case .hidden, .skipped:
+            // should have been filtered out already
+            Text(verbatim: "")
+        case .now:
+            Text("Now", comment: "Label for a trip that's arriving right now")
+                .font(Typography.headlineBold)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case .boarding:
+            Text("BRD", comment: "Shorthand for boarding")
+                .font(Typography.headlineBold)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case .arriving:
+            Text("ARR", comment: "Shorthand for arriving")
+                .font(Typography.headlineBold)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case .approaching:
+            PredictionText(minutes: 1)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case let .time(format):
+            Text(Date(instant: format.predictionTime), style: .time)
+                .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case let .timeWithStatus(format):
+            VStack(alignment: .trailing, spacing: 0) {
                 Text(Date(instant: format.predictionTime), style: .time)
                     .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
                     .realtime(hideIndicator: hideRealtimeIndicators)
                     .opacity(min(1.0, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters.distantFutureFirst(
-                            date: format.predictionTime.toNSDate(),
-                            vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                        )
-                        : accessibilityFormatters
-                        .distantFutureOther(date: format.predictionTime.toNSDate()))
-            case let .timeWithStatus(format):
-                VStack(alignment: .trailing, spacing: 0) {
-                    Text(Date(instant: format.predictionTime), style: .time)
-                        .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
-                        .realtime(hideIndicator: hideRealtimeIndicators)
-                        .opacity(min(1.0, maxTextAlpha))
-                        .accessibilityLabel(isFirst
-                            ? accessibilityFormatters.distantFutureFirst(
-                                date: format.predictionTime.toNSDate(),
-                                vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                            )
-                            : accessibilityFormatters
-                            .distantFutureOther(date: format.predictionTime.toNSDate()))
-                    Text(format.status)
-                        .font(Typography.footnoteSemibold)
-                        .opacity(min(0.6, maxTextAlpha))
-                        .multilineTextAlignment(.trailing)
-                }
-            case let .timeWithSchedule(format):
-                VStack(alignment: .trailing, spacing: 0) {
-                    Text(Date(instant: format.predictionTime), style: .time)
-                        .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
-                        .realtime(hideIndicator: hideRealtimeIndicators)
-                        .opacity(min(1.0, maxTextAlpha))
-                        .accessibilityLabel(isFirst
-                            ? accessibilityFormatters.distantFutureFirst(
-                                date: format.predictionTime.toNSDate(),
-                                vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                            )
-                            : accessibilityFormatters
-                            .distantFutureOther(date: format.predictionTime.toNSDate()))
-                    Text(Date(instant: format.scheduledTime), style: .time)
-                        .font(Typography.footnoteSemibold)
-                        .opacity(min(0.6, maxTextAlpha))
-                        .strikethrough()
-                        .multilineTextAlignment(.trailing)
-                }
-            case let .minutes(format):
-                PredictionText(minutes: format.minutes)
+                    .accessibilityLabel(label)
+                Text(format.status)
+                    .font(Typography.footnoteSemibold)
+                    .opacity(min(0.6, maxTextAlpha))
+                    .multilineTextAlignment(.trailing)
+            }
+        case let .timeWithSchedule(format):
+            VStack(alignment: .trailing, spacing: 0) {
+                Text(Date(instant: format.predictionTime), style: .time)
+                    .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
                     .realtime(hideIndicator: hideRealtimeIndicators)
                     .opacity(min(1.0, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters.predictionMinutesFirst(
-                            minutes: format.minutes,
-                            vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                        )
-                        : accessibilityFormatters.predictionMinutesOther(minutes: format.minutes))
-            case let .scheduleTime(format):
-                Text(format.scheduledTime.toNSDate(), style: .time)
+                    .accessibilityLabel(label)
+                Text(Date(instant: format.scheduledTime), style: .time)
+                    .font(Typography.footnoteSemibold)
                     .opacity(min(0.6, maxTextAlpha))
-                    .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters.scheduleTimeFirst(
-                            date: format.scheduledTime.toNSDate(),
-                            vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                        )
-                        : accessibilityFormatters.scheduleTimeOther(date: format.scheduledTime.toNSDate()))
-            case let .scheduleMinutes(format):
-                PredictionText(minutes: format.minutes)
-                    .opacity(min(0.6, maxTextAlpha))
-                    .accessibilityLabel(isFirst
-                        ? accessibilityFormatters.scheduleMinutesFirst(
-                            minutes: format.minutes,
-                            vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                        )
-                        : accessibilityFormatters.scheduleMinutesOther(minutes: format.minutes))
-            case let .cancelled(format):
-                HStack(spacing: Self.subjectSpacing) {
-                    Text("Cancelled", comment: "The status label for a cancelled trip")
-                        .font(Typography.footnote)
-                        .opacity(min(0.6, maxTextAlpha))
-                    Text(format.scheduledTime.toNSDate(), style: .time)
-                        .font(Typography.footnoteSemibold)
-                        .strikethrough()
-                        .opacity(min(0.6, maxTextAlpha))
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel(isFirst
-                    ? accessibilityFormatters.cancelledFirst(
-                        date: format.scheduledTime.toNSDate(),
-                        vehicleText: routeType?.typeText(isOnly: isOnly) ?? ""
-                    )
-                    : accessibilityFormatters.cancelledOther(date: format.scheduledTime.toNSDate()))
+                    .strikethrough()
+                    .multilineTextAlignment(.trailing)
             }
+        case let .minutes(format):
+            PredictionText(minutes: format.minutes)
+                .realtime(hideIndicator: hideRealtimeIndicators)
+                .opacity(min(1.0, maxTextAlpha))
+                .accessibilityLabel(label)
+        case let .scheduleTime(format):
+            Text(format.scheduledTime.toNSDate(), style: .time)
+                .opacity(min(0.6, maxTextAlpha))
+                .font(format.headline ? Typography.headlineSemibold : Typography.footnoteSemibold)
+                .accessibilityLabel(label)
+        case let .scheduleMinutes(format):
+            PredictionText(minutes: format.minutes)
+                .opacity(min(0.6, maxTextAlpha))
+                .accessibilityLabel(label)
+        case let .cancelled(format):
+            HStack(spacing: Self.subjectSpacing) {
+                Text("Cancelled", comment: "The status label for a cancelled trip")
+                    .font(Typography.footnote)
+                    .opacity(min(0.6, maxTextAlpha))
+                Text(format.scheduledTime.toNSDate(), style: .time)
+                    .font(Typography.footnoteSemibold)
+                    .strikethrough()
+                    .opacity(min(0.6, maxTextAlpha))
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(label)
+        }
+    }
+
+    @ViewBuilder
+    var predictionView: some View {
+        switch prediction {
+        case let .some(prediction): tripInstantView(prediction)
         case let .disruption(formattedAlert, iconName: iconName):
             DisruptionView(spec: formattedAlert.predictionReplacement, iconName: iconName, maxTextAlpha: maxTextAlpha)
         case let .noTrips(format):
