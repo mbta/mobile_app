56dbc128c35c205acda1cc9a5a7b900cf4e5c485
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-06-24 10:01:37 -0400

    test(android.FavoriteConfirmation): Add single direction cases

--- androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/component/FavoriteConfirmationTest.kt
+++ androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/component/FavoriteConfirmationTest.kt
@@ -3,6 +3,7 @@ package com.mbta.tid.mbta_app.android.component
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import com.mbta.tid.mbta_app.android.testUtils.waitUntilDefaultTimeout
 import com.mbta.tid.mbta_app.model.Direction
 import com.mbta.tid.mbta_app.model.RouteCardData
 import com.mbta.tid.mbta_app.model.RouteStopDirection
@@ -13,17 +14,15 @@ import kotlin.test.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
-class FavoriteConfirmationDialogTest {
+class FavoriteConfirmationTest {
 
     @get:Rule val composeTestRule = createComposeRule()
 
     val line = RouteCardData.LineOrRoute.Line(TestData.getLine("line-Green"), emptySet())
     val stop = TestData.getStop("place-boyls")
-    val directions =
-        listOf(
-            Direction(id = 0, name = "West", destination = "Copley & West"),
-            Direction(id = 1, name = "East", destination = "Park St & North"),
-        )
+    val direction0 = Direction(id = 0, name = "West", destination = "Copley & West")
+    val direction1 = Direction(id = 1, name = "East", destination = "Park St & North")
+    val directions = listOf(direction0, direction1)
 
     @Test
     fun testWithoutTappingAnyButtonSavesProposedChanges() {
@@ -150,6 +149,114 @@ class FavoriteConfirmationDialogTest {
         assertEquals(
             updateFavoritesCalledFor,
             mapOf(RouteStopDirection(line.id, stop.id, 0) to false),
+        )
+    }
+
+    @Test
+    fun testFavoritingOnlyDirectionUpdatesFavoritesWithoutDialog() {
+        var updateFavoritesCalledFor: Map<RouteStopDirection, Boolean> = mapOf()
+        var onCloseCalled = false
+
+        composeTestRule.setContent {
+            FavoriteConfirmation(
+                lineOrRoute = line,
+                stop = stop,
+                directions = listOf(direction0),
+                selectedDirection = 0,
+                isFavorite = { false },
+                updateFavorites = { updateFavoritesCalledFor = it },
+                onClose = { onCloseCalled = true },
+            )
+        }
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilDefaultTimeout { onCloseCalled }
+        assertTrue(onCloseCalled)
+        assertEquals(
+            updateFavoritesCalledFor,
+            mapOf(RouteStopDirection(line.id, stop.id, 0) to true),
+        )
+    }
+
+    @Test
+    fun testUnfavoritingOnlyDirectionUpdatesFavoritesWithoutDialog() {
+        var updateFavoritesCalledFor: Map<RouteStopDirection, Boolean> = mapOf()
+        var onCloseCalled = false
+
+        composeTestRule.setContent {
+            FavoriteConfirmation(
+                lineOrRoute = line,
+                stop = stop,
+                directions = listOf(direction0),
+                selectedDirection = 0,
+                isFavorite = { true },
+                updateFavorites = { updateFavoritesCalledFor = it },
+                onClose = { onCloseCalled = true },
+            )
+        }
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilDefaultTimeout { onCloseCalled }
+        assertTrue(onCloseCalled)
+        assertEquals(
+            updateFavoritesCalledFor,
+            mapOf(RouteStopDirection(line.id, stop.id, 0) to false),
+        )
+    }
+
+    @Test
+    fun testFavoritingWhenOnlyDirectionIsOppositePresentsDialog() {
+        var updateFavoritesCalledFor: Map<RouteStopDirection, Boolean> = mapOf()
+        var onCloseCalled = false
+
+        composeTestRule.setContent {
+            FavoriteConfirmation(
+                lineOrRoute = line,
+                stop = stop,
+                directions = listOf(direction1),
+                selectedDirection = 0,
+                isFavorite = { false },
+                updateFavorites = { updateFavoritesCalledFor = it },
+                onClose = { onCloseCalled = true },
+            )
+        }
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithText("East", substring = true).performClick()
+        composeTestRule.onNodeWithText("Add").performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilDefaultTimeout { onCloseCalled }
+        assertTrue(onCloseCalled)
+        assertEquals(
+            updateFavoritesCalledFor,
+            mapOf(RouteStopDirection(line.id, stop.id, 1) to true),
+        )
+    }
+
+    @Test
+    fun testFavoritingWhenTwoDirectionsPresentsDialog() {
+        var updateFavoritesCalledFor: Map<RouteStopDirection, Boolean> = mapOf()
+        var onCloseCalled = false
+
+        composeTestRule.setContent {
+            FavoriteConfirmation(
+                lineOrRoute = line,
+                stop = stop,
+                directions = directions,
+                selectedDirection = 0,
+                isFavorite = { rsd -> rsd.direction == 1 },
+                updateFavorites = { updateFavoritesCalledFor = it },
+                onClose = { onCloseCalled = true },
+            )
+        }
+        composeTestRule.waitForIdle()
+        composeTestRule.onNodeWithText("Add").performClick()
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilDefaultTimeout { onCloseCalled }
+        assertTrue(onCloseCalled)
+        assertEquals(
+            updateFavoritesCalledFor,
+            mapOf(
+                RouteStopDirection(line.id, stop.id, 0) to true,
+                RouteStopDirection(line.id, stop.id, 1) to true,
+            ),
         )
     }
 }
