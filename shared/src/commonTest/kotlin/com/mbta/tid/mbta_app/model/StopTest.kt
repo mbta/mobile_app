c291fbba57bddc166b9b6693618f00583e427e62
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-06-24 09:42:15 -0400

    test(Stop.isLastStopForAllPatterns)

--- shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/model/StopTest.kt
+++ shared/src/commonTest/kotlin/com/mbta/tid/mbta_app/model/StopTest.kt
@@ -68,4 +68,116 @@ class StopTest {
             Stop.resolvedParentToAllStops(listOf(child1.id, standalone.id), GlobalResponse(objects)),
         )
     }
+
+    @Test
+    fun `isLastSTopForAllPatterns true when last stop in every pattern appears in`() {
+        val objects = ObjectCollectionBuilder()
+        val targetParent =
+            objects.stop {
+                id = "targetParent"
+                locationType = LocationType.STATION
+                childStopIds = listOf("targetChild")
+            }
+        val targetChild =
+            objects.stop {
+                id = "targetChild"
+                locationType = LocationType.STOP
+                parentStationId = "targetParent"
+            }
+
+        val other1 = objects.stop {}
+        val other2 = objects.stop {}
+        val other3 = objects.stop {}
+
+        val route = objects.route()
+
+        val rp1 =
+            objects.routePattern(route) {
+                directionId = 0
+                representativeTrip {
+                    directionId = 0
+                    stopIds = listOf(other1.id, other2.id, targetChild.id)
+                }
+            }
+
+        val rp1Direction1 =
+            objects.routePattern(route) {
+                directionId = 1
+                representativeTrip {
+                    directionId = 1
+                    stopIds = listOf(targetChild.id, other2.id, other1.id)
+                }
+            }
+
+        val rp2 =
+            objects.routePattern(route) {
+                directionId = 0
+                representativeTrip {
+                    directionId = 0
+                    stopIds = listOf(other1.id, other2.id, other3.id)
+                }
+            }
+
+        val global = GlobalResponse(objects)
+
+        assertTrue(
+            targetParent.isLastStopForAllPatterns(0, listOf(rp1, rp1Direction1, rp2), global)
+        )
+    }
+
+    @Test
+    fun `isLastSTopForAllPatterns false when mid-route in a pattern`() {
+        val objects = ObjectCollectionBuilder()
+        val targetParent =
+            objects.stop {
+                id = "targetParent"
+                locationType = LocationType.STATION
+                childStopIds = listOf("targetChild")
+            }
+        val targetChild =
+            objects.stop {
+                id = "targetChild"
+                locationType = LocationType.STOP
+                parentStationId = "targetParent"
+            }
+
+        val other1 = objects.stop {}
+        val other2 = objects.stop {}
+        val other3 = objects.stop {}
+
+        val route = objects.route()
+
+        val rp1 =
+            objects.routePattern(route) {
+                directionId = 0
+                representativeTrip {
+                    directionId = 0
+                    stopIds = listOf(other1.id, targetChild.id, other2.id)
+                }
+            }
+
+        val rp1Direction1 =
+            objects.routePattern(route) {
+                directionId = 1
+                representativeTrip {
+                    directionId = 1
+                    stopIds = listOf(other2.id, targetChild.id, other1.id)
+                }
+            }
+
+        val rp2 =
+            objects.routePattern(route) {
+                directionId = 0
+                representativeTrip {
+                    directionId = 0
+                    stopIds = listOf(other1.id, other2.id, other3.id)
+                }
+            }
+
+        val global = GlobalResponse(objects)
+
+        assertFalse(
+            targetParent.isLastStopForAllPatterns(0, listOf(rp1, rp1Direction1, rp2), global)
+        )
+    }
 }
