99caa22a5207122f0d40cba661a35e512086a42d
Emma Simon <esimon@mbta.com>
2025-07-02 15:51:48 -0400

    fix(Android): Tweak direction toggle to match spec

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/DirectionPicker.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/DirectionPicker.kt
@@ -1,19 +1,20 @@
 package com.mbta.tid.mbta_app.android.stopDetails
 
 import androidx.annotation.ColorRes
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
@@ -23,6 +24,7 @@ import com.mbta.tid.mbta_app.android.MyApplicationTheme
 import com.mbta.tid.mbta_app.android.R
 import com.mbta.tid.mbta_app.android.component.DirectionLabel
 import com.mbta.tid.mbta_app.android.util.fromHex
+import com.mbta.tid.mbta_app.android.util.modifiers.haloContainer
 import com.mbta.tid.mbta_app.model.Direction
 import com.mbta.tid.mbta_app.model.ObjectCollectionBuilder
 import com.mbta.tid.mbta_app.model.Route
@@ -40,24 +42,23 @@ fun DirectionPicker(
 ) {
     if (availableDirections.size > 1) {
         val deselectedBackgroundColor =
-            colorResource(deselectedBackgroundColor(route)).copy(alpha = 0.6f)
+            colorResource(deselectedBackgroundColor(route))
+                .copy(alpha = 0.6f)
+                .compositeOver(Color.fromHex(route.color))
+
         TabRow(
             modifier =
                 modifier
-                    .padding(horizontal = 2.dp)
-                    .background(
-                        deselectedBackgroundColor.copy(alpha = 0.6f),
-                        RoundedCornerShape(8.dp),
-                    )
-                    .padding(2.dp)
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(6.dp)),
+                    .haloContainer(2.dp, deselectedBackgroundColor, Color.Transparent, 6.dp)
+                    .fillMaxWidth(),
+            containerColor = deselectedBackgroundColor,
             selectedTabIndex = selectedDirectionId ?: 0,
             indicator = {},
             divider = {},
         ) {
             for (direction in availableDirections) {
                 val isSelected = selectedDirectionId == direction
+                val selectedDirection = directions[(direction)]
                 val action = { updateDirectionId(direction) }
 
                 Tab(
@@ -65,18 +66,22 @@ fun DirectionPicker(
                     onClick = action,
                     modifier =
                         Modifier.fillMaxHeight()
-                            .background(Color.fromHex(route.color))
-                            .background(deselectedBackgroundColor)
-                            .background(
+                            .haloContainer(
+                                0.dp,
+                                Color.Transparent,
                                 if (isSelected) Color.fromHex(route.color) else Color.Transparent,
-                                shape = if (isSelected) RoundedCornerShape(6.dp) else RectangleShape,
-                            )
-                            .clip(RoundedCornerShape(6.dp)),
+                                6.dp,
+                            ),
                     selectedContentColor = Color.fromHex(route.textColor),
                     unselectedContentColor = colorResource(R.color.deselected_toggle_text),
                 ) {
-                    Column(modifier = Modifier.padding(8.dp)) {
-                        DirectionLabel(direction = directions[(direction)])
+                    Row(
+                        Modifier.fillMaxWidth().heightIn(min = 44.dp).padding(8.dp),
+                        if (selectedDirection.destination == null) Arrangement.Center
+                        else Arrangement.Start,
+                        Alignment.CenterVertically,
+                    ) {
+                        DirectionLabel(direction = selectedDirection)
                     }
                 }
             }
@@ -85,7 +90,7 @@ fun DirectionPicker(
         availableDirections.firstOrNull()?.let {
             DirectionLabel(
                 direction = directions[it],
-                modifier.padding(horizontal = 8.dp).fillMaxWidth().semantics(
+                modifier.padding(horizontal = 12.dp).fillMaxWidth().semantics(
                     mergeDescendants = true
                 ) {
                     heading()
@@ -100,22 +105,60 @@ fun DirectionPicker(
 @Composable
 private fun DirectionPickerPreview() {
     val objects = ObjectCollectionBuilder()
-    val route =
+    val red =
+        objects.route {
+            color = "DA291C"
+            textColor = "FFFFFF"
+        }
+    val bus =
         objects.route {
             color = "FFC72C"
             textColor = "000000"
         }
+    val green =
+        objects.route {
+            color = "00843D"
+            textColor = "FFFFFF"
+        }
     MyApplicationTheme {
-        DirectionPicker(
-            availableDirections = listOf(0, 1),
-            directions =
-                listOf(
-                    Direction(name = "Outbound", destination = "Out", id = 0),
-                    Direction(name = "Inbound", destination = "In", id = 1),
-                ),
-            route = route,
-            selectedDirectionId = 0,
-            updateDirectionId = {},
-        )
+        Column {
+            DirectionPicker(
+                availableDirections = listOf(0, 1),
+                directions =
+                    listOf(
+                        Direction(name = "South", destination = "Ashmont/Braintree", id = 0),
+                        Direction(name = "North", destination = "Alewife", id = 1),
+                    ),
+                route = red,
+                selectedDirectionId = 0,
+                updateDirectionId = {},
+            )
+            DirectionPicker(
+                availableDirections = listOf(0, 1),
+                directions =
+                    listOf(
+                        Direction(
+                            name = "Outbound",
+                            destination = "Fields Corner or St Peter’s Square",
+                            id = 0,
+                        ),
+                        Direction(name = "Inbound", destination = "Ruggles Station", id = 1),
+                    ),
+                route = bus,
+                selectedDirectionId = 1,
+                updateDirectionId = {},
+            )
+            DirectionPicker(
+                availableDirections = listOf(0, 1),
+                directions =
+                    listOf(
+                        Direction(name = "West", destination = null, id = 0),
+                        Direction(name = "East", destination = null, id = 1),
+                    ),
+                route = green,
+                selectedDirectionId = 0,
+                updateDirectionId = {},
+            )
+        }
     }
 }
