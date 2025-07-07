878b8a84a6421d01b2ec83c5a42b3177f29e79b9
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-07-07 13:23:10 -0400

    fix(ios.startKoinIOSTestApp): Specify default dispatcher

--- shared/src/iosMain/kotlin/com/mbta/tid/mbta_app/Helpers.kt
+++ shared/src/iosMain/kotlin/com/mbta/tid/mbta_app/Helpers.kt
@@ -11,6 +11,8 @@ import com.mbta.tid.mbta_app.dependencyInjection.repositoriesModule
 import com.mbta.tid.mbta_app.endToEnd.endToEndModule
 import com.mbta.tid.mbta_app.viewModel.viewModelModule
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
@@ -64,7 +66,12 @@ subsequently load in specific modules to override these base definitions.
 fun startKoinIOSTestApp() {
     startKoin {
         modules(
-            platformModule() + viewModelModule() + module { single<Analytics> { MockAnalytics() } }
+            platformModule() +
+                viewModelModule() +
+                module {
+                    single<Analytics> { MockAnalytics() }
+                    single<CoroutineDispatcher> { Dispatchers.Default }
+                }
         )
     }
     loadDefaultRepoModules()
