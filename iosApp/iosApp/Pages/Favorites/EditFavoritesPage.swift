63593451d8ceb6dd9998ee5cc0f622278832ec71
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-07-11 11:48:08 -0400

    wip(ios.EditFavoritesPage): Add the requisite data

--- iosApp/iosApp/Pages/Favorites/EditFavoritesPage.swift
+++ iosApp/iosApp/Pages/Favorites/EditFavoritesPage.swift
@@ -14,6 +14,14 @@ struct EditFavoritesPage: View {
     let onClose: () -> Void
 
     @State var globalResponse: GlobalResponse?
+<<<<<<< HEAD
+=======
+    @State var favoritesVMState: FavoritesViewModel.State = .init()
+
+    @State var favoritesState: [RouteStopDirection: Bool] = [:]
+
+    @State var routeCardData: [RouteCardData]?
+>>>>>>> 56bee737e (wip(ios.EditFavoritesPage): Add the requisite data)
 
     let errorBannerVM: ErrorBannerViewModel
     let globalRepository: IGlobalRepository = RepositoryDI().global
@@ -22,15 +30,31 @@ struct EditFavoritesPage: View {
         VStack {
             SheetHeader(
                 title: NSLocalizedString("Edit Favorites", comment: "Title for flow to edit favorites"),
-                onClose: onClose,
+                onClose: {
+                    viewModel.updateFavorites(updatedFavorites: favoritesState.mapValues { KotlinBoolean(bool: $0) })
+                    onClose()
+                },
                 closeText: NSLocalizedString("Done", comment: "Button text for closing flow")
             )
-            Text(verbatim: "TODO")
+            EditFavoritesList(routeCardData: favoritesVMState.staticRouteCardData,
+                              global: globalResponse, deleteFavorite: { rsd in
+                                  favoritesState[rsd] = false
+                              })
             Spacer()
         }
         .background(Color.fill2)
         .onAppear {
             loadGlobal()
+            routeCardData = favoritesVMState.staticRouteCardData
+        }
+        .task {
+            for await model in viewModel.models {
+                favoritesVMState = model
+                let initialFavorites = model.favorites ?? []
+                favoritesState = initialFavorites.reduce(into: [RouteStopDirection: Bool]()) { partialResult, rsd in
+                    partialResult[rsd] = true
+                }
+            }
         }
     }
 
@@ -52,6 +76,38 @@ struct EditFavoritesPage: View {
                 getData: { try await globalRepository.getGlobalData() },
                 onRefreshAfterError: loadGlobal
             )
+        }
+    }
+}
+
+struct EditFavoritesList: View {
+    let routeCardData: [RouteCardData]?
+    let global: GlobalResponse?
+    let deleteFavorite: (RouteStopDirection) -> Void
+
+    var body: some View {
+        if let routeCardData, !routeCardData.isEmpty {
+            Text("TODO: show favorites here")
+        }
+
+        else if routeCardData != nil {
+            ScrollView {
+                // TODO: Separate NoStopsView
+                Text("No stops added", comment: "Indicates the absence of favorites")
+            }
+        }
+
+        else {
+            ScrollView {
+                LazyVStack(alignment: .center, spacing: 14) {
+                    ForEach(0 ..< 5) { _ in
+                        LoadingRouteCard()
+                    }
+                }
+                .padding(.vertical, 4)
+                .padding(.horizontal, 16)
+                .loadingPlaceholder()
+            }
         }
     }
 }
