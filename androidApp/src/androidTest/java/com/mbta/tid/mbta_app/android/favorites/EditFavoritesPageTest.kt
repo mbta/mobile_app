f8bc355c962f3e0c7249b701afb5126646521c45
Emma Simon <esimon@mbta.com>
2025-07-18 15:52:18 -0400

    test(Android): Add a test for edit favorites order

--- androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/favorites/EditFavoritesPageTest.kt
+++ androidApp/src/androidTest/java/com/mbta/tid/mbta_app/android/favorites/EditFavoritesPageTest.kt
@@ -1,11 +1,14 @@
 package com.mbta.tid.mbta_app.android.favorites
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.mbta.tid.mbta_app.android.pages.EditFavoritesPage
@@ -23,6 +26,7 @@ import com.mbta.tid.mbta_app.usecases.FavoritesUsecases
 import dev.mokkery.spy
 import dev.mokkery.verify.VerifyMode
 import dev.mokkery.verifySuspend
+import io.github.dellisd.spatialk.geojson.Position
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Instant
 import kotlinx.coroutines.runBlocking
@@ -34,6 +38,12 @@ import org.koin.test.KoinTest
 class EditFavoritesPageTest : KoinTest {
     val builder = ObjectCollectionBuilder()
     val now = Instant.fromEpochMilliseconds(System.currentTimeMillis())
+    val line =
+        builder.line {
+            id = "line_1"
+            color = "FF0000"
+            textColor = "FFFFFF"
+        }
     val route =
         builder.route {
             id = "route_1"
@@ -44,7 +54,7 @@ class EditFavoritesPageTest : KoinTest {
             longName = "Sample Route Long Name"
             shortName = "Sample Route"
             textColor = "000000"
-            lineId = "line_1"
+            lineId = line.id
             routePatternIds = mutableListOf("pattern_1", "pattern_2")
         }
     val routePatternOne =
@@ -52,7 +62,7 @@ class EditFavoritesPageTest : KoinTest {
             id = "pattern_1"
             directionId = 0
             name = "Sample Route Pattern"
-            routeId = "route_1"
+            routeId = route.id
             representativeTripId = "trip_1"
         }
     val routePatternTwo =
@@ -60,66 +70,56 @@ class EditFavoritesPageTest : KoinTest {
             id = "pattern_2"
             directionId = 1
             name = "Sample Route Pattern Two"
-            routeId = "route_1"
-            representativeTripId = "trip_1"
+            routeId = route.id
+            representativeTripId = "trip_2"
         }
     val sampleStop =
         builder.stop {
             id = "stop_1"
-            name = "Sample Stop"
+            name = "Sample Stop 1"
             locationType = LocationType.STOP
             latitude = 0.0
             longitude = 0.0
         }
-    val line =
-        builder.line {
-            id = "line_1"
-            color = "FF0000"
-            textColor = "FFFFFF"
+    val farStop =
+        builder.stop {
+            id = "stop_2"
+            name = "Sample Stop 2"
+            locationType = LocationType.STOP
+            latitude = 1.0
+            longitude = 1.0
         }
-    val trip =
+    val trip1 =
         builder.trip {
             id = "trip_1"
-            routeId = "route_1"
+            routeId = route.id
             directionId = 0
             headsign = "Sample Headsign"
-            routePatternId = "pattern_1"
+            routePatternId = routePatternOne.id
             stopIds = listOf(sampleStop.id)
+        }
+    val trip2 =
+        builder.trip {
+            id = "trip_2"
+            routeId = route.id
+            directionId = 1
+            headsign = "Other Headsign"
+            routePatternId = routePatternTwo.id
+            stopIds = listOf(farStop.id)
         }
     val prediction =
         builder.prediction {
             id = "prediction_1"
             revenue = true
-            stopId = "stop_1"
-            tripId = "trip_1"
-            routeId = "route_1"
+            stopId = sampleStop.id
+            tripId = trip1.id
+            routeId = route.id
             stopSequence = 1
             directionId = 0
             arrivalTime = now.plus(1.minutes)
             departureTime = now.plus(1.5.minutes)
         }
 
-    val greenLineRoute =
-        builder.route {
-            id = "route_2"
-            type = RouteType.LIGHT_RAIL
-            color = "008000"
-            directionNames = listOf("Inbound", "Outbound")
-            directionDestinations = listOf("Park Street", "Lechmere")
-            longName = "Green Line Long Name"
-            shortName = "Green Line"
-            textColor = "FFFFFF"
-            lineId = "line-Green"
-            routePatternIds = mutableListOf("pattern_3", "pattern_4")
-        }
-    val greenLineRoutePatternOne =
-        builder.routePattern(greenLineRoute) {
-            id = "pattern_3"
-            directionId = 0
-            name = "Green Line Pattern"
-            routeId = "route_2"
-            representativeTripId = "trip_2"
-        }
     val greenLine =
         builder.line {
             id = "line-Green"
@@ -128,30 +128,51 @@ class EditFavoritesPageTest : KoinTest {
             color = "008000"
             textColor = "FFFFFF"
         }
+    val greenLineRoute =
+        builder.route {
+            id = "route_gl"
+            type = RouteType.LIGHT_RAIL
+            color = "008000"
+            directionNames = listOf("Inbound", "Outbound")
+            directionDestinations = listOf("Park Street", "Lechmere")
+            longName = "Green Line Long Name"
+            shortName = "Green Line"
+            textColor = "FFFFFF"
+            lineId = greenLine.id
+            routePatternIds = mutableListOf("pattern_gl")
+        }
+    val greenLineRoutePatternOne =
+        builder.routePattern(greenLineRoute) {
+            id = "pattern_gl"
+            directionId = 0
+            name = "Green Line Pattern"
+            routeId = greenLineRoute.id
+            representativeTripId = "trip_gl"
+        }
     val greenLineStop =
         builder.stop {
-            id = "stop_2"
+            id = "stop_gl"
             name = "Green Line Stop"
             locationType = LocationType.STOP
-            latitude = 0.0
-            longitude = 0.0
+            latitude = 2.0
+            longitude = 2.0
         }
     val greenLineTrip =
         builder.trip {
-            id = "trip_2"
-            routeId = "route_2"
+            id = "trip_gl"
+            routeId = greenLineRoute.id
             directionId = 0
             headsign = "Green Line Head Sign"
-            routePatternId = "pattern_3"
+            routePatternId = greenLineRoutePatternOne.id
             stopIds = listOf(greenLineStop.id)
         }
     val greenLinePrediction =
         builder.prediction {
-            id = "prediction_2"
+            id = "prediction_gl"
             revenue = true
-            stopId = "stop_2"
-            tripId = "trip_2"
-            routeId = "route_2"
+            stopId = greenLineStop.id
+            tripId = greenLineTrip.id
+            routeId = greenLineRoute.id
             stopSequence = 1
             directionId = 0
             arrivalTime = now.plus(5.minutes)
@@ -167,14 +188,16 @@ class EditFavoritesPageTest : KoinTest {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun testFavoritesDisplayCorrectly(): Unit = runBlocking {
-        val favorites = setOf(RouteStopDirection("route_1", "stop_1", 0))
+        val favorites = setOf(RouteStopDirection(route.id, sampleStop.id, 0))
         val repository = MockFavoritesRepository(Favorites(favorites))
         val usecase = FavoritesUsecases(repository)
         val viewModel = FavoritesViewModel(usecase)
         viewModel.favorites = favorites
 
         composeTestRule.setContent {
-            KoinContext(koinApplication.koin) { EditFavoritesPage(globalResponse, viewModel) {} }
+            KoinContext(koinApplication.koin) {
+                EditFavoritesPage(globalResponse, null, viewModel) {}
+            }
         }
 
         composeTestRule.waitUntilExactlyOneExistsDefaultTimeout(hasText("Sample Route"))
@@ -194,7 +217,9 @@ class EditFavoritesPageTest : KoinTest {
         val viewModel = FavoritesViewModel(usecase)
 
         composeTestRule.setContent {
-            KoinContext(koinApplication.koin) { EditFavoritesPage(globalResponse, viewModel) {} }
+            KoinContext(koinApplication.koin) {
+                EditFavoritesPage(globalResponse, null, viewModel) {}
+            }
         }
 
         composeTestRule.waitForIdle()
@@ -207,8 +232,8 @@ class EditFavoritesPageTest : KoinTest {
     fun testDeleteFavorite(): Unit = runBlocking {
         val favorites =
             setOf(
-                RouteStopDirection("route_1", "stop_1", 0),
-                RouteStopDirection("line-Green", "stop_2", 0),
+                RouteStopDirection(route.id, sampleStop.id, 0),
+                RouteStopDirection(greenLine.id, greenLineStop.id, 0),
             )
         val repository = MockFavoritesRepository(Favorites(favorites))
         val spiedRepo = spy<IFavoritesRepository>(repository)
@@ -217,7 +242,9 @@ class EditFavoritesPageTest : KoinTest {
         viewModel.favorites = favorites
 
         composeTestRule.setContent {
-            KoinContext(koinApplication.koin) { EditFavoritesPage(globalResponse, viewModel) {} }
+            KoinContext(koinApplication.koin) {
+                EditFavoritesPage(globalResponse, null, viewModel) {}
+            }
         }
 
         composeTestRule.waitUntilExactlyOneExistsDefaultTimeout(hasText("Sample Route"))
@@ -230,7 +257,9 @@ class EditFavoritesPageTest : KoinTest {
         composeTestRule.onAllNodesWithTag("trashCan")[0].performClick()
         composeTestRule.awaitIdle()
         verifySuspend(VerifyMode.exactly(1)) {
-            spiedRepo.setFavorites(Favorites(setOf(RouteStopDirection("line-Green", "stop_2", 0))))
+            spiedRepo.setFavorites(
+                Favorites(setOf(RouteStopDirection(greenLine.id, greenLineStop.id, 0)))
+            )
         }
 
         // Should be deleted
@@ -240,5 +269,32 @@ class EditFavoritesPageTest : KoinTest {
         // Should remain
         composeTestRule.onNodeWithText("Green Line Long Name").assertIsDisplayed()
         composeTestRule.onNodeWithText("Green Line Stop").assertIsDisplayed()
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun testFavoritesOrderStopsBasedOnPosition(): Unit = runBlocking {
+        val favorites =
+            setOf(
+                RouteStopDirection(greenLine.id, greenLineStop.id, 0),
+                RouteStopDirection(route.id, farStop.id, 1),
+                RouteStopDirection(route.id, sampleStop.id, 0),
+            )
+        val repository = MockFavoritesRepository(Favorites(favorites))
+        val usecase = FavoritesUsecases(repository)
+        val viewModel = FavoritesViewModel(usecase)
+        viewModel.favorites = favorites
+
+        composeTestRule.setContent {
+            KoinContext(koinApplication.koin) {
+                EditFavoritesPage(globalResponse, Position(0.0, 0.0), viewModel) {}
+            }
+        }
+
+        val stops = composeTestRule.onAllNodesWithText("Stop", substring = true)
+        stops[0].assertTextEquals("Sample Stop 1")
+        stops[1].assertTextEquals("Sample Stop 2")
+        stops[2].assertTextEquals("Green Line Stop")
+        stops.assertCountEquals(3)
     }
 }
