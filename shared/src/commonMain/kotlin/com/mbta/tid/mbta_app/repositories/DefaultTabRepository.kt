4d3ef7eb4fe971668c18c20fd811f253ff6f6326
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-07-24 15:59:44 -0400

    wip(shared): DefaultTabRepository

---
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/repositories/DefaultTabRepository.kt
@@ -0,0 +1,50 @@
+package com.mbta.tid.mbta_app.repositories
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+enum class DefaultTab {
+    Favorites,
+    Nearby,
+}
+
+interface IDefaultTabRepository {
+
+    suspend fun getDefaultTab(): DefaultTab
+
+    suspend fun setDefaultTab(defaultTab: DefaultTab)
+
+    suspend fun hasSeenFavorites(): Boolean
+
+    suspend fun setHasSeenFavorites(hasSeenFavorites: Boolean)
+}
+
+class DefaultTabRepository : IDefaultTabRepository, KoinComponent {
+    private val dataStore: DataStore<Preferences> by inject()
+
+    private val defaultTabKey = stringPreferencesKey("default_tab")
+    private val hasSeenFavoritesKey = stringPreferencesKey("has_seen_favoirtes")
+
+    override suspend fun getDefaultTab(): DefaultTab {
+        return dataStore.data.map { it[defaultTabKey] }.first()?.let { DefaultTab.valueOf(it) }
+            ?: DefaultTab.Favorites
+    }
+
+    override suspend fun setDefaultTab(defaultTab: DefaultTab) {
+        dataStore.edit { it[defaultTabKey] = defaultTab.name }
+    }
+
+    override suspend fun hasSeenFavorites(): Boolean {
+        return dataStore.data.map { it[hasSeenFavoritesKey] }.first()?.let { it == "true" } ?: false
+    }
+
+    override suspend fun setHasSeenFavorites(hasSeenFavorites: Boolean) {
+        dataStore.edit { it[hasSeenFavoritesKey] = hasSeenFavorites.toString() }
+    }
+}
