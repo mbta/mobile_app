7e95c026a8c8b3976eb1de6222c853cf48442c48
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-08-19 10:59:04 -0400

    chore: android 2.0.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         targetSdk = 35
         versionCode =
             Integer.parseInt((findProperty("android.injected.version.code") ?: "1") as String)
-        versionName = "2.0.0"
+        versionName = "2.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
