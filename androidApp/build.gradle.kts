8a7f5b60b8806a637786cd9b50c0bdce982c2101
Emma Simon <esimon@mbta.com>
2025-09-02 14:13:42 -0400

    chore: 2.0.3 version bump (#1288)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         targetSdk = 35
         versionCode =
             Integer.parseInt((findProperty("android.injected.version.code") ?: "1") as String)
-        versionName = "2.0.2"
+        versionName = "2.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
