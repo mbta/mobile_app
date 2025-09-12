f053b3c8afddea91223157f20746398400fde953
Emma Simon <esimon@mbta.com>
2025-09-12 16:18:23 -0400

    feat(Android): Add network capability check

--- shared/src/androidMain/kotlin/com/mbta/tid/mbta_app/network/NetworkConnectivityMonitor.kt
+++ shared/src/androidMain/kotlin/com/mbta/tid/mbta_app/network/NetworkConnectivityMonitor.kt
@@ -5,15 +5,27 @@ import android.content.Context
 import android.content.Context.CONNECTIVITY_SERVICE
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
 
 internal class NetworkConnectivityMonitor(context: Context) : INetworkConnectivityMonitor {
     private var networkCallback: ConnectivityManager.NetworkCallback? = null
     private val connectivityManager =
         context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val activeCapabilities =
+        listOf(
+            NetworkCapabilities.NET_CAPABILITY_INTERNET,
+            NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED,
+            NetworkCapabilities.NET_CAPABILITY_VALIDATED,
+        )
 
     @SuppressLint("MissingPermission")
     // Permission is included in AndroidManifest.xml
     override fun registerListener(onNetworkAvailable: () -> Unit, onNetworkLost: () -> Unit) {
+        fun checkCapabilities(networkCapabilities: NetworkCapabilities) {
+            if (activeCapabilities.all(networkCapabilities::hasCapability)) onNetworkAvailable()
+            else onNetworkLost()
+        }
+
         networkCallback =
             object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
@@ -27,12 +39,21 @@ internal class NetworkConnectivityMonitor(context: Context) : INetworkConnectivi
                 override fun onLost(network: Network) {
                     onNetworkLost()
                 }
+
+                override fun onCapabilitiesChanged(
+                    network: Network,
+                    networkCapabilities: NetworkCapabilities,
+                ) {
+                    checkCapabilities(networkCapabilities)
+                }
             }
+
         // The above callback does not get called when there is no network on app startup,
         // this covers that scenario.
-        if (connectivityManager.activeNetwork == null) {
-            onNetworkLost()
-        }
+        val activeNetwork = connectivityManager.activeNetwork
+        if (activeNetwork == null) onNetworkLost()
+        connectivityManager.getNetworkCapabilities(activeNetwork)?.let { checkCapabilities(it) }
+
         networkCallback?.let { connectivityManager.registerDefaultNetworkCallback(it) }
     }
 }
