114f18eebeaf6ce29cb35356b93a0978988bba88
Emma Simon <esimon@mbta.com>
2025-11-03 12:24:06 -0500

    fix: Relocate onOpenURL modifier
    For some reason the tests broke when it was directly on ContentView

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -84,48 +84,7 @@ struct ContentView: View {
                 updateTabBarVisibility()
             }
         }
-        .onOpenURL { url in
-            let deepLink = DeepLinkState.companion.from(url: url.absoluteString)
-            nearbyVM.popToEntrypoint()
 
-            switch onEnum(of: deepLink) {
-            case let .stop(stop):
-                let nav = stop.sheetRoute
-                nearbyVM.pushNavEntry(.stopDetails(
-                    stopId: nav.stopId,
-                    stopFilter: nav.stopFilter,
-                    tripFilter: nav.tripFilter
-                ))
-
-            case let .alert(alert):
-                var stop: Stop?
-                if let stopId = alert.stopId {
-                    nearbyVM.pushNavEntry(
-                        .stopDetails(stopId: stopId, stopFilter: nil, tripFilter: nil)
-                    )
-                    stop = globalData?.getStop(stopId: alert.stopId)
-                }
-                var line: Line?
-                var routes: [Route]?
-                if let routeId = alert.routeId {
-                    let lineOrRouteId = LineOrRoute.Id.companion.fromString(id: routeId)
-                    let lineOrRoute = globalData?.getLineOrRoute(lineOrRouteId: lineOrRouteId)
-                    switch onEnum(of: lineOrRoute) {
-                    case let .line(resolved):
-                        line = resolved.line
-                        routes = Array(resolved.routes)
-                    case let .route(resolved):
-                        routes = [resolved.route]
-                    default: break
-                    }
-                }
-                nearbyVM.pushNavEntry(
-                    .alertDetails(alertId: alert.alertId, line: line, routes: routes, stop: stop)
-                )
-
-            default: break
-            }
-        }
         .onChange(of: nearbyVM.navigationStack.lastSafe()) { _ in
             updateTabBarVisibility()
         }
@@ -373,6 +332,48 @@ struct ContentView: View {
             }
             .animation(.easeOut, value: nearbyVM.navigationStack.lastSafe().sheetItemIdentifiable()?.id)
             .background { Color.fill2.ignoresSafeArea(edges: .all).animation(nil, value: "") }
+        }
+        .onOpenURL { url in
+            let deepLink = DeepLinkState.companion.from(url: url.absoluteString)
+            nearbyVM.popToEntrypoint()
+
+            switch onEnum(of: deepLink) {
+            case let .stop(stop):
+                let nav = stop.sheetRoute
+                nearbyVM.pushNavEntry(.stopDetails(
+                    stopId: nav.stopId,
+                    stopFilter: nav.stopFilter,
+                    tripFilter: nav.tripFilter
+                ))
+
+            case let .alert(alert):
+                var stop: Stop?
+                if let stopId = alert.stopId {
+                    nearbyVM.pushNavEntry(
+                        .stopDetails(stopId: stopId, stopFilter: nil, tripFilter: nil)
+                    )
+                    stop = globalData?.getStop(stopId: alert.stopId)
+                }
+                var line: Line?
+                var routes: [Route]?
+                if let routeId = alert.routeId {
+                    let lineOrRouteId = LineOrRoute.Id.companion.fromString(id: routeId)
+                    let lineOrRoute = globalData?.getLineOrRoute(lineOrRouteId: lineOrRouteId)
+                    switch onEnum(of: lineOrRoute) {
+                    case let .line(resolved):
+                        line = resolved.line
+                        routes = Array(resolved.routes)
+                    case let .route(resolved):
+                        routes = [resolved.route]
+                    default: break
+                    }
+                }
+                nearbyVM.pushNavEntry(
+                    .alertDetails(alertId: alert.alertId, line: line, routes: routes, stop: stop)
+                )
+
+            default: break
+            }
         }
     }
 
