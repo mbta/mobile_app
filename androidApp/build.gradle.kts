d7aa81d81436877e925dc2b5585ee21f9fc940e4
Emma Simon <esimon@mbta.com>
2026-01-05 10:29:12 -0500

    chore: 2.0.10 version bump

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -63,7 +63,7 @@ android {
         targetSdk = 35
         versionCode =
             Integer.parseInt((findProperty("android.injected.version.code") ?: "1") as String)
-        versionName = "2.0.9"
+        versionName = "2.0.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
