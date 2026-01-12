414a3c93548c2b78ceae61806bd3a2f4d2b22f14
Melody Horn <mhorn@mbta.com>
2026-01-12 12:23:05 -0700

    chore: bump version to 2.0.11

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -63,7 +63,7 @@ android {
         targetSdk = 35
         versionCode =
             Integer.parseInt((findProperty("android.injected.version.code") ?: "1") as String)
-        versionName = "2.0.10"
+        versionName = "2.0.11"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
