ca2a1e0bd253f6fba8069e015d0b3397b626011f
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2026-03-24 15:32:06 -0400

    fix(android.AlertListContainer): halo background for whole container

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/AlertListContainer.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/stopDetails/AlertListContainer.kt
@@ -3,16 +3,17 @@ package com.mbta.tid.mbta_app.android.stopDetails
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mbta.tid.mbta_app.android.R
 import com.mbta.tid.mbta_app.android.util.fromHex
-import com.mbta.tid.mbta_app.android.util.modifiers.haloContainer
 import com.mbta.tid.mbta_app.model.Alert
 import com.mbta.tid.mbta_app.model.AlertCardSpec
 import com.mbta.tid.mbta_app.model.ObjectCollectionBuilder
@@ -29,62 +30,72 @@ fun AlertListContainer(
     val outerCornerRadius = 8.dp
     val internalCornerRadius = 4.dp
     Column(
-        modifier = Modifier.haloContainer(2.dp, backgroundColor = colorResource(R.color.halo)),
-        verticalArrangement = Arrangement.spacedBy(1.dp),
+        modifier =
+            Modifier.background(colorResource(R.color.halo), RoundedCornerShape(outerCornerRadius))
+                .clip(RoundedCornerShape(outerCornerRadius))
     ) {
-        highPriority.forEachIndexed { index, content ->
-            val topRadius = if (index == 0) outerCornerRadius else internalCornerRadius
-            val bottomRadius =
-                if (index == highPriorityCount && (middleContent == null && lowPriorityCount == 0))
-                    outerCornerRadius
-                else internalCornerRadius
-            content(
+        Column(
+            modifier = Modifier.padding(2.dp),
+            verticalArrangement = Arrangement.spacedBy(1.dp),
+        ) {
+            highPriority.forEachIndexed { index, content ->
+                val topRadius = if (index == 0) outerCornerRadius else internalCornerRadius
+                val bottomRadius =
+                    if (
+                        index == highPriorityCount - 1 &&
+                            (middleContent == null && lowPriorityCount == 0)
+                    )
+                        outerCornerRadius
+                    else internalCornerRadius
+                content(
+                    Modifier.background(
+                        colorResource(R.color.fill3),
+                        RoundedCornerShape(
+                            topStart = topRadius,
+                            topEnd = topRadius,
+                            bottomStart = bottomRadius,
+                            bottomEnd = bottomRadius,
+                        ),
+                    )
+                )
+            }
+
+            val middleContentTopRadius =
+                if (highPriorityCount == 0) outerCornerRadius else internalCornerRadius
+            val middleContentBottomRadius =
+                if (lowPriorityCount == 0) outerCornerRadius else internalCornerRadius
+
+            middleContent?.invoke(
                 Modifier.background(
                     colorResource(R.color.fill3),
                     RoundedCornerShape(
-                        topStart = topRadius,
-                        topEnd = topRadius,
-                        bottomStart = bottomRadius,
-                        bottomEnd = bottomRadius,
+                        topStart = middleContentTopRadius,
+                        topEnd = middleContentTopRadius,
+                        bottomStart = middleContentBottomRadius,
+                        bottomEnd = middleContentBottomRadius,
                     ),
                 )
             )
-        }
 
-        val middleContentTopRadius =
-            if (highPriorityCount == 0) outerCornerRadius else internalCornerRadius
-        val middleContentBottomRadius =
-            if (lowPriorityCount == 0) outerCornerRadius else internalCornerRadius
-
-        middleContent?.invoke(
-            Modifier.background(
-                colorResource(R.color.fill3),
-                RoundedCornerShape(
-                    topStart = middleContentTopRadius,
-                    topEnd = middleContentTopRadius,
-                    bottomStart = middleContentBottomRadius,
-                    bottomEnd = middleContentBottomRadius,
-                ),
-            )
-        )
-
-        lowPriority.forEachIndexed { index, content ->
-            val topRadius =
-                if (index == 0 && highPriorityCount == 0 && middleContent == null) outerCornerRadius
-                else internalCornerRadius
-            val bottomRadius =
-                if (index == lowPriorityCount) outerCornerRadius else internalCornerRadius
-            content(
-                Modifier.background(
-                    colorResource(R.color.fill2),
-                    RoundedCornerShape(
-                        topStart = topRadius,
-                        topEnd = topRadius,
-                        bottomStart = bottomRadius,
-                        bottomEnd = bottomRadius,
-                    ),
+            lowPriority.forEachIndexed { index, content ->
+                val topRadius =
+                    if (index == 0 && highPriorityCount == 0 && middleContent == null)
+                        outerCornerRadius
+                    else internalCornerRadius
+                val bottomRadius =
+                    if (index == lowPriorityCount - 1) outerCornerRadius else internalCornerRadius
+                content(
+                    Modifier.background(
+                        colorResource(R.color.fill2),
+                        RoundedCornerShape(
+                            topStart = topRadius,
+                            topEnd = topRadius,
+                            bottomStart = bottomRadius,
+                            bottomEnd = bottomRadius,
+                        ),
+                    )
                 )
-            )
+            }
         }
     }
 }
