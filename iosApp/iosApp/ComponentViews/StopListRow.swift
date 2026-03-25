49d0b324b9ff6442a67733a78361725a42abe5a0
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2026-03-25 09:25:32 -0400

    fix(ios.StopListRow): Disruption card wider than stop rows

--- iosApp/iosApp/ComponentViews/StopListRow.swift
+++ iosApp/iosApp/ComponentViews/StopListRow.swift
@@ -109,7 +109,11 @@ struct StopListRow<Descriptor: View, RightSideContent: View>: View {
                         routeAccents: routeAccents,
                         onViewDetails: { onOpenAlertDetails(disruption.alert) },
                         internalPadding: .init(top: 0, leading: 5, bottom: 0, trailing: 0)
-                    ).withRoundedBorder()
+                    ).background(Color.fill3)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .padding(1)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.halo, lineWidth: 2))
+                        .padding(.top, 1)
                         .padding(.horizontal, -4)
                 }
             }
