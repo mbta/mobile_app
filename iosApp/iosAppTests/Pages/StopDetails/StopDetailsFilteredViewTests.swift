4e63c0f26241d9e4a3706ce2cc5be4ab86782e66
Kayla Brady <kbrady@mbta.com>
2026-05-14 09:23:03 -0400

    test(ios.StopDetailsFilteredView): Test no loading when stop + route match

--- iosApp/iosAppTests/Pages/StopDetails/StopDetailsFilteredViewTests.swift
+++ iosApp/iosAppTests/Pages/StopDetails/StopDetailsFilteredViewTests.swift
@@ -61,4 +61,77 @@ final class StopDetailsFilteredViewTests: XCTestCase {
         ]))
         wait(for: [tapButtonExp, confirmationDialogExp], timeout: 4)
     }
+
+    @MainActor func testShowsDataWhenStopAndRouteHaventChanged() throws {
+        let objects = ObjectCollectionBuilder()
+        let stop = objects.stop { _ in }
+        let route = objects.route()
+        let directionId: Int32 = 0
+
+        let favoritesRepository = MockFavoritesRepository()
+
+        let stopDetailsVM = MockStopDetailsViewModel(initialState: .init(
+            routeData: .some(StopDetailsViewModel.RouteDataFiltered(
+                filteredWith: .init(stopId: stop.id, stopFilter: .init(routeId: route.id, directionId: 1),
+                                    tripFilter: nil),
+                stopData: .init(route: route, stop: stop, data: [RouteCardData.Leaf(
+                    lineOrRoute: LineOrRoute.Route(route: route),
+                    stop: stop,
+                    directionId: 0,
+                    routePatterns: [],
+                    stopIds: Set([stop.id]),
+                    upcomingTrips: [],
+                    alertsHere: [],
+                    allDataLoaded: true,
+                    hasSchedulesToday: true,
+                    subwayServiceStartTime: nil,
+                    alertsDownstream: [],
+                    context: .stopDetailsFiltered
+                ), RouteCardData.Leaf(
+                    lineOrRoute: LineOrRoute.Route(route: route),
+                    stop: stop,
+                    directionId: 1,
+                    routePatterns: [],
+                    stopIds: Set([stop.id]),
+                    upcomingTrips: [],
+                    alertsHere: [],
+                    allDataLoaded: true,
+                    hasSchedulesToday: true,
+                    subwayServiceStartTime: nil,
+                    alertsDownstream: [],
+                    context: .stopDetailsFiltered
+                )],
+                globalData: GlobalResponse(objects: objects))
+            )),
+            alertSummaries: [:],
+            awaitingPredictionsAfterBackground: false
+        ))
+
+        let sut = StopDetailsFilteredView(
+            stopId: stop.id,
+            stopFilter: .init(routeId: route.id, directionId: directionId),
+            tripFilter: nil,
+            routeData: nil,
+            favorites: .init(routeStopDirection: [:]),
+            global: .init(objects: objects),
+            now: Date.now,
+            onUpdateFavorites: {},
+            setStopFilter: { _ in },
+            setTripFilter: { _ in },
+            navCallbacks: .companion.empty,
+            errorBannerVM: MockErrorBannerViewModel(),
+            nearbyVM: .init(),
+            mapVM: MockMapViewModel(),
+            stopDetailsVM: stopDetailsVM,
+        )
+
+        let exp = sut.inspection.inspect(after: 1) { view in
+            XCTAssertNotNil(
+                try? view.find(StopDetailsFilteredPickerView.self)
+            )
+        }
+
+        ViewHosting.host(view: sut.withFixedSettings([:]))
+        wait(for: [exp], timeout: 2)
+    }
 }
