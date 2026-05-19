38d81ea879b812120b400f37e8c62602774f87be
Kayla Brady <kbrady@mbta.com>
2026-05-19 16:02:03 -0400

    feat(android.StopDetailsFiltered): Show direction picker + flood color on load

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/routeCard/LoadingRouteCard.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/component/routeCard/LoadingRouteCard.kt
@@ -3,14 +3,13 @@ package com.mbta.tid.mbta_app.android.component.routeCard
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.mbta.tid.mbta_app.android.util.modifiers.loadingShimmer
 import com.mbta.tid.mbta_app.model.LoadingPlaceholders
 import com.mbta.tid.mbta_app.utils.EasternTimeInstant
 
 @Composable
 fun LoadingRouteCard() {
     val placeholderRouteData = LoadingPlaceholders.nearbyRoute()
-    Column(modifier = Modifier.loadingShimmer(withShimmer = false)) {
+    Column(modifier = Modifier) {
         RouteCard(
             placeholderRouteData,
             null,
