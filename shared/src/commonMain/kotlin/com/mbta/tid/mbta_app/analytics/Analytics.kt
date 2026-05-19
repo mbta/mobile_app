1596abb54f37dc9be360cb228c172ddf9b7ba93c
Melody Horn <mhorn@mbta.com>
2026-05-19 09:13:53 -0600

    fix formatting and add test

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/analytics/Analytics.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/analytics/Analytics.kt
@@ -74,10 +74,13 @@ public abstract class Analytics {
 
     public fun notificationClicked(analyticsLabel: String) {
         val pieces = analyticsLabel.split(';')
-        val parameters = pieces.map { piece ->
-            val (name, value) = piece.split('=', limit = 2)
-            name to value
-        }.toTypedArray()
+        val parameters =
+            pieces
+                .map { piece ->
+                    val (name, value) = piece.split('=', limit = 2)
+                    name to value
+                }
+                .toTypedArray()
         logEvent("notification_clicked", *parameters)
     }
 
