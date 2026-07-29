c8ef34b30eb826e9c5bf9b01a0faa8c2d42a4394
Kayla Brady <kbrady@mbta.com>
2026-07-29 11:55:10 -0400

    fix: don't sort stops since changed to sets

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/viewModel/FavoritesViewModel.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/viewModel/FavoritesViewModel.kt
@@ -154,12 +154,10 @@ public class FavoritesViewModel(
         val stopIds =
             remember(favorites, globalData) {
                 val stops = favorites?.keys?.mapNotNull { globalData?.getStop(it.stop) }
-                stops
-                    ?.flatMap { stop ->
-                        stop.childStopIds.filter { globalData?.stops?.containsKey(it) ?: false } +
-                            stop.id
-                    }
-                    ?.sorted()
+                stops?.flatMap { stop ->
+                    stop.childStopIds.filter { globalData?.stops?.containsKey(it) ?: false } +
+                        stop.id
+                }
             }
         val schedules = getSchedules(stopIds?.toSet(), errorKey)
         val predictions =
