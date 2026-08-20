7ea2d71eb78c7703dfa7d0e25a43760a5408920a
Tony Lizarraga <126337469+thedecaftony@users.noreply.github.com>
2026-08-20 18:45:19 +0000

    Feat: Group Android notifications by alert id + Overwrite update noti… (#1938)
    * Feat: Group Android notifications by alert id + Overwrite update notifications that have the same title and body
    
    * Rename prevNotification to ungroupedNotification

--- androidApp/src/main/java/com/mbta/tid/mbta_app/android/notification/MBTAGoMessagingService.kt
+++ androidApp/src/main/java/com/mbta/tid/mbta_app/android/notification/MBTAGoMessagingService.kt
@@ -1,12 +1,14 @@
 package com.mbta.tid.mbta_app.android.notification
 
 import android.Manifest
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.media.AudioAttributes
 import android.media.RingtoneManager
+import android.service.notification.StatusBarNotification
 import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -48,18 +50,8 @@ class MBTAGoMessagingService : FirebaseMessagingService() {
                 intent,
                 PendingIntent.FLAG_IMMUTABLE,
             )
-
         val channelId = getString(R.string.alerts_channel)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder =
-            NotificationCompat.Builder(applicationContext, channelId)
-                .setSmallIcon(R.drawable.app_icon_monochrome)
-                .setContentTitle(message.notification?.title)
-                .setContentText(message.notification?.body)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
 
         val notificationManager = NotificationManagerCompat.from(applicationContext)
 
@@ -76,6 +68,97 @@ class MBTAGoMessagingService : FirebaseMessagingService() {
 
         notificationManager.createNotificationChannel(channel)
 
-        notificationManager.notify(Random.nextInt(), notificationBuilder.build())
+        val notificationBuilder =
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.app_icon_monochrome)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+
+        // A message having alert_id in the body means it is a data only notification to help
+        // Android notifications group together. It will update the ungrouped notification with the
+        // same tag
+        if (message.data.containsKey("alert_id")) {
+            val alertId = message.data["alert_id"]
+            val ungroupedNotification =
+                findUngroupedNotification(notificationManager, message.data["tag"]) ?: return
+
+            updateNotification(
+                notificationManager,
+                notificationBuilder,
+                ungroupedNotification,
+                alertId,
+                channelId,
+                message,
+            )
+        } else {
+            notificationBuilder
+                .setContentTitle(message.notification?.title)
+                .setContentText(message.notification?.body)
+            notificationManager.notify(
+                message.notification?.tag,
+                0,
+                notificationBuilder.build(),
+            )
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun updateNotification(
+        notificationManager: NotificationManagerCompat,
+        notificationBuilder: NotificationCompat.Builder,
+        ungroupedNotification: StatusBarNotification,
+        alertId: String?,
+        channelId: String,
+        message: RemoteMessage,
+    ) {
+        // Try using the title and body from the previous notification, if they exist.
+        // Otherwise, use the title and body from the new message.
+        val title =
+            ungroupedNotification.notification.extras.getString(Notification.EXTRA_TITLE)
+                ?: message.data.getOrDefault("title", null)
+        val body =
+            ungroupedNotification.notification.extras.getString(Notification.EXTRA_TEXT)
+                ?: message.data.getOrDefault("body", null)
+        notificationBuilder
+            .setContentTitle(title)
+            .setContentText(body)
+            .setOnlyAlertOnce(true)
+            .setGroup(alertId)
+
+        notificationManager.notify(
+            ungroupedNotification.tag,
+            ungroupedNotification.id,
+            notificationBuilder.build(),
+        )
+
+        // A notification summary is required, using latest notification title and body.
+        val summaryNotification =
+            NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.drawable.app_icon_monochrome)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setGroup(alertId)
+                .setGroupSummary(true)
+                .build()
+
+        notificationManager.notify(
+            "summary-${alertId}",
+            ungroupedNotification.id,
+            summaryNotification,
+        )
+    }
+
+    private fun findUngroupedNotification(
+        notificationManager: NotificationManagerCompat,
+        tag: String?,
+    ): StatusBarNotification? {
+        val activeNotifications = notificationManager.activeNotifications
+        // Finding notification by filtering out all the ones that already have a group
+        // and then checking if the tag matches the one we are looking for
+        return activeNotifications.find { notification ->
+            !notification.isGroup && notification.tag != null && notification.tag == tag
+        }
     }
 }
