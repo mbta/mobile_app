38f146aec27919cedc806667414c99cfed06f841
Emma Simon <esimon@mbta.com>
2026-08-26 12:33:42 -0400

    fix: Move Sentry beforeSend check to platform code
    The docs are really vague and apparently you can't just parse the OS
    context as a map, it's a specific type of object, but we have no access
    to that object unless we're on Android, in shared code we can't use any
    libraries outside of io.sentry.kotlin.multiplatform, so I needed to move
    the check into the platform specific code so that the OS context can be
    cast into the right type. I tested locally with Sentry enabled to verify
    that the kernel version string is populated.

---
+++ shared/src/jvmMain/kotlin/com/mbta/tid/mbta_app/utils/SentryBeforeSend.jvm.kt
@@ -0,0 +1,9 @@
+package com.mbta.tid.mbta_app.utils
+
+import io.sentry.kotlin.multiplatform.SentryEvent
+
+internal class JVMSentryBeforeSend : SentryBeforeSend {
+    override fun processEvent(event: SentryEvent): SentryEvent? = event
+}
+
+internal actual fun getSentryBeforeSend(): SentryBeforeSend = JVMSentryBeforeSend()
