b52f184705be475d8e0880ab2df8ab10fc767319
Emma Simon <esimon@mbta.com>
2026-08-26 17:25:39 +0000

    fix: Move Sentry beforeSend check to platform code (#1948)
    The docs are really vague and apparently you can't just parse the OS
    context as a map, it's a specific type of object, but we have no access
    to that object unless we're on Android, in shared code we can't use any
    libraries outside of io.sentry.kotlin.multiplatform, so I needed to move
    the check into the platform specific code so that the OS context can be
    cast into the right type. I tested locally with Sentry enabled to verify
    that the kernel version string is populated.

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/AppSetup.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/AppSetup.kt
@@ -1,22 +1,16 @@
 package com.mbta.tid.mbta_app
 
+import com.mbta.tid.mbta_app.utils.getSentryBeforeSend
 import io.sentry.kotlin.multiplatform.Sentry
-import io.sentry.kotlin.multiplatform.SentryEvent
 import io.sentry.kotlin.multiplatform.SentryOptions
 
 public fun initializeSentry(dsn: String, environment: String) {
+    val beforeSend = getSentryBeforeSend()
     val configuration: (SentryOptions) -> Unit = {
         it.dsn = dsn
         it.environment = environment
         it.beforeBreadcrumb = { breadcrumb -> breadcrumb }
-        it.beforeSend = { event -> if (shouldSkipEvent(event)) null else event }
+        it.beforeSend = beforeSend::processEvent
     }
     Sentry.init(configuration)
-}
-
-private fun shouldSkipEvent(event: SentryEvent): Boolean {
-    val osContext = event.contexts["os"] as? Map<*, *>
-    val kernelVersion = osContext?.get("kernel_version") as? String
-    val isGoogleCI = kernelVersion?.contains("android-build") ?: false
-    return isGoogleCI
 }
