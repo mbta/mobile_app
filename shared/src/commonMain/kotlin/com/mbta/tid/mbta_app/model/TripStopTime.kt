074d2c113b009c05c8ead109acc6bc3a18c11e39
Emma Simon <esimon@mbta.com>
2026-09-01 14:52:57 -0400

    fix: Use departure time for CR and ferry trips
    On nearby transit and stop details, show the departure times for
    schedule based modes, instead of arrivals.

--- shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/TripStopTime.kt
+++ shared/src/commonMain/kotlin/com/mbta/tid/mbta_app/model/TripStopTime.kt
@@ -9,6 +9,13 @@ public interface TripStopTime : Comparable<TripStopTime> {
     public val stopTime: EasternTimeInstant?
         get() = arrivalTime ?: departureTime
 
+    /**
+     * For Ferry and Commuter Rail, the departure time is the primary time that should be displayed,
+     * but if arrival is the only time that exists, we should still fall back to that.
+     */
+    public val scheduleBasedStopTime: EasternTimeInstant?
+        get() = departureTime ?: arrivalTime
+
     public fun stopTimeAfter(now: EasternTimeInstant): EasternTimeInstant? =
         arrivalTime?.takeUnless { it < now } ?: departureTime
 
